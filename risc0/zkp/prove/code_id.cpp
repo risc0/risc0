2616c7324a4661bec514b999e2d639e35996d296
Jeremy Bruestle <jeremy.bruestle@gmail.com>
2022-02-20 15:08:28 -0800

    Verify needs code-id too, but just read

--- risc0/zkp/prove/code_id.cpp
+++ risc0/zkp/prove/code_id.cpp
@@ -52,18 +52,4 @@ void writeCodeID(const std::string& filename, const CodeID& id) {
   }
 }
 
-CodeID readCodeID(const std::string& filename) {
-  std::ifstream file(filename, std::ios::in | std::ios::binary);
-  if (!file) {
-    throw std::runtime_error("Unable to open file: " + filename);
-  }
-  CodeID id;
-  file.read(reinterpret_cast<char*>(&id), sizeof(CodeID)); 
-  file.close();
-  if (!file.good()) {
-    throw std::runtime_error("Error reading code id file: " + filename);
-  }
-  return id;
-}
-
 }  // namespace risc0
--- risc0/zkp/prove/code_id.h
+++ risc0/zkp/prove/code_id.h
@@ -1,20 +1,11 @@
 #pragma once
 
-#include <array>
-
-#include "risc0/core/util.h"
-#include "risc0/zkp/circuit/constants.h"
-#include "risc0/zkp/core/sha256.h"
+#include "risc0/zkp/verify/code_id.h"
 
 namespace risc0 {
 
-constexpr size_t kCodeDigestCount = log2Ceil(kMaxCycles / kMinCycles) + 1;
-
-using CodeID = std::array<ShaDigest, kCodeDigestCount>;
-
 CodeID makeCodeID(const std::string& elfFile);
 void writeCodeID(const std::string& filename, const CodeID& id);
-CodeID readCodeID(const std::string& filename);
 
 } // namespace risc0
 
