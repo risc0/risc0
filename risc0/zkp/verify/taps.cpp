cc0de7504cae59a097a54f7f2d0155433841fed5
Jeremy Bruestle <jeremy.bruestle@gmail.com>
2022-03-16 16:24:59 -0700

    Fixes for CR

--- risc0/zkp/verify/taps.cpp
+++ risc0/zkp/verify/taps.cpp
@@ -8,7 +8,7 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or detailied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -64,7 +64,7 @@ void TapSet::finalize() {
       size_t comboID = combosToID[combo];
       // Add the actual taps
       for (size_t back : combo) {
-        impl::TapData data;
+        detail::TapData data;
         REQUIRE(back < 65536);
         REQUIRE(reg < 65536);
         REQUIRE(combo.size() < 256);
--- risc0/zkp/verify/taps.h
+++ risc0/zkp/verify/taps.h
@@ -8,7 +8,7 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or detailied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -37,9 +37,9 @@ enum class RegisterGroup : uint8_t {
 
 static constexpr size_t kNumRegisterGroups = 3;
 
-namespace impl {
+namespace detail {
 
-// This class is implementation detail and carefully built to be efficient on
+// This class is detailementation detail and carefully built to be efficient on
 // RISC-V for use in recursion.
 struct TapData {
   uint16_t offset;     // The offset in register group (reg #)
@@ -62,25 +62,25 @@ struct TapData {
   }
 };
 
-} // namespace impl
+} // namespace detail
 
-// Ref object to hide implementation details
+// Ref object to hide detailementation details
 class TapRef {
 public:
-  TapRef(const impl::TapData* data) : data_(data) {}
+  TapRef(const detail::TapData* data) : data_(data) {}
   RegisterGroup group() const { return data_->group; }
   size_t offset() const { return data_->offset; }
   size_t back() const { return data_->back; }
   size_t comboID() const { return data_->combo; }
 
 private:
-  const impl::TapData* data_;
+  const detail::TapData* data_;
 };
 
 // Not much of an interator, just enough to make range for happy
 class TapIterator {
 public:
-  TapIterator(const impl::TapData* data) : data_(data) {}
+  TapIterator(const detail::TapData* data) : data_(data) {}
   TapRef operator*() const { return TapRef(data_); }
   TapIterator& operator++() {
     ++data_;
@@ -90,13 +90,13 @@ public:
   bool operator!=(const TapIterator& rhs) const { return data_ != rhs.data_; }
 
 private:
-  const impl::TapData* data_;
+  const detail::TapData* data_;
 };
 
 // Ref object to view a group of taps as a single register
 class RegRef {
 public:
-  RegRef(const impl::TapData* data) : data_(data) {}
+  RegRef(const detail::TapData* data) : data_(data) {}
   // Get tap elements which are fixed for a given register
   RegisterGroup group() const { return data_->group; }
   size_t offset() const { return data_->offset; }
@@ -111,12 +111,12 @@ public:
 
 private:
   // Points to the first tap for this register
-  const impl::TapData* data_;
+  const detail::TapData* data_;
 };
 
 class RegIterator {
 public:
-  RegIterator(const impl::TapData* data) : data_(data) {}
+  RegIterator(const detail::TapData* data) : data_(data) {}
   RegRef operator*() const { return RegRef(data_); }
   RegIterator& operator++() {
     data_ = data_ + data_->skip;
@@ -127,14 +127,14 @@ public:
 
 private:
   // Points to the first tap for this register
-  const impl::TapData* data_;
+  const detail::TapData* data_;
 };
 
-namespace impl {
+namespace detail {
 
 // Combo data holds the tap set for each 'combo'.  Basically, combo N consists
 // of taps in the range [offsets[n], offsets[n+1]).  Again this is an
-// implementation detail, and the format is designed to put the actual arrays
+// detailementation detail, and the format is designed to put the actual arrays
 // into static data.
 struct ComboData {
   uint16_t* taps;
@@ -142,24 +142,24 @@ struct ComboData {
   uint16_t count;
 };
 
-} // namespace impl
+} // namespace detail
 
 class ComboRef {
 public:
-  ComboRef(const impl::ComboData* data, size_t id) : data_(data), id_(id) {}
+  ComboRef(const detail::ComboData* data, size_t id) : data_(data), id_(id) {}
   size_t id() const { return id_; }
   size_t size() const { return data_->offsets[id_ + 1] - data_->offsets[id_]; }
   const uint16_t* begin() const { return data_->taps + data_->offsets[id_]; }
   const uint16_t* end() const { return data_->taps + data_->offsets[id_ + 1]; }
 
 private:
-  const impl::ComboData* data_;
+  const detail::ComboData* data_;
   size_t id_;
 };
 
 class ComboIterator {
 public:
-  ComboIterator(const impl::ComboData* data, size_t id) : data_(data), id_(id) {}
+  ComboIterator(const detail::ComboData* data, size_t id) : data_(data), id_(id) {}
   ComboRef operator*() const { return ComboRef(data_, id_); }
   ComboIterator& operator++() {
     id_++;
@@ -167,19 +167,19 @@ public:
   }
 
 private:
-  const impl::ComboData* data_;
+  const detail::ComboData* data_;
   size_t id_;
 };
 
-namespace impl {
+namespace detail {
 
 struct TapSetData {
-  const impl::TapData* taps;
+  const detail::TapData* taps;
   size_t groupBegin[kNumRegisterGroups + 1];
   ComboData combos;
 };
 
-} // namespace impl
+} // namespace detail
 
 template <typename It> class IteratorRange {
 public:
@@ -194,39 +194,48 @@ private:
 
 class TapSetRef {
 private:
-  const impl::TapSetData* data_;
+  const detail::TapSetData* data_;
 
 public:
-  TapSetRef(const impl::TapSetData* data) : data_(data) {}
+  TapSetRef(const detail::TapSetData* data) : data_(data) {}
+
   size_t tapsSize() { return data_->groupBegin[kNumRegisterGroups]; }
+
   IteratorRange<TapIterator> taps() {
     return IteratorRange<TapIterator>(data_->taps,
                                       data_->taps + data_->groupBegin[kNumRegisterGroups]);
   }
+
   IteratorRange<RegIterator> regs() {
     return IteratorRange<RegIterator>(data_->taps,
                                       data_->taps + data_->groupBegin[kNumRegisterGroups]);
   }
+
   IteratorRange<TapIterator> groupTaps(RegisterGroup group) {
     size_t groupID = static_cast<size_t>(group);
     return IteratorRange<TapIterator>(data_->taps + data_->groupBegin[groupID],
                                       data_->taps + data_->groupBegin[groupID + 1]);
   }
+
   IteratorRange<RegIterator> groupRegs(RegisterGroup group) {
     size_t groupID = static_cast<size_t>(group);
     return IteratorRange<RegIterator>(data_->taps + data_->groupBegin[groupID],
                                       data_->taps + data_->groupBegin[groupID + 1]);
   }
+
   size_t groupSize(RegisterGroup group) {
     size_t groupID = static_cast<size_t>(group);
     size_t last = (data_->taps + data_->groupBegin[groupID + 1] - 1)->offset;
     return last + 1;
   }
+
   size_t combosSize() const { return data_->combos.count; }
+
   IteratorRange<ComboIterator> combos() {
     return IteratorRange<ComboIterator>(ComboIterator(&data_->combos, 0),
                                         ComboIterator(&data_->combos, data_->combos.count));
   }
+
   ComboRef getCombo(size_t id) { return ComboRef(&data_->combos, id); }
 };
 
@@ -236,16 +245,16 @@ private:
 
   // Stuff used only while building
   bool finalized_;
-  using reg_t = std::set<size_t>;
-  using group_t = std::map<size_t, reg_t>;
-  using all_t = std::map<RegisterGroup, group_t>;
-  all_t all_;
+  using Reg = std::set<size_t>;
+  using Group = std::map<size_t, Reg>;
+  using All = std::map<RegisterGroup, group_t>;
+  All all_;
 
   // Stuff that exists post finalization
-  std::vector<impl::TapData> taps_;
+  std::vector<detail::TapData> taps_;
   std::vector<uint16_t> comboTaps_;
   std::vector<uint16_t> comboBegin_;
-  impl::TapSetData data_;
+  detail::TapSetData data_;
 
 public:
   // Make an empty tap set
