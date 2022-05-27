f3f440329725b564afc5cf36138899ef18ab3823
Frank Laub <flaub@risc0.com>
2022-05-27 13:30:58 -0700

    Use tbb-sys 1.1

--- risc0/zkp/build.rs
+++ risc0/zkp/build.rs
@@ -12,13 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::env;
+
 use cxx_build::CFG;
 
 fn main() {
     CFG.include_prefix = "risc0/zkp";
     CFG.exported_header_links = vec!["risc0-core-sys"];
 
-    cxx_build::bridge("lib.rs")
+    let mut build = cxx_build::bridge("lib.rs");
+    build
         .file("accel/backend/cpu/impl.cpp")
         .file("core/ntt.cpp")
         .file("core/poly.cpp")
@@ -34,8 +37,13 @@ fn main() {
         .define("__TBB_NO_IMPLICIT_LINKAGE", None)
         .flag_if_supported("/std:c++17")
         .flag_if_supported("-std=c++17")
-        .warnings(false)
-        .compile("risc0-zkp-sys");
+        .warnings(false);
+
+    if let Some(inc_dir) = env::var_os("DEP_TBB_INCLUDE") {
+        build.include(inc_dir);
+    }
+
+    build.compile("risc0-zkp-sys");
 
     println!("cargo:rustc-link-lib=static=tbb");
     println!("cargo:rustc-link-lib=static=risc0-core-sys");
