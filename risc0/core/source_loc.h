8f0140289f10ffc47ddb34c8003de131619ebc8d
Frank Laub <frank@risczero.com>
2022-07-16 22:58:34 -0700

    Fix older C++ compat

--- risc0/core/source_loc.h
+++ risc0/core/source_loc.h
@@ -21,15 +21,26 @@
 /// until c++20, we build our own minialist version.  This is currently used to help track EDSL
 /// expressions for the IR.
 
-namespace risc0 {
+#ifdef __has_builtin
+#if __has_builtin(__builtin_FILE)
+#define FILE_EXPR __builtin_FILE()
+#else
+#define FILE_EXPR __FILE__
+#endif
+#else
+#define FILE_EXPR __FILE__
+#endif
 
-/// A source location.  Capture as much as we can from the compiler, which might be nothing.
-struct SourceLoc {
-public:
-  /// Get the "current" source location.  When used in default values, this effectively captures the
-  /// call site of the function declaring the default value, which is very useful.
-  static constexpr SourceLoc current(const char* filename = __builtin_FILE(),
-                                     int line = __builtin_LINE(),
+#ifdef __has_builtin
+#if __has_builtin(__builtin_LINE)
+#define LINE_EXPR __builtin_LINE()
+#else
+#define LINE_EXPR __LINE__
+#endif
+#else
+#define LINE_EXPR __LINE__
+#endif
+
 #ifdef __has_builtin
 #if __has_builtin(__builtin_COLUMN)
 #define COLUMN_EXPR __builtin_COLUMN()
@@ -39,6 +50,16 @@ public:
 #else
 #define COLUMN_EXPR 0
 #endif
+
+namespace risc0 {
+
+/// A source location.  Capture as much as we can from the compiler, which might be nothing.
+struct SourceLoc {
+public:
+  /// Get the "current" source location.  When used in default values, this effectively captures the
+  /// call site of the function declaring the default value, which is very useful.
+  static constexpr SourceLoc current(const char* filename = FILE_EXPR,
+                                     int line = LINE_EXPR,
                                      int column = COLUMN_EXPR) noexcept {
     SourceLoc loc;
     loc.filename = filename;
