99c60ed2e1d0a8e8cb60a4c1655521d051646002
Tim Zerrell <tim.zerrell@risczero.com>
2022-07-21 12:51:06 -0700

    Add intro doc for risc0-make-id

--- risc0/zkvm/prove/make-id/src/main.rs
+++ risc0/zkvm/prove/make-id/src/main.rs
@@ -12,12 +12,21 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//! Creates RISC Zero MethodIDs from ELF files
+//!
+//! A simple command line tool for creating
+//! [MethodID](risc0_zkvm::host::MethodId)s from RISC-V ELF files. It is
+//! intended for basic, low-level use cases. In particular, users looking to
+//! create MethodIDs for Rust code are advised to instead use our zkVM: visit
+//! our [Risc Zero Rust Starter repository](https://github.com/risc0/risc0-rust-starter)
+//! for help getting started.
+
 use clap::Parser;
 use std::fs;
 
 use risc0_zkvm::host::{MethodId, DEFAULT_METHOD_ID_LIMIT};
 
-/// Generates a MethodID for a given RISC-V ELF binary.
+/// The command line arguments necessary to run [main].
 #[derive(Parser)]
 #[clap(about, version, author)]
 struct Args {
@@ -32,6 +41,11 @@ struct Args {
     limit: u32,
 }
 
+/// Generates a MethodID for a given RISC-V ELF binary.
+///
+/// Must be provided command line arguments formatted according to [Args].
+/// Alternatively, command line help is available by running with the argument
+/// `--help`.
 fn main() {
     let args = Args::parse();
     let elf_contents = fs::read(args.elf).unwrap();
