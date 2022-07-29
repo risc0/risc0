48e4b24164fd68820c358e05bed8acbc7138991d
Nils McCarthy <nils@risc0.com>
2022-07-28 18:54:25 -0700

    Add rust criterion benchmarks for running stuff in the guest

--- risc0/zkvm/sdk/rust/methods/build.rs
+++ risc0/zkvm/sdk/rust/methods/build.rs
@@ -1,7 +1,12 @@
 use risc0_build::{embed_methods_with_options, GuestOptions};
-use std::collections::HashMap;
+use std::{collections::HashMap, env};
 
 fn main() {
+    if env::var("CARGO_CFG_TARGET_OS").unwrap().contains("zkvm") {
+        // Guest shouldn't recursively depend on itself.
+        return;
+    }
+
     let inner_pkg_options = GuestOptions {
         code_limit: 10,
         features: vec!["test_feature1".to_string(), "test_feature2".to_string()],
--- risc0/zkvm/sdk/rust/methods/src/lib.rs
+++ risc0/zkvm/sdk/rust/methods/src/lib.rs
@@ -14,4 +14,7 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
+pub mod bench;
+
+#[cfg(not(target_os = "zkvm"))]
 include!(concat!(env!("OUT_DIR"), "/methods.rs"));
