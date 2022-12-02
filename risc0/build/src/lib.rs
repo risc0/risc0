994cb088b3ceb22b650d1754f765c63659456539
Frank Laub <frank@risczero.com>
2022-12-02 05:09:54 -0800

    Drop replacements

--- risc0/build/src/lib.rs
+++ risc0/build/src/lib.rs
@@ -44,7 +44,7 @@
 //! then `Cargo.toml` might include
 //! ```toml
 //! [build-dependencies]
-//! risc0-build = "0.12"
+//! risc0-build = "1.0.0-rc.1"
 //!
 //! [package.metadata.risc0]
 //! methods = ["guest"]
