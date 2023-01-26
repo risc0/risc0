fd8a7241279e8c959c70ea69ffa7beaac6bf70a4
Jeremy Bruestle <jeremy@risczero.com>
2023-01-26 23:41:12 +0000

    Fix endinanness on cuda

--- risc0/zkp/kernels/cuda/sha256.h
+++ risc0/zkp/kernels/cuda/sha256.h
@@ -194,6 +194,10 @@ __device__ inline ShaDigest shaHashInner(const T* data, size_t size, size_t stri
     compress(state, words);
   }
 
+  for (size_t i = 0; i < 8; i++) {
+    state.words[i] = impl::convertU32(state.words[i]);
+  }
+
   return state;
 }
 
