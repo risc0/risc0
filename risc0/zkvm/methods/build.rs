99e682ed41029d57aed1d3846f6ede6e96d1a491
Victor Graf <victor@subgraf.dev>
2023-02-02 13:57:05 -0800

    address review comments

--- risc0/zkvm/methods/build.rs
+++ risc0/zkvm/methods/build.rs
@@ -11,11 +11,10 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 use std::{collections::HashMap, env};
 
 use risc0_build::{embed_methods_with_options, GuestOptions};
-
-#[allow(unreachable_code)]
 
 fn main() {
     env_logger::init();
