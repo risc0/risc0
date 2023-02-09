80907eb90648a813aff5965426a9adbc2c4200aa
Tim Zerrell <tim.zerrell@risczero.com>
2023-02-09 12:48:30 -0800

    Add test for factors example

--- examples/factors/factors/src/main.rs
+++ examples/factors/factors/src/main.rs
@@ -32,7 +32,7 @@ fn main() {
     prover.add_input_u32_slice(&to_vec(&b).expect("should be serializable"));
     // Run prover & generate receipt
     let receipt = prover.run()
-        .expect("Valid code should be provable if it doesn't overflow the cycle limit. See `embed_methods_with_options` for information on adjusting maximum cycle count.");
+        .expect("Should be able to prove valid code that fits in the cycle count.");
 
     // Extract journal of receipt (i.e. output c, where c = a * b)
     let c: u64 = from_slice(&receipt.journal).expect(
@@ -48,4 +48,32 @@ fn main() {
     receipt.verify(&MULTIPLY_ID).expect(
         "Code you have proven should successfully verify; did you specify the correct method ID?",
     );
+}
+
+#[cfg(test)]
+mod tests {
+    use factors_methods::{MULTIPLY_ELF, MULTIPLY_ID};
+    use risc0_zkvm::serde::{from_slice, to_vec};
+    use risc0_zkvm::Prover;
+
+    const TEST_FACTOR_ONE: u64 = 17;
+    const TEST_FACTOR_TWO: u64 = 23;
+
+    #[test]
+    fn run_factors() {
+        let mut prover = Prover::new(MULTIPLY_ELF, MULTIPLY_ID).expect(
+            "Prover should be constructed from valid method source code and corresponding method ID",
+        );
+
+        prover.add_input_u32_slice(&to_vec(&TEST_FACTOR_ONE).expect("should be serializable"));
+        prover.add_input_u32_slice(&to_vec(&TEST_FACTOR_TWO).expect("should be serializable"));
+
+        let receipt = prover.run().expect("Should be able to prove valid code");
+        receipt.verify(&MULTIPLY_ID).expect("Proven code should verify");
+
+        let result: u64 = from_slice(&receipt.journal).expect(
+            "Journal output should deserialize into the same types (& order) that it was written",
+        );
+        assert_eq!(result, TEST_FACTOR_ONE * TEST_FACTOR_TWO, "We expect the zkVM output to be the product of the inputs")
+    }
 }
