d154164f070d1f2d12f080d1f9ced672ffb61efe
Tim Zerrell <tim.zerrell@risczero.com>
2023-02-28 11:57:06 -0800

    Revise add_input_u8_slice docs
    Co-authored-by: Frank Laub <flaub@risc0.com>

--- risc0/zkvm/src/prove/mod.rs
+++ risc0/zkvm/src/prove/mod.rs
@@ -265,8 +265,7 @@ impl<'a> Prover<'a> {
     /// guest reads them in the same order they are added by the [Prover].
     /// However, to reduce maintenance burden and the chance of mistakes, we
     /// recommend instead using a single `struct` to hold all the inputs and
-    /// calling [Prover::add_input_u8_slice] just once (on that input
-    /// `struct`).
+    /// calling [Prover::add_input_u8_slice] just once (on the serialized representation of the input.
     pub fn add_input_u8_slice(&mut self, slice: &[u8]) {
         self.inner.input.extend_from_slice(slice);
     }
