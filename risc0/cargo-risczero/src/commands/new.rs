f5dd57de130ee7f3d574ba92d1f27ce7d94ce26e
Parker Thompson <parker@risczero.com>
2023-03-17 15:50:40 -0700

    Added additional options to `cargo-risczero new`

--- risc0/cargo-risczero/src/commands/new.rs
+++ risc0/cargo-risczero/src/commands/new.rs
@@ -3,7 +3,7 @@ use std::path::PathBuf;
 use cargo_generate::{GenerateArgs, TemplatePath, Vcs};
 use convert_case::{Case, Casing};
 
-const RISC0_GH_REPO: &str = "risc0/risc0";
+const RISC0_GH_REPO: &str = "https://github.com/risc0/risc0";
 const RICS0_TEMPLATE_DIR: &str = "templates/rust-stater";
 const RISC0_RELEASE_TAG: &str = "v0.13.0";
 
@@ -18,16 +18,26 @@ pub struct NewCommand {
     #[clap(value_parser, long, short, default_value = RISC0_GH_REPO)]
     pub template: String,
 
-    /// template git branch or tag.
-    #[clap(value_parser, long, short, default_value = RISC0_RELEASE_TAG)]
+    /// template git tag.
+    #[clap(value_parser, long, default_value = RISC0_RELEASE_TAG)]
+    pub tag: String,
+
+    /// template git branch, overrides `tag` option
+    #[clap(value_parser, long, default_value = "")]
     pub branch: String,
 
     /// Destination directory to create project in.
     ///
     /// Example `cargo risczero new --dest /tmp/ cool-project` ->
     /// `/tmp/cool-project/`
-    #[clap(value_parser, long, short)]
+    ///
+    /// Default: `pwd`
+    #[clap(value_parser, long)]
     pub dest: Option<PathBuf>,
+
+    /// Disable init'ing a git repo in the dest project
+    #[clap(value_parser, long, global = true)]
+    pub no_git: bool,
 }
 
 impl NewCommand {
@@ -46,17 +56,24 @@ impl NewCommand {
                 .join(self.name.clone())
         };
 
+        let mut template_path = TemplatePath {
+            auto_path: Some(self.template.clone()),
+            subfolder,
+            git: None,
+            branch: None,
+            path: None,
+            favorite: None,
+            tag: Some(self.tag.clone()),
+            test: false,
+        };
+
+        if !self.branch.is_empty() {
+            template_path.branch = Some(self.branch.clone());
+            template_path.tag = None;
+        }
+
         cargo_generate::generate(GenerateArgs {
-            template_path: TemplatePath {
-                auto_path: Some(self.template.clone()),
-                subfolder,
-                git: None,
-                branch: None,
-                path: None,
-                favorite: None,
-                tag: Some(self.branch.clone()),
-                test: false,
-            },
+            template_path,
             list_favorites: false,
             name: Some(self.name.to_case(Case::Snake)),
             force: true,
@@ -64,7 +81,11 @@ impl NewCommand {
             template_values_file: None,
             silent: false,
             config: None,
-            vcs: Some(Vcs::Git),
+            vcs: if self.no_git {
+                Some(Vcs::None)
+            } else {
+                Some(Vcs::Git)
+            },
             lib: false,
             bin: false,
             ssh_identity: None,
@@ -76,6 +97,6 @@ impl NewCommand {
             overwrite: false,
             other_args: None,
         })
-        .unwrap();
+        .expect("Failed to generate project");
     }
 }
