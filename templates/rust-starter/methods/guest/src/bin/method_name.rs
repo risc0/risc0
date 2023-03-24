7eba478190190bd3f9928a1325d15a6eb2bfbab5
Parker Thompson <parker@risczero.com>
2023-03-24 15:31:10 -0700

    Move comment related to no_std in template

--- templates/rust-starter/methods/guest/src/bin/method_name.rs
+++ templates/rust-starter/methods/guest/src/bin/method_name.rs
@@ -1,8 +1,8 @@
 // TODO: Rename this file to change the name of this method from METHOD_NAME
 
 #![no_main]
-// If you want to try std support, also update the guest Cargo.toml file
 {% unless risc0_std -%}
+// If you want to try std support, also update the guest Cargo.toml file
 #![no_std]  // std support is experimental
 {% endunless %}
 risc0_zkvm::guest::entry!(main);
