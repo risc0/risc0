8e678e22d8a7839924ee4b5781140fe38775b94b
Frank Laub <frank@risczero.com>
2023-03-29 12:30:55 -0700

    Fixes for cargo-risczero new

--- templates/rust-starter/host/src/main.rs
+++ templates/rust-starter/host/src/main.rs
@@ -3,27 +3,25 @@
 // `METHOD_NAME_ID` with `MULTIPLY_ID`
 use methods::{METHOD_NAME_ELF, METHOD_NAME_ID};
 use risc0_zkvm::Prover;
-// use risc0_zkvm::serde::{from_slice, to_vec};
 
 fn main() {
     // Make the prover.
-    let mut prover = Prover::new(METHOD_NAME_ELF, METHOD_NAME_ID).expect(
-        "Prover should be constructed from valid method source code and corresponding method ID",
-    );
+    let mut prover =
+        Prover::new(METHOD_NAME_ELF).expect("Prover should be constructed from valid ELF binary");
 
     // TODO: Implement communication with the guest here
 
     // Run prover & generate receipt
     let receipt = prover.run().expect(
-        "Code should be provable unless it had an error or overflowed the maximum cycle count",
+        "Code should be provable unless it had an error or exceeded the maximum cycle limit",
     );
+
+    // TODO: Implement code for transmitting or serializing the receipt for
+    // other parties to verify here
 
     // Optional: Verify receipt to confirm that recipients will also be able to
     // verify your receipt
     receipt.verify(&METHOD_NAME_ID).expect(
         "Code you have proven should successfully verify; did you specify the correct method ID?",
     );
-
-    // TODO: Implement code for transmitting or serializing the receipt for
-    // other parties to verify here
 }
