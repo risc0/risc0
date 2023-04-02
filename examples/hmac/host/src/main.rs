7f6d5c4720bdcd394a78f10c47278dd70c629e2c
pavlovdog <pavlovdog.moscow@gmail.com>
2023-04-02 01:02:16 +0100

    Add HMAC

---
+++ examples/hmac/host/src/main.rs
@@ -0,0 +1,77 @@
+// Copyright 2023 RISC Zero, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use clap::{Arg, Command};
+use methods::{HOTP_ELF, HOTP_ID};
+use risc0_zkp::core::sha::Digest;
+use risc0_zkvm::serde::{from_slice, to_vec};
+use risc0_zkvm::{Prover, Receipt};
+
+fn hmac(key: &str, message: &str) -> Receipt {
+    // Make the prover.
+    let mut prover = Prover::new(HOTP_ELF, HOTP_ID)
+        .expect("Prover should be constructed from matching code and method ID");
+
+    prover.add_input_u32_slice(&to_vec(key).expect("input string should serialize"));
+    prover.add_input_u32_slice(&to_vec(message).expect("input string should serialize"));
+
+    // Run prover & generate receipt
+    prover.run().expect("Code should be provable")
+}
+
+fn main() {
+    // Parse command line
+    let matches = Command::new("hash")
+        .arg(Arg::new("message").default_value("Some text"))
+        .arg(Arg::new("key").default_value("123"))
+        .get_matches();
+
+    let message = matches.get_one::<String>("message").unwrap();
+    let key = matches.get_one::<String>("key").unwrap();
+
+    // Prove hash and verify it
+    let receipt = hmac(key, message);
+    receipt.verify(HOTP_ID).expect("Proven code should verify");
+
+    let journal = receipt.journal;
+    let digest =
+        from_slice::<Digest>(journal.as_slice()).expect("Journal should contain SHA Digest");
+
+    println!("I provably know data whose SHA-256 hash is {}", digest);
+}
+
+// #[cfg(test)]
+// mod tests {
+//     use methods::HOTP_ID;
+//     use risc0_zkp::core::sha::Digest;
+//     use risc0_zkvm::serde::from_slice;
+//
+//     use crate::hmac;
+//
+//     const TEST_STRING: &str = "abc";
+//
+//     #[test]
+//     fn main() {
+//         let receipt = hmac(TEST_STRING);
+//         receipt.verify(HOTP_ID).expect("Proven code should verify");
+//
+//         let digest = from_slice::<Digest>(receipt.journal.as_slice())
+//             .expect("Journal should contain SHA Digest");
+//         assert_eq!(
+//             hex::encode(digest.as_bytes()),
+//             "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad",
+//             "We expect to match the reference SHA-256 hash of the standard test value 'abc'"
+//         );
+//     }
+// }
---
+++ examples/hmac/methods/guest/src/bin/hotp.rs
@@ -0,0 +1,68 @@
+// Copyright 2023 RISC Zero, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#![no_main]
+
+use risc0_zkvm::guest::env;
+use risc0_zkvm::sha::{sha, Sha};
+
+risc0_zkvm::guest::entry!(main);
+
+pub const SHA_DIGEST_BYTES: usize = 64;
+const SHA_KEY_BYTES: usize = 64;
+
+pub fn main() {
+    let key: String = env::read();
+    let message: String = env::read();
+
+    // set constants for HMAC
+    let inner_pad_byte: u8 = 0x36;
+    let outer_pad_byte: u8 = 0x5c;
+    let key_pad_byte:   u8 = 0x00;
+
+    let mut auth_key: &mut [u8; SHA_KEY_BYTES] = &mut [key_pad_byte; SHA_KEY_BYTES];
+
+    // if the key is longer than the hasher's block length, it should be truncated using the hasher
+    if key.len() > SHA_KEY_BYTES {
+        // derive new authentication from provided key
+        // sha1_ctx.update(key);
+        let sha = sha().hash_bytes(&key.as_bytes());
+
+        // assign derived authentication key
+        auth_key[..SHA_DIGEST_BYTES].copy_from_slice(&*sha.as_bytes());
+    } else {
+        auth_key[..key.len()].copy_from_slice(key.as_bytes());
+    }
+
+    // generate padding arrays
+    let mut inner_padding: [u8; SHA_KEY_BYTES] = [inner_pad_byte; SHA_KEY_BYTES];
+    let mut outer_padding: [u8; SHA_KEY_BYTES] = [outer_pad_byte; SHA_KEY_BYTES];
+
+    for offset in 0..auth_key.len() {
+        inner_padding[offset] ^= auth_key[offset];
+        outer_padding[offset] ^= auth_key[offset];
+    }
+
+    let inner_hash = sha().hash_bytes(&[
+        &inner_padding,
+        message.as_bytes()
+    ].concat()[..]);
+
+    let mac = sha().hash_bytes(&[
+        &outer_padding,
+        &*inner_hash.as_bytes()
+    ].concat()[..]);
+
+    env::commit(&*mac);
+}
