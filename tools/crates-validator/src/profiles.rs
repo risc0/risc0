f9d7ba3cb974a0829167df99e375659fe5832412
Parker Thompson <parker@risczero.com>
2023-04-07 14:51:06 -0700

    Added license strings

--- tools/crates-validator/src/profiles.rs
+++ tools/crates-validator/src/profiles.rs
@@ -1,3 +1,17 @@
+// Copyright 2023 RISC Zero, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use crate::CrateProfile;
 
 pub const SKIP_CRATES: &[&str] = &[
@@ -28,8 +42,8 @@ pub fn lookup_crate(crate_name: &str, mut profile: CrateProfile) -> CrateProfile
         "crossbeam-epoch" | "crossbeam-deque" | "rayon" | "rayon-core" | "crossbeam-queue"
         | "concurrent-queue" | "vsdb" | "vsdbsled" | "redox_users" | "async-channel"
         | "async-io" | "async-executor" | "blocking" => {
-            // NOTE: the crate 'crossbeam-utils'|'crossbeam-channel'|'crossbeam' itself still fails to build
-            // because you can't crates-io patch itself
+            // NOTE: the crate 'crossbeam-utils'|'crossbeam-channel'|'crossbeam' itself
+            // still fails to build because you can't crates-io patch itself
             profile.crossbeam_patch = true;
         }
         "criterion" => {
