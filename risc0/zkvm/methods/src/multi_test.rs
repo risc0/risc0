d4a24311b86e8f823f6bf78b5c043da1461f69d2
Erik Kaneda <erik@risczero.com>
2023-05-03 10:36:09 -0700

    Revert "multitest: add test that serializes f32 and f64"
    We have coverage in the serializer and deserializer so this should be sufficient.

--- risc0/zkvm/methods/src/multi_test.rs
+++ risc0/zkvm/methods/src/multi_test.rs
@@ -61,7 +61,6 @@ pub enum MultiTestSpec {
         /// Busy loop until the guest has run for at least this number of cycles
         cycles: u32,
     },
-    Float,
 }
 
 declare_syscall!(pub SYS_MULTI_TEST);
