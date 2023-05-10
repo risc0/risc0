f1146f37ac7de5952d81f90431a69c29d0c12783
Victor Graf <victor@risczero.com>
2023-05-10 12:15:33 -0700

    remove unused import

--- risc0/cargo-risczero/src/commands/build.rs
+++ risc0/cargo-risczero/src/commands/build.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::{env, fs, path::PathBuf};
+use std::{fs, path::PathBuf};
 
 use clap::Parser;
 
