20a719fadeba3ea4ee6bb039b7c2d5be3c890abf
3lkn <109624900+3lkn@users.noreply.github.com>
2023-05-16 10:25:23 -0700

    adding comments to rust starter template

--- templates/rust-starter/host/src/main.rs
+++ templates/rust-starter/host/src/main.rs
@@ -2,13 +2,25 @@
 // is `multiply`, replace `METHOD_NAME_ELF` with `MULTIPLY_ELF` and replace
 // `METHOD_NAME_ID` with `MULTIPLY_ID`
 use methods::{METHOD_NAME_ELF, METHOD_NAME_ID};
-use risc0_zkvm::{Executor, ExecutorEnv};
-use risc0_zkvm::serde::{from_slice, to_vec};
+use risc0_zkvm::{
+    serde::{from_slice, to_vec},
+    Executor, ExecutorEnv,
+};
 
 fn main() {
+    // First, we construct an executor environment
     let env = ExecutorEnv::default();
+
+    // TODO: add guest input to the executor environment using
+    // ExecutorEnv::add_input()
+
+    // Next, we make an executor, loading the (renamed) ELF binary.
     let mut exec = Executor::from_elf(env, METHOD_NAME_ELF).unwrap();
+
+    // Run the executor to produce a session.
     let session = exec.run().unwrap();
+
+    // Prove the session to produce a receipt.
     let receipt = session.prove().unwrap();
 
     // TODO: Implement code for transmitting or serializing the receipt for
