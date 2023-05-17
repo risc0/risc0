753aa8cabd34a77210a85b7f1e6c9f3dd85693b2
3lkn <109624900+3lkn@users.noreply.github.com>
2023-05-17 14:01:21 -0700

    adding library to factors example

--- examples/factors/src/main.rs
+++ examples/factors/src/main.rs
@@ -12,7 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use factors_methods::{MULTIPLY_ELF, MULTIPLY_ID};
+use factors::multiply_factors;
+use factors_methods::MULTIPLY_ID;
 use risc0_zkvm::{
     serde::{from_slice, to_vec},
     Executor, ExecutorEnv, SessionReceipt,
@@ -20,7 +21,7 @@ use risc0_zkvm::{
 
 fn main() {
     // Pick two numbers
-    let (receipt, _) = factors(17, 23);
+    let (receipt, _) = multiply_factors(17, 23);
 
     // Here is where one would send 'receipt' over the network...
 
@@ -28,47 +29,4 @@ fn main() {
     receipt.verify(MULTIPLY_ID).expect(
         "Code you have proven should successfully verify; did you specify the correct image ID?",
     );
-}
-
-// Multiply them inside the ZKP
-fn factors(a: u64, b: u64) -> (SessionReceipt, u64) {
-    let env = ExecutorEnv::builder()
-        // Send a & b to the guest
-        .add_input(&to_vec(&a).unwrap())
-        .add_input(&to_vec(&b).unwrap())
-        .build();
-
-    // First, we make an executor, loading the 'multiply' ELF binary.
-    let mut exec = Executor::from_elf(env, MULTIPLY_ELF).unwrap();
-
-    // Run the executor to produce a session.
-    let session = exec.run().unwrap();
-
-    // Prove the session to produce a receipt.
-    let receipt = session.prove().unwrap();
-
-    // Extract journal of receipt (i.e. output c, where c = a * b)
-    let c: u64 = from_slice(&receipt.journal).expect(
-        "Journal output should deserialize into the same types (& order) that it was written",
-    );
-
-    // Print an assertion
-    println!("I know the factors of {}, and I can prove it!", c);
-
-    (receipt, c)
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn factors() {
-        const TEST_FACTOR_ONE: u64 = 17;
-        const TEST_FACTOR_TWO: u64 = 23;
-        let (_, result) = super::factors(17, 23);
-        assert_eq!(
-            result,
-            TEST_FACTOR_ONE * TEST_FACTOR_TWO,
-            "We expect the zkVM output to be the product of the inputs"
-        )
-    }
 }
