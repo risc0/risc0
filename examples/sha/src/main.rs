efe8dbf819914120930fc8d4ffb1453094b55fe4
Victor Graf <victor@risczero.com>
2023-05-18 11:13:09 -0700

    add explanatory comments to the provavble_hash method and return receipt

--- examples/sha/src/main.rs
+++ examples/sha/src/main.rs
@@ -20,7 +20,17 @@ use risc0_zkvm::{
 };
 use sha_methods::{HASH_ELF, HASH_RUST_CRYPTO_ELF};
 
-fn provably_hash(input: &str, use_rust_crypto: bool) -> Digest {
+/// Hash the given bytes, returning the digest and a [Receipt] that can be used
+/// to verify that the that the hash was computed correctly (i.e. that the
+/// Prover knows a preimage for the given SHA-256 hash)
+///
+/// Select which method to use with `use_rust_crypto`.
+/// HASH_ELF uses the risc0_zkvm::sha interface for hashing.
+/// HASH_RUST_CRYPTO_ELF uses RustCrypto's [sha2] crate, patched to use the RISC
+/// Zero accelerator. See `src/methods/guest/Cargo.toml` for the patch
+/// definition, which can be used to enable SHA-256 accelerrator support
+/// everywhere the [sha2] crate is used.
+fn provably_hash(input: &str, use_rust_crypto: bool) -> (Digest, Receipt) {
     let env = ExecutorEnv::builder()
         .add_input(&to_vec(input).unwrap())
         .build();
@@ -35,10 +45,11 @@ fn provably_hash(input: &str, use_rust_crypto: bool) -> Digest {
     let session = exec.run().unwrap();
     let receipt = session.prove().unwrap();
 
-    from_slice::<Vec<u8>, _>(&receipt.journal)
+    let digest = from_slice::<Vec<u8>, _>(&receipt.journal)
         .unwrap()
         .try_into()
-        .unwrap()
+        .unwrap();
+    (digest, receipt)
 }
 
 fn main() {
@@ -48,17 +59,25 @@ fn main() {
         .get_matches();
     let message = matches.get_one::<String>("message").unwrap();
 
-    // Prove hash and verify it
+    // Prove hash the message.
     let digest = provably_hash(message, false);
+
+    // Verify the receipt, ensuring the prover knows a valid SHA-256 preimage.
+    receipt
+        .verify(HASH_ELF)
+        .expect("receipt verification failed");
 
     println!("I provably know data whose SHA-256 hash is {}", digest);
 }
 
 #[cfg(test)]
 mod tests {
+    use sha_methods::{HASH_ID, HASH_RUST_CRYPTO_ID};
+
     #[test]
     fn hash_abc() {
-        let digest = super::provably_hash("abc", false);
+        let (digest, receipt) = super::provably_hash("abc", false);
+        receipt.verify(HASH_ID).unwrap();
         assert_eq!(
             hex::encode(digest.as_bytes()),
             "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad",
@@ -69,6 +88,7 @@ mod tests {
     #[test]
     fn hash_abc_rust_crypto() {
         let digest = super::provably_hash("abc", true);
+        receipt.verify(HASH_RUST_CRYPTO_ID).unwrap();
         assert_eq!(
             hex::encode(digest.as_bytes()),
             "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad",
