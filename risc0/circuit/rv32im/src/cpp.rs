48f1a324876848f9f64baf6cb06808106d36c55b
Frank Laub <flaub@risc0.com>
2023-05-18 14:10:32 -0700

    Fix for #569 (#570)

--- risc0/circuit/rv32im/src/cpp.rs
+++ risc0/circuit/rv32im/src/cpp.rs
@@ -131,13 +131,7 @@ impl PolyFp<BabyBear> for CircuitImpl {
     ) -> BabyBearExtElem {
         let args: Vec<*const BabyBearElem> = args.iter().map(|x| (*x).as_ptr()).collect();
         unsafe {
-            risc0_circuit_rv32im_poly_fp(
-                cycle,
-                steps,
-                mix as *const BabyBearExtElem,
-                args.as_ptr(),
-                args.len(),
-            )
+            risc0_circuit_rv32im_poly_fp(cycle, steps, mix as *const BabyBearExtElem, args.as_ptr())
         }
     }
 }
