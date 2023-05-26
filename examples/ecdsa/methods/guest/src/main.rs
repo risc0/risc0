f72c277881b5002e4d37d1ed484f358367bbaee7
Victor Graf <victor@risczero.com>
2023-05-26 14:10:15 -0700

    add inital version of ecdsa verification example

---
+++ examples/ecdsa/methods/guest/src/main.rs
@@ -0,0 +1,37 @@
+// Copyright 2023 RISC Zero, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#![no_main]
+
+use k256::{
+    ecdsa::{signature::Verifier, Signature, VerifyingKey},
+    EncodedPoint,
+};
+use risc0_zkvm::guest::env;
+
+risc0_zkvm::guest::entry!(main);
+
+// Example of using the risc0_zkvm::sha module to hash data.
+pub fn main() {
+    // Decode the verifying key, message, and signature from the inputs.
+    let (encoded_verifying_key, message, signature): (EncodedPoint, Vec<u8>, Signature) =
+        env::read();
+    let verifying_key = VerifyingKey::from_encoded_point(&encoded_verifying_key).unwrap();
+
+    // Verify the signature, panicking if verification fails.
+    assert!(verifying_key.verify(&message, &signature).is_ok());
+
+    // Commit to the journal the verifying key and messge that was signed.
+    env::commit(&(encoded_verifying_key, message));
+}
---
+++ examples/ecdsa/src/main.rs
@@ -0,0 +1,72 @@
+// Copyright 2023 RISC Zero, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use ecdsa_methods::ECDSA_VERIFY_ELF;
+use k256::{
+    ecdsa::{signature::Signer, Signature, SigningKey, VerifyingKey},
+    EncodedPoint,
+};
+use rand_core::OsRng;
+use risc0_zkvm::{
+    serde::{from_slice, to_vec},
+    Executor, ExecutorEnv, SessionReceipt,
+};
+
+fn prove_ecdsa_verification(
+    verifying_key: &VerifyingKey,
+    message: &[u8],
+    signature: &Signature,
+) -> SessionReceipt {
+    let env = ExecutorEnv::builder()
+        .add_input(&to_vec(&(verifying_key.to_encoded_point(true), message, signature)).unwrap())
+        .build();
+
+    let mut exec = Executor::from_elf(env, ECDSA_VERIFY_ELF).unwrap();
+    let session = exec.run().unwrap();
+    println!(
+        "Total insn cycles: {}",
+        session
+            .segments
+            .iter()
+            .map(|s| s.resolve().unwrap().insn_cycles)
+            .reduce(|sum, cycles| sum + cycles)
+            .unwrap()
+    );
+    session.prove().unwrap()
+}
+
+fn main() {
+    // Signing
+    let signing_key = SigningKey::random(&mut OsRng); // Serialize with `::to_bytes()`
+    let message = b"ECDSA proves knowledge of a secret number in the context of a single message";
+
+    // Note: The signature type must be annotated or otherwise inferable as
+    // `Signer` has many impls of the `Signer` trait (for both regular and
+    // recoverable signature types).
+    let signature: Signature = signing_key.sign(message);
+
+    let receipt = prove_ecdsa_verification(signing_key.verifying_key(), message, &signature);
+
+    let (receipt_verifying_key, receipt_message) =
+        from_slice::<(EncodedPoint, Vec<u8>), _>(&receipt.journal)
+            .unwrap()
+            .try_into()
+            .unwrap();
+
+    println!(
+        "Verified the signature over message {:?} with key {}",
+        std::str::from_utf8(&receipt_message[..]).unwrap(),
+        receipt_verifying_key,
+    );
+}
