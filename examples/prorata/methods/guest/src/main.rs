97bea837bd28e90b80c9c1a382995a3020299e91
Choong Ng <choong@risczero.com>
2023-06-01 21:24:43 +0000

    wip: get rid of unused cbor crate and quiet warning about one of its dependencies.

--- examples/prorata/methods/guest/src/main.rs
+++ examples/prorata/methods/guest/src/main.rs
@@ -1,10 +1,23 @@
+// Copyright 2023 RISC Zero, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #![no_main]
 // If you want to try std support, also update the guest Cargo.toml file
 // #![no_std]  // std support is experimental
 
 use prorata_core::{allocate_for_csv, AllocationQuery, AllocationQueryResult};
 use risc0_zkvm::guest::env;
-use rust_decimal::Decimal;
 
 risc0_zkvm::guest::entry!(main);
 
