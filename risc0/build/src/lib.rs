dd3bcc786facb8760a85a7a6795e1ea947f7e2ce
Victor Graf <victor@risczero.com>
2023-06-07 17:32:13 -0700

    update standard to point to 16108417d828fe40298a1e87da4a12b400a76205

--- risc0/build/src/lib.rs
+++ risc0/build/src/lib.rs
@@ -135,9 +135,9 @@ struct ZipMapEntry {
 // Sources for standard library, and where they should be mapped to.
 const RUST_LIB_MAP : &[ZipMapEntry] = &[
     ZipMapEntry {
-        filename: "7e199f10288a5d072a81e6502b0a6020688be086.zip",
-        zip_url: "https://github.com/risc0/rust/archive/7e199f10288a5d072a81e6502b0a6020688be086.zip",
-        src_prefix: "rust-7e199f10288a5d072a81e6502b0a6020688be086/library",
+        filename: "16108417d828fe40298a1e87da4a12b400a76205.zip",
+        zip_url: "https://github.com/risc0/rust/archive/16108417d828fe40298a1e87da4a12b400a76205.zip",
+        src_prefix: "rust-16108417d828fe40298a1e87da4a12b400a76205/library",
         dst_prefix: "library"
     },
     ZipMapEntry {
