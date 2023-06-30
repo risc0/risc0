39c30ba82a962bbe43ddaa74b667215c570f4bf8
Parker Thompson <parker@risczero.com>
2023-06-30 14:18:34 -0700

    Bonsai SDK - image UUID to imageID (#662)
    * Migrated to imageId being supplied as the primary key for image/upload
    
    * Convert to bonsai-sdk to thiserror

--- bonsai/sdk/src/alpha.rs
+++ bonsai/sdk/src/alpha.rs
@@ -14,10 +14,10 @@
 
 use std::{fs::File, path::Path};
 
-use anyhow::{bail, Context, Result};
 use reqwest::{blocking::Client as BlockingClient, header};
+use thiserror::Error;
 
-use self::responses::{CreateSessRes, ProofReq, SessionStatusRes, UploadRes};
+use self::responses::{CreateSessRes, ImgUploadRes, ProofReq, SessionStatusRes, UploadRes};
 
 /// Collection of serialization object for the REST api
 pub mod responses {
@@ -30,6 +30,13 @@ pub mod responses {
         pub url: String,
         /// Generated UUID for this input
         pub uuid: String,
+    }
+
+    /// Response of a image upload request
+    #[derive(Deserialize, Serialize)]
+    pub struct ImgUploadRes {
+        /// Presigned URL to be supplied to a PUT request
+        pub url: String,
     }
 
     /// Session creation response
@@ -70,27 +77,48 @@ pub struct SessionId {
     pub uuid: String,
 }
 
+/// Bonsai Alpha SDK error classes
+#[derive(Debug, Error)]
+pub enum SdkErr {
+    /// The API already has the supplied imageID
+    #[error("the supplied imageId already exists")]
+    ImageIdExists,
+    /// Server side failure
+    #[error("server error `{0}`")]
+    InternalServerErr(String),
+    /// http reqwest errors
+    #[error("HTTP error from reqwest")]
+    HttpErr(#[from] reqwest::Error),
+    /// Header construction error
+    #[error("HTTP header failed to construct")]
+    HttpHeaderErr(#[from] reqwest::header::InvalidHeaderValue),
+    /// Missing BONSAI_API_KEY
+    #[error("missing BONSAI_API_KEY env var")]
+    MissingApiKey,
+    /// Missing BONSAI_API_URL
+    #[error("missing BONSAI_API_URL env var")]
+    MissingApiUrl,
+    /// Missing file
+    #[error("failed to find file on disk")]
+    FileNotFound(#[from] std::io::Error),
+}
+
 impl SessionId {
     /// Construct a [SessionId] from a UUID [String]
     pub fn new(uuid: String) -> Self {
         Self { uuid }
     }
 
-    /// Retries the current status of the Session
-    pub fn status(&self, client: &Client) -> Result<SessionStatusRes> {
+    /// Fetches the current status of the Session
+    pub fn status(&self, client: &Client) -> Result<SessionStatusRes, SdkErr> {
         let url = format!("{}/sessions/status/{}", client.url, self.uuid);
-        let res = client
-            .client
-            .get(url)
-            .send()
-            .context("Failed to GEt session status")?;
+        let res = client.client.get(url).send()?;
 
         if !res.status().is_success() {
             let body = res.text()?;
-            bail!("Request failed - server error: '{body}'");
+            return Err(SdkErr::InternalServerErr(body));
         }
-        res.json::<SessionStatusRes>()
-            .context("Failed to deserialize Session status result")
+        Ok(res.json::<SessionStatusRes>()?)
     }
 }
 
@@ -101,15 +129,14 @@ pub struct Client {
 }
 
 /// Creates a [reqwest::Client] for internal connection pooling
-fn construct_req_client(api_key: &str) -> Result<BlockingClient> {
+fn construct_req_client(api_key: &str) -> Result<BlockingClient, SdkErr> {
     let mut headers = header::HeaderMap::new();
     headers.insert("x-api-key", header::HeaderValue::from_str(api_key)?);
 
-    BlockingClient::builder()
+    Ok(BlockingClient::builder()
         .default_headers(headers)
         .pool_max_idle_per_host(0)
-        .build()
-        .context("Failed to build reqwest client")
+        .build()?)
 }
 
 impl Client {
@@ -117,9 +144,9 @@ impl Client {
     ///
     /// Uses the BONSAI_API_URL and BONSAI_API_KEY environment variables to
     /// construct a client
-    pub fn from_env() -> Result<Self> {
-        let api_url = std::env::var("BONSAI_API_URL").context("Missing BONSAI_API_URL env var")?;
-        let api_key = std::env::var("BONSAI_API_KEY").context("Missing BONSAI_API_KEY env var")?;
+    pub fn from_env() -> Result<Self, SdkErr> {
+        let api_url = std::env::var("BONSAI_API_URL").or_else(|_| Err(SdkErr::MissingApiUrl))?;
+        let api_key = std::env::var("BONSAI_API_KEY").or_else(|_| Err(SdkErr::MissingApiKey))?;
 
         let client = construct_req_client(&api_key)?;
 
@@ -130,38 +157,50 @@ impl Client {
     }
 
     /// Construct a [Client] from url + api key strings
-    pub fn from_parts(url: String, key: String) -> Result<Self> {
+    pub fn from_parts(url: String, key: String) -> Result<Self, SdkErr> {
         let client = construct_req_client(&key)?;
         Ok(Self { url, client })
     }
 
     /// Fetch a upload presigned url for a given route
-    fn get_upload_url(&self, route: &str) -> Result<UploadRes> {
+    fn get_upload_url(&self, route: &str) -> Result<UploadRes, SdkErr> {
         let res = self
             .client
             .get(format!("{}/{}/upload", self.url, route))
-            .send()
-            .context("Failed to fetch upload location")?;
+            .send()?;
 
         if !res.status().is_success() {
             let body = res.text()?;
-            bail!("Request failed - server error: '{body}'");
+            return Err(SdkErr::InternalServerErr(body));
         }
 
-        res.json::<UploadRes>()
-            .context("Failed to deserialize upload response")
+        Ok(res.json::<UploadRes>()?)
+    }
+
+    fn get_image_upload_url(&self, image_id: &str) -> Result<ImgUploadRes, SdkErr> {
+        let res = self
+            .client
+            .get(format!("{}/images/upload/{}", self.url, image_id))
+            .send()?;
+
+        if res.status() == 204 {
+            return Err(SdkErr::ImageIdExists);
+        }
+
+        if !res.status().is_success() {
+            let body = res.text()?;
+            return Err(SdkErr::InternalServerErr(body));
+        }
+
+        Ok(res.json::<ImgUploadRes>()?)
     }
 
     /// Upload body to a given URL
-    fn put_data<T: Into<reqwest::blocking::Body>>(&self, url: &str, body: T) -> Result<()> {
-        let res = self
-            .client
-            .put(url)
-            .body(body)
-            .send()
-            .context("Failed to PUT data to destination")?;
+    fn put_data<T: Into<reqwest::blocking::Body>>(&self, url: &str, body: T) -> Result<(), SdkErr> {
+        let res = self.client.put(url).body(body).send()?;
         if !res.status().is_success() {
-            bail!("Failed to PUT to provided URL");
+            let body = res.text()?;
+            return Err(SdkErr::InternalServerErr(body));
         }
 
         Ok(())
@@ -174,10 +213,10 @@ impl Client {
     /// The image data can be either:
     /// * ELF file bytes
     /// * bincode encoded MemoryImage
-    pub fn upload_img(&self, buf: Vec<u8>) -> Result<String> {
-        let upload_data = self.get_upload_url("images")?;
-        self.put_data(&upload_data.url, buf)?;
-        Ok(upload_data.uuid)
+    pub fn upload_img(&self, image_id: &str, buf: Vec<u8>) -> Result<(), SdkErr> {
+        let upload_res = self.get_image_upload_url(image_id)?;
+        self.put_data(&upload_res.url, buf)?;
+        Ok(())
     }
 
     /// Upload a image file to the /images/ route
@@ -185,29 +224,29 @@ impl Client {
     /// The image data can be either:
     /// * ELF file bytes
     /// * bincode encoded MemoryImage
-    pub fn upload_img_file(&self, path: &Path) -> Result<String> {
-        let upload_data = self.get_upload_url("images")?;
+    pub fn upload_img_file(&self, image_id: &str, path: &Path) -> Result<(), SdkErr> {
+        let upload_data = self.get_image_upload_url(image_id)?;
 
-        let fd = File::open(path).context("Unable to open supplied image file")?;
+        let fd = File::open(path)?;
         self.put_data(&upload_data.url, fd)?;
 
-        Ok(upload_data.uuid)
+        Ok(())
     }
 
     // - /inputs
 
     /// Upload a input buffer to the /inputs/ route
-    pub fn upload_input(&self, buf: Vec<u8>) -> Result<String> {
+    pub fn upload_input(&self, buf: Vec<u8>) -> Result<String, SdkErr> {
         let upload_data = self.get_upload_url("inputs")?;
         self.put_data(&upload_data.url, buf)?;
         Ok(upload_data.uuid)
     }
 
     /// Upload a input file to the /inputs/ route
-    pub fn upload_input_file(&self, path: &Path) -> Result<String> {
+    pub fn upload_input_file(&self, path: &Path) -> Result<String, SdkErr> {
         let upload_data = self.get_upload_url("inputs")?;
 
-        let fd = File::open(path).context("Unable to open supplied image file")?;
+        let fd = File::open(path)?;
         self.put_data(&upload_data.url, fd)?;
 
         Ok(upload_data.uuid)
@@ -217,9 +256,9 @@ impl Client {
 
     /// Create a new proof request Session
     ///
-    /// Supply the img_id and input_id created from uploading those files in
+    /// Supply the image_id and input_id created from uploading those files in
     /// previous steps
-    pub fn create_session(&self, img_id: String, input_id: String) -> Result<SessionId> {
+    pub fn create_session(&self, img_id: String, input_id: String) -> Result<SessionId, SdkErr> {
         let url = format!("{}/sessions/create", self.url);
 
         let req = ProofReq {
@@ -227,21 +266,14 @@ impl Client {
             input: input_id,
         };
 
-        let res = self
-            .client
-            .post(url)
-            .json(&req)
-            .send()
-            .context("Failed to submit session/create POST request")?;
+        let res = self.client.post(url).json(&req).send()?;
 
         if !res.status().is_success() {
             let body = res.text()?;
-            bail!("Request failed - server error: '{body}'");
+            return Err(SdkErr::InternalServerErr(body));
         }
 
-        let res: CreateSessRes = res
-            .json()
-            .context("Failed to deserialize Session status result")?;
+        let res: CreateSessRes = res.json()?;
 
         Ok(SessionId::new(res.uuid))
     }
@@ -251,14 +283,8 @@ impl Client {
     /// Download a given url to a buffer
     ///
     /// Useful to download a [SessionId] receipt_url
-    pub fn download(&self, url: &str) -> Result<Vec<u8>> {
-        let data = self
-            .client
-            .get(url)
-            .send()
-            .context("Failed to download url to buffer")?
-            .bytes()
-            .context("Failed to get raw bytes from download")?;
+    pub fn download(&self, url: &str) -> Result<Vec<u8>, SdkErr> {
+        let data = self.client.get(url).send()?.bytes()?;
 
         Ok(data.into())
     }
@@ -272,46 +298,7 @@ mod tests {
     use super::*;
 
     const TEST_KEY: &str = "TESTKEY";
-
-    fn run_upload_mock(route_path: &str) {
-        let data = vec![];
-
-        let server = MockServer::start();
-
-        let image_uuid = Uuid::new_v4();
-        let put_url = format!("http://{}/upload/{}", server.address(), image_uuid);
-        let response = UploadRes {
-            url: put_url,
-            uuid: image_uuid.to_string(),
-        };
-
-        let get_mock = server.mock(|when, then| {
-            when.method(GET)
-                .path(format!("/{route_path}/upload"))
-                .header("x-api-key", TEST_KEY);
-            then.status(200)
-                .header("content-type", "application/json")
-                .json_body_obj(&response);
-        });
-
-        let put_mock = server.mock(|when, then| {
-            when.method(PUT).path(format!("/upload/{}", image_uuid));
-            then.status(200);
-        });
-
-        let server_url = format!("http://{}", server.address());
-        let client = super::Client::from_parts(server_url, TEST_KEY.to_string())
-            .expect("Failed to construct client");
-        let res = if route_path == "images" {
-            client.upload_img(data).expect("Failed to upload img")
-        } else {
-            client.upload_input(data).expect("Failed to upload input")
-        };
-
-        assert_eq!(res, response.uuid);
-        get_mock.assert();
-        put_mock.assert();
-    }
+    const TEST_ID: &str = "0x5891b5b522d5df086d0ff0b110fbd9d21bb4fc7163af34d08286a2e846f6be03";
 
     #[test]
     fn client_from_parts() {
@@ -336,12 +323,102 @@ mod tests {
 
     #[test]
     fn image_upload() {
-        run_upload_mock("images");
+        let data = vec![];
+
+        let server = MockServer::start();
+
+        let put_url = format!("http://{}/upload/{TEST_ID}", server.address());
+        let response = ImgUploadRes { url: put_url };
+
+        let get_mock = server.mock(|when, then| {
+            when.method(GET)
+                .path(format!("/images/upload/{TEST_ID}"))
+                .header("x-api-key", TEST_KEY);
+            then.status(200)
+                .header("content-type", "application/json")
+                .json_body_obj(&response);
+        });
+
+        let put_mock = server.mock(|when, then| {
+            when.method(PUT).path(format!("/upload/{TEST_ID}"));
+            then.status(200);
+        });
+
+        let server_url = format!("http://{}", server.address());
+        let client = super::Client::from_parts(server_url, TEST_KEY.to_string())
+            .expect("Failed to construct client");
+        client
+            .upload_img(TEST_ID, data)
+            .expect("Failed to upload input");
+        get_mock.assert();
+        put_mock.assert();
+    }
+
+    #[test]
+    #[should_panic(expected = "value: ImageIdExists")]
+    fn image_upload_dup() {
+        let data = vec![];
+
+        let server = MockServer::start();
+
+        let put_url = format!("http://{}/upload/{TEST_ID}", server.address());
+        let response = ImgUploadRes { url: put_url };
+
+        server.mock(|when, then| {
+            when.method(GET)
+                .path(format!("/images/upload/{TEST_ID}"))
+                .header("x-api-key", TEST_KEY);
+            then.status(204).json_body_obj(&response);
+        });
+
+        server.mock(|when, then| {
+            when.method(PUT).path(format!("/upload/{TEST_ID}"));
+            then.status(200);
+        });
+
+        let server_url = format!("http://{}", server.address());
+        let client = super::Client::from_parts(server_url, TEST_KEY.to_string())
+            .expect("Failed to construct client");
+        client.upload_img(TEST_ID, data).unwrap()
     }
 
     #[test]
     fn input_upload() {
-        run_upload_mock("inputs");
+        env_logger::init();
+        let data = vec![];
+
+        let server = MockServer::start();
+
+        let input_uuid = Uuid::new_v4();
+        let put_url = format!("http://{}/upload/{}", server.address(), input_uuid);
+        let response = UploadRes {
+            url: put_url,
+            uuid: input_uuid.to_string(),
+        };
+
+        let get_mock = server.mock(|when, then| {
+            when.method(GET)
+                .path("/inputs/upload")
+                .header("x-api-key", TEST_KEY);
+            then.status(200)
+                .header("content-type", "application/json")
+                .json_body_obj(&response);
+        });
+
+        let put_mock = server.mock(|when, then| {
+            when.method(PUT).path(format!("/upload/{}", input_uuid));
+            then.status(200);
+        });
+
+        let server_url = format!("http://{}", server.address());
+        let client = super::Client::from_parts(server_url, TEST_KEY.to_string())
+            .expect("Failed to construct client");
+        let res = client.upload_input(data).expect("Failed to upload input");
+
+        assert_eq!(res, response.uuid);
+
+        get_mock.assert();
+        put_mock.assert();
     }
 
     #[test]
@@ -349,7 +426,7 @@ mod tests {
         let server = MockServer::start();
 
         let request = ProofReq {
-            img: Uuid::new_v4().to_string(),
+            img: TEST_ID.to_string(),
             input: Uuid::new_v4().to_string(),
         };
         let response = CreateSessRes {
