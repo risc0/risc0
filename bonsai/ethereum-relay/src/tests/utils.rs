9017662c401022179f7ee74821aedfd191d4528c
Victor Graf <victor@risczero.com>
2023-08-01 17:01:30 -0700

    remove unused test utility

--- bonsai/ethereum-relay/src/tests/utils.rs
+++ bonsai/ethereum-relay/src/tests/utils.rs
@@ -128,43 +128,4 @@ pub(crate) mod tests {
             Err(anyhow::anyhow!("terminate success").into())
         }
     }
-
-    use ethers::{
-        self,
-        providers::{JsonRpcClient, Middleware, Provider, PubsubClient},
-    };
-    use tokio_stream::Stream;
-
-    // This wrapper only exists to provide a nicer API and play well with the rest
-    // of the components.
-    pub(crate) struct EventStream<P: JsonRpcClient> {
-        filter: ethers::types::Filter,
-        provider: Provider<P>,
-    }
-
-    impl<P: JsonRpcClient> EventStream<P> {
-        pub(crate) fn new(provider: Provider<P>, address: ethers::types::Address) -> Self {
-            Self {
-                provider,
-                filter: ethers::types::Filter::new().address(address),
-            }
-        }
-
-        pub(crate) fn from_block(mut self, block: u64) -> Self {
-            self.filter = self.filter.from_block(block);
-            self
-        }
-    }
-
-    impl<P: JsonRpcClient> EventStream<P> {
-        pub(crate) async fn poll(&self) -> impl Stream<Item = ethers::types::Log> + '_ {
-            self.provider.watch(&self.filter).await.unwrap().stream()
-        }
-    }
-
-    impl<P: PubsubClient> EventStream<P> {
-        pub(crate) async fn watch(&self) -> impl Stream<Item = ethers::types::Log> + '_ {
-            self.provider.subscribe_logs(&self.filter).await.unwrap()
-        }
-    }
 }
