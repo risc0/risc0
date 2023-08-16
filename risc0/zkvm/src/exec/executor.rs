3ade8555e7d7474b897e58794ab44e06fde46f7f
Erik Kaneda <erik@risczero.com>
2023-08-15 17:50:01 -0700

    fix doc

--- risc0/zkvm/src/exec/executor.rs
+++ risc0/zkvm/src/exec/executor.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/// This module implements the Executor.
+//! This module implements the Executor.
 use std::{cell::RefCell, collections::HashMap, fmt::Debug, io::Write, mem::take, rc::Rc};
 
 use addr2line::{
