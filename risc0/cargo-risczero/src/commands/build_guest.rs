f556052e9c9af9b8250a479c37e9a41ad4bdd11c
Frank Laub <flaub@risc0.com>
2023-08-18 01:35:41 +0700

    Include tools in build-toolchain (#804)
    * Since Cargo.lock changed, update docker test image_ids
    * Use tempdir for Dockerfile
    * Use `docker` feature for tests
    * Export all files from docker build

--- risc0/cargo-risczero/src/commands/build_guest.rs
+++ risc0/cargo-risczero/src/commands/build_guest.rs
@@ -13,9 +13,8 @@
 // limitations under the License.
 
 use std::{
-    fs::{self, File},
-    io::Write,
-    path::PathBuf,
+    fs,
+    path::{Path, PathBuf},
     process::Command,
 };
 
@@ -24,7 +23,11 @@ use cargo_metadata::MetadataCommand;
 use clap::Parser;
 use docker_generate::DockerFile;
 use risc0_binfmt::{MemoryImage, Program};
-use risc0_zkvm_platform::{memory, memory::MEM_SIZE, PAGE_SIZE};
+use risc0_zkvm_platform::{
+    memory::{MEM_SIZE, TEXT_START},
+    PAGE_SIZE,
+};
+use tempfile::tempdir;
 
 use crate::utils::{ensure_binary, CommandExt};
 
@@ -34,42 +37,51 @@ const DOCKER_IGNORE: &str = r#"
 **/.git
 "#;
 
+const TARGET_DIR: &str = "target/riscv-guest/riscv32im-risc0-zkvm-elf/docker";
+
 /// `cargo risczero build`
 #[derive(Parser)]
 pub struct BuildGuest {
-    /// Location of the Cargo.toml of the guest code
+    /// Location of the Cargo.toml for the guest code.
     ///
-    /// This path is relative to the current directory
-    #[clap(value_parser, long)]
-    pub manifest_path: String,
+    /// This path is relative to the current directory.
+    #[arg(long)]
+    pub manifest_path: PathBuf,
 }
 
 impl BuildGuest {
     pub fn run(&self) -> Result<()> {
         let meta = MetadataCommand::new()
-            .manifest_path(self.manifest_path.as_str())
+            .manifest_path(&self.manifest_path)
             .exec()?;
-        let pkg_name = &meta
-            .root_package()
-            .context("failed to parse Cargo.toml")?
-            .name;
-        eprintln!("Building the riscv32im-risc0-zkvm-elf binary for {pkg_name}...");
+        let root_pkg = meta.root_package().context("failed to parse Cargo.toml")?;
+        let pkg_name = &root_pkg.name;
+
+        eprintln!("Building ELF binaries in {pkg_name} for riscv32im-risc0-zkvm-elf target...");
+
         ensure_binary("docker", &["--version"])?;
+
         if let Err(err) = self.check_cargo_lock() {
             eprintln!("{err}");
         }
-        let package_name = pkg_name.replace('-', "_");
-        self.create_dockerfile(package_name.as_str())?;
-        self.build()?;
-        self.clean()?;
 
-        let paths = fs::read_dir(&format!(
-            "./target/riscv-guest/riscv32im-risc0-zkvm-elf/docker/{package_name}/"
-        ))?;
+        let pkg_name = pkg_name.replace('-', "_");
+        {
+            let temp_dir = tempdir()?;
+            let temp_path = temp_dir.path();
+            self.create_dockerfile(temp_path, pkg_name.as_str())?;
+            self.build(temp_path)?;
+        }
+
+        let target_dir = PathBuf::from(TARGET_DIR);
         println!("ELFs ready at:");
-        for path in paths {
-            let entry = path.unwrap().path();
-            println!("{} - ImageID: {}", entry.display(), self.image_id(&entry)?);
+
+        for target in root_pkg.targets.iter() {
+            if target.is_bin() {
+                let elf_path = target_dir.join(&pkg_name).join(&target.name);
+                let image_id = self.compute_image_id(&elf_path)?;
+                println!("ImageID: {} - {:?}", image_id, elf_path);
+            }
         }
 
         Ok(())
@@ -78,44 +90,49 @@ impl BuildGuest {
     /// Create the dockerfile.
     ///
     /// Overwrites if a dockerfile already exists.
-    fn create_dockerfile(&self, pkg_name: &str) -> Result<()> {
-        let manifest_env = &[("CARGO_MANIFEST_PATH", self.manifest_path.as_str())];
-        let pkg_env = &[("PKG_NAME", pkg_name)];
-        let c_flags = format!(
-            "-C passes=loweratomic -C link-arg=-Ttext=0x{:08X} -C link-arg=--fatal-warnings",
-            memory::TEXT_START
+    fn create_dockerfile(&self, temp_dir: &Path, pkg_name: &str) -> Result<()> {
+        let manifest_env = &[("CARGO_MANIFEST_PATH", self.manifest_path.to_str().unwrap())];
+        let rustflags = format!(
+            "-C passes=loweratomic -C link-arg=-Ttext=0x{TEXT_START:08X} -C link-arg=--fatal-warnings",
         );
-        let c_flags_env = &[("RUSTFLAGS", c_flags.as_str())];
+        let rustflags_env = &[("RUSTFLAGS", rustflags.as_str())];
 
         let build = DockerFile::new()
             .from_alias("build", "risczero/risc0-guest-builder:v0.17")
             .workdir("/src")
             .copy(".", ".")
             .env(manifest_env)
-            .env(c_flags_env)
+            .env(rustflags_env)
+            .env(&[("CARGO_TARGET_DIR", "target")])
+            // Fetching separately allows docker to cache the downloads, assuming the Cargo.lock
+            // doesn't change.
             .run(
-                "cargo +risc0 fetch --target riscv32im-risc0-zkvm-elf --manifest-path $CARGO_MANIFEST_PATH --locked",
+                "cargo +risc0 fetch \
+                    --locked \
+                    --target riscv32im-risc0-zkvm-elf \
+                    --manifest-path $CARGO_MANIFEST_PATH",
             )
             .run(
-                "CARGO_TARGET_DIR=target \\\n\
-                \tcargo +risc0 build \\\n\
-                \t--locked \\\n\
-                \t--release \\\n\
-                \t--target riscv32im-risc0-zkvm-elf \\\n\
-                \t--manifest-path $CARGO_MANIFEST_PATH")
-            .run(r"find target/riscv32im-risc0-zkvm-elf/release -maxdepth 1 -type f -exec test -x {} \; -exec cp {} /tmp/ \;");
+                "cargo +risc0 build \
+                    --locked \
+                    --release \
+                    --target riscv32im-risc0-zkvm-elf \
+                    --manifest-path $CARGO_MANIFEST_PATH",
+            );
 
-        let binary: DockerFile<'_> = DockerFile::new()
-            .comment("binary stage")
-            .from_alias("binary", "scratch")
-            .env(pkg_env)
-            .copy_from("build", "/tmp", "/$PKG_NAME");
+        let out_dir = format!("/{pkg_name}");
+        let binary = DockerFile::new()
+            .comment("export stage")
+            .from_alias("export", "scratch")
+            .copy_from(
+                "build",
+                "/src/target/riscv32im-risc0-zkvm-elf/release",
+                out_dir.as_str(),
+            );
 
         let file = DockerFile::new().dockerfile(build).dockerfile(binary);
-        let mut dockerfile = File::create("Dockerfile")?;
-        dockerfile.write_all(file.to_string().as_bytes())?;
-        let mut dockerignore = File::create(".dockerignore")?;
-        dockerignore.write_all(DOCKER_IGNORE.as_bytes())?;
+        fs::write(temp_dir.join("Dockerfile"), file.to_string())?;
+        fs::write(temp_dir.join("Dockerfile.dockerignore"), DOCKER_IGNORE)?;
 
         Ok(())
     }
@@ -123,28 +140,19 @@ impl BuildGuest {
     /// Build the dockerfile and ouputs the ELF.
     ///
     /// Overwrites if an ELF with the same name already exists.
-    fn build(&self) -> Result<()> {
+    fn build(&self, temp_dir: &Path) -> Result<()> {
         Command::new("docker")
-            .args([
-                "build",
-                "--output=target/riscv-guest/riscv32im-risc0-zkvm-elf/docker/",
-                "--target=binary",
-                ".",
-            ])
-            .run_verbose()?;
-
-        Ok(())
-    }
-
-    /// Remove Dockerfile and .dockerignore files.
-    fn clean(&self) -> Result<()> {
-        Command::new("rm")
-            .args(["Dockerfile", ".dockerignore"])
+            .arg("build")
+            .arg(format!("--output={TARGET_DIR}"))
+            .arg("-f")
+            .arg(temp_dir.join("Dockerfile"))
+            .arg(".")
             .run_verbose()
     }
 
     fn check_cargo_lock(&self) -> Result<()> {
-        let lock_file = PathBuf::from(&self.manifest_path)
+        let lock_file = self
+            .manifest_path
             .parent()
             .context("invalid manifest path")?
             .join("Cargo.lock");
@@ -156,7 +164,7 @@ impl BuildGuest {
     }
 
     /// Compute the image ID for a given ELF.
-    fn image_id(&self, elf_path: &PathBuf) -> Result<String> {
+    fn compute_image_id(&self, elf_path: &Path) -> Result<String> {
         let elf = fs::read(elf_path)?;
         let program = Program::load_elf(&elf, MEM_SIZE as u32).context("unable to load elf")?;
         let image = MemoryImage::new(&program, PAGE_SIZE as u32)
@@ -165,65 +173,55 @@ impl BuildGuest {
     }
 }
 
+// requires Docker to be installed
+#[cfg(feature = "docker")]
 #[cfg(test)]
 mod test {
-    use std::{env, path::PathBuf};
+    use std::path::PathBuf;
 
-    use super::BuildGuest;
-
-    struct Elf {
-        path: String,
-        image_id: String,
-    }
+    use super::{BuildGuest, TARGET_DIR};
 
     struct Tester {
-        manifest_path: String,
-        elfs: Vec<Elf>,
+        builder: BuildGuest,
     }
 
     impl Tester {
-        fn run(&self) {
-            env::set_current_dir("../../").unwrap();
+        fn new(manifest_path: &str) -> Self {
+            std::env::set_current_dir("../../").unwrap();
             let builder = BuildGuest {
-                manifest_path: self.manifest_path.clone(),
+                manifest_path: PathBuf::from(manifest_path),
             };
             builder.run().unwrap();
-            for elf in self.elfs.iter() {
-                assert_eq!(
-                    builder.image_id(&PathBuf::from(&elf.path)).unwrap(),
-                    elf.image_id
-                );
-            }
+            Self { builder }
+        }
+
+        fn compare_image_id(&self, bin_path: &str, expected: &str) {
+            let target_dir = PathBuf::from(TARGET_DIR);
+            let elf_path = target_dir.join(bin_path);
+            let actual = self.builder.compute_image_id(&elf_path).unwrap();
+            assert_eq!(expected, actual);
         }
     }
+
+    // Test build reproducibility for risc0_zkvm_methods_guest.
+    // If the code of the package or any of its dependencies change,
+    // it may be required to recompute the expected image_ids.
+    // For that, run:
+    // `cargo risczero build --manifest-path risc0/zkvm/methods/guest/Cargo.toml`
     #[test]
-    #[ignore] // requires Docker to be installed
-              // Test build reproducibility for risc0_zkvm_methods_guest.
-              // If the code of the package or any of its dependencies change,
-              // it may be required to recompute the expected image_ids.
-              // For that, run:
-              // `cargo risczero build --manifest-path risc0/zkvm/methods/guest/Cargo.toml`
     fn test_reproducible_methods_guest() {
-        let zkvm_methods_guest = Tester {
-            manifest_path: "risc0/zkvm/methods/guest/Cargo.toml".to_string(),
-            elfs: vec![
-                Elf {
-                    path: "target/riscv-guest/riscv32im-risc0-zkvm-elf/docker/risc0_zkvm_methods_guest/hello_commit"
-                    .to_string(),
-                    image_id: "eb12f9b97d8759327f651afeb09ae9a5713e7dbc428284d453b8cf56e8dadd5a".to_string()
-                },
-                Elf {
-                    path: "target/riscv-guest/riscv32im-risc0-zkvm-elf/docker/risc0_zkvm_methods_guest/multi_test"
-                    .to_string(),
-                    image_id: "761900e766a4ae1d8edcb2b49dc9aee54b94e42c9b0d6421cfb112314c4e3efc".to_string(),
-                },
-                Elf {
-                    path: "target/riscv-guest/riscv32im-risc0-zkvm-elf/docker/risc0_zkvm_methods_guest/slice_io"
-                    .to_string(),
-                    image_id: "3f2ad1a2d500ab4ab927eebe241d872d3f598065b8987b182410cf01f350f74c".to_string()
-                }
-            ],
-        };
-        zkvm_methods_guest.run()
+        let tester = Tester::new("risc0/zkvm/methods/guest/Cargo.toml");
+        tester.compare_image_id(
+            "risc0_zkvm_methods_guest/hello_commit",
+            "eb12f9b97d8759327f651afeb09ae9a5713e7dbc428284d453b8cf56e8dadd5a",
+        );
+        tester.compare_image_id(
+            "risc0_zkvm_methods_guest/multi_test",
+            "44750696d3d67caaaf9b1ca9441d1ffe5d5b9370d6945ce56b1972c10e42da59",
+        );
+        tester.compare_image_id(
+            "risc0_zkvm_methods_guest/slice_io",
+            "3f2ad1a2d500ab4ab927eebe241d872d3f598065b8987b182410cf01f350f74c",
+        );
     }
 }
