7faa59cb203b59e2ba83fd85e7007ab62f45546d
Rami Lukata <32602478+rlukata@users.noreply.github.com>
2023-08-18 12:28:29 -0700

    Update risc0/zkp/src/verify/mod.rs
    Co-authored-by: Frank Laub <flaub@risc0.com>

--- risc0/zkp/src/verify/mod.rs
+++ risc0/zkp/src/verify/mod.rs
@@ -46,7 +46,7 @@ pub enum VerificationError {
 
 impl fmt::Debug for VerificationError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        Display::fmt(&self, f)
+        fmt::Display::fmt(&self, f)
     }
 }
 
