2292906cdb4f12128a68857591ff2a7bf609c022
Erik Kaneda <erik@risczero.com>
2023-09-19 14:16:10 -0700

    fix

--- bonsai/rest-api-mock/src/prover.rs
+++ bonsai/rest-api-mock/src/prover.rs
@@ -102,7 +102,9 @@ impl Prover {
                         anyhow::anyhow!("failed to build executor environment: {:?}", e)
                     })?;
                 let mut exec = Executor::new(env, mem_img)?;
-                let session = exec.run().context("Executor failed to generate a successful session)?;
+                let session = exec
+                    .run()
+                    .context("Executor failed to generate a successful session")?;
 
                 let receipt = Receipt {
                     inner: InnerReceipt::Fake,
