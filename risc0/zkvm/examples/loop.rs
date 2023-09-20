63aa82092a5749ff1fb9074545c7de3b06fc05ff
capossele <angelocapossele@gmail.com>
2023-09-20 13:23:34 +0100

    add bench lopp workflow

--- risc0/zkvm/examples/loop.rs
+++ risc0/zkvm/examples/loop.rs
@@ -26,6 +26,15 @@ use risc0_zkvm_methods::{
 };
 use tracing_subscriber::{prelude::*, EnvFilter};
 
+#[derive(serde::Serialize, Debug)]
+struct PerformanceData {
+    cycles: String,
+    duration: String,
+    ram: String,
+    seal: String,
+    speed: String,
+}
+
 #[derive(Parser)]
 struct Args {
     /// Number of iterations.
@@ -36,12 +45,17 @@ struct Args {
     #[arg(short = 'f', long)]
     hashfn: Option<String>,
 
-    /// Specify the segment po2
+    /// Specify the segment po2.
     #[arg(short, long, default_value_t = 20)]
     po2: usize,
 
+    /// Don't print results.
     #[arg(long, short)]
     quiet: bool,
+
+    /// Print results in json format.
+    #[arg(long, short)]
+    json: bool,
 }
 
 fn main() {
@@ -78,42 +92,74 @@ fn main() {
         let throughput = (cycles as f64) / duration.as_secs_f64();
 
         if !args.quiet {
-            println!(
-                "| {:>9}k | {:>10} | {:>10} | {:>10} | {:>8}hz |",
-                cycles / 1024,
-                duration.human_duration().to_string(),
-                usage.human_count_bytes().to_string(),
-                seal.human_count_bytes().to_string(),
-                throughput.human_count_bare().to_string()
-            );
+            if args.json {
+                let entry = PerformanceData {
+                    cycles: format!("{:}k", cycles / 1024),
+                    duration: duration.human_duration().to_string(),
+                    ram: usage.human_count_bytes().to_string(),
+                    seal: seal.human_count_bytes().to_string(),
+                    speed: format!("{:}Hz", throughput.human_count_bare().to_string()),
+                };
+                match serde_json::to_string_pretty(&entry) {
+                    Ok(json_str) => print!("{json_str}"),
+                    Err(e) => println!("Error serializing to JSON: {}", e),
+                }
+            } else {
+                println!(
+                    "| {:>9}k | {:>10} | {:>10} | {:>10} | {:>8}hz |",
+                    cycles / 1024,
+                    duration.human_duration().to_string(),
+                    usage.human_count_bytes().to_string(),
+                    seal.human_count_bytes().to_string(),
+                    throughput.human_count_bare().to_string()
+                );
+            }
         }
     } else {
-        println!(
-            "| {:>10} | {:>10} | {:>10} | {:>10} | {:>10} |",
-            "Cycles", "Duration", "RAM", "Seal", "Speed"
-        );
+        if args.json {
+            println!("[");
+        } else {
+            println!(
+                "| {:>10} | {:>10} | {:>10} | {:>10} | {:>10} |",
+                "Cycles", "Duration", "RAM", "Seal", "Speed"
+            );
+        }
 
-        for iterations in [
-            0,           // warm-up
-            1,           // 16, 64K
-            4 * 1024,    // 17, 128K
-            16 * 1024,   // 18, 256K
-            32 * 1024,   // 19, 512K
-            64 * 1024,   // 20, 1M
-            200 * 1024,  // 21, 2M
-            400 * 1024,  // 22, 4M
-            900 * 1024,  // 23, 8M
-            1400 * 1024, // 24, 16M
-        ] {
-            run_with_iterations(iterations, args.po2);
+        let input = [
+            0usize, // warm-up
+            1,      // 16, 64K
+            4 * 1024, // 17, 128K
+                    // 16 * 1024, // 18, 256K
+                    // 32 * 1024, // 19, 512K
+                    // 64 * 1024, // 20, 1M
+                    // 200 * 1024,  // 21, 2M
+                    // 400 * 1024,  // 22, 4M
+                    // 900 * 1024,  // 23, 8M
+                    // 1400 * 1024, // 24, 16M
+        ];
+        let len = input.len();
+
+        for (index, &iteration) in input.iter().enumerate() {
+            run_with_iterations(iteration, args.po2, args.json);
+
+            if args.json {
+                if index == len - 1 {
+                    println!("\n]");
+                } else if index != 0 {
+                    println!(",");
+                }
+            }
         }
     }
 }
 
-fn run_with_iterations(iterations: usize, po2: usize) {
+fn run_with_iterations(iterations: usize, po2: usize, json: bool) {
     let mut cmd = Command::new(std::env::current_exe().unwrap());
     if iterations == 0 {
         cmd.arg("--quiet");
+    }
+    if json {
+        cmd.arg("--json");
     }
     let ok = cmd
         .arg("--iterations")
