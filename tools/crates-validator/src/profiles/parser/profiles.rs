94f1d24b39419dcc87277134f6ed7b95ea41f93c
Matheus <cardosaum@pm.me>
2023-09-22 23:53:29 +0700

    refactor: Improve profiles parser

--- tools/crates-validator/src/profiles/parser/profiles.rs
+++ tools/crates-validator/src/profiles/parser/profiles.rs
@@ -2,52 +2,52 @@ use anyhow::{anyhow, ensure, Context, Result};
 use serde_valid::Validate;
 use serde_yaml::{Mapping, Sequence, Value};
 
-use crate::profiles::helpers::{ProfileVariant, ToProfile};
+use crate::profiles::helpers::ProfileVariant;
 use crate::profiles::Profile;
 
-pub(crate) fn parse(config: &Value) -> Result<ProfileVariant> {
-    match config.get("crates") {
-        None => config.clone().to_profile(),
-        Some(Value::Mapping(m)) => m.clone().to_profile(),
-        Some(Value::Sequence(s)) => s.clone().to_profile(),
-        _ => Err(anyhow!("Invalid profile: {:?}", config)),
+pub(crate) trait ParseProfile {
+    fn parse(self) -> Result<ProfileVariant>;
+}
+
+impl ParseProfile for Value {
+    fn parse(self) -> Result<ProfileVariant> {
+        match self {
+            Value::Mapping(m) => m.parse(),
+            Value::Sequence(s) => s.parse(),
+            _ => Err(anyhow!("Invalid profile type: {self:?}")),
+        }
     }
 }
 
-impl ToProfile for Mapping {
-    fn to_profile(self) -> Result<ProfileVariant> {
-        dbg!(&self);
-        let profile = serde_yaml::from_value::<Profile>(Value::Mapping(self.clone()))
-            .context("Failed to parse profile")?;
-        ensure!(profile.validate().is_ok(), "Invalid profile: {:?}", profile);
-        Ok(ProfileVariant::Single(profile))
-    }
-}
-
-impl ToProfile for Sequence {
-    fn to_profile(self) -> Result<ProfileVariant> {
+impl ParseProfile for Sequence {
+    fn parse(self) -> Result<ProfileVariant> {
         let profiles = self
             .into_iter()
-            .map(|p| p.to_profile())
+            .map(|p| p.parse())
             .collect::<Result<Vec<ProfileVariant>>>()?
             .into();
         Ok(profiles)
     }
 }
 
-impl ToProfile for Value {
-    fn to_profile(self) -> Result<ProfileVariant> {
-        match self {
-            Value::Mapping(mapping) => mapping.to_profile(),
-            Value::Sequence(sequence) => sequence.to_profile(),
-            t => Err(anyhow!("Invalid profile type: {t:?}")),
+impl ParseProfile for Mapping {
+    fn parse(self) -> Result<ProfileVariant> {
+        match self.get("crates") {
+            None => parse_item(self.into()),
+            Some(t) => t.clone().parse(),
         }
     }
 }
 
+fn parse_item(item: Value) -> Result<ProfileVariant> {
+    let profile = serde_yaml::from_value::<Profile>(item).context("Failed to parse profile")?;
+    ensure!(profile.validate().is_ok(), "Invalid profile: {:?}", profile);
+    Ok(ProfileVariant::Single(profile))
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::profiles::{ProfileSettings, parser::utils};
+    use crate::profiles::{parser::utils, ProfileSettings, PATH_YAML_CONFIG};
 
     use super::*;
     use rstest::*;
@@ -60,16 +60,20 @@ mod tests {
     }
 
     fn to_yaml(profiles: Vec<Option<&str>>) -> Value {
-        profiles.into_iter().map(get_profile).map(|p| {
-            p.and_then(|p| serde_yaml::to_value(p).ok())
-                .unwrap_or(Value::Null)
-        }).collect()
+        profiles
+            .into_iter()
+            .map(get_profile)
+            .map(|p| {
+                p.and_then(|p| serde_yaml::to_value(p).ok())
+                    .unwrap_or(Value::Null)
+            })
+            .collect()
     }
 
     #[test]
     fn can_read_config() {
-        let config = utils::read_profile("profiles/profiles.yaml").unwrap();
-        let profiles: Vec<Profile> = parse(&config).unwrap().into();
+        let config = utils::read_profile(PATH_YAML_CONFIG).unwrap();
+        let profiles: Vec<Profile> = config.parse().unwrap().into();
         assert!(!profiles.is_empty());
     }
 
@@ -81,7 +85,7 @@ mod tests {
     #[case::correct_file(to_yaml(vec![Some("example")]))]
     #[case::correct_file(to_yaml(vec![Some("example1"), Some("example2")]))]
     fn can_parse_config(#[case] config: Value) {
-        let profiles: Vec<Profile> = parse(&config).unwrap().into();
+        let profiles: Vec<Profile> = dbg!(config).parse().unwrap().into();
         assert!(!profiles.is_empty());
     }
 }
