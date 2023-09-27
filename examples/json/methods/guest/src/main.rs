e1be0babd1683bf149f3bce9714be8d5740b583f
Roy Rotstein <112157037+Roee-87@users.noreply.github.com>
2023-09-27 14:19:04 -0700

    Update examples/json/methods/guest/src/main.rs
    Co-authored-by: Frank Laub <flaub@risc0.com>

--- examples/json/methods/guest/src/main.rs
+++ examples/json/methods/guest/src/main.rs
@@ -32,6 +32,6 @@ pub fn main() {
         data: proven_val,
         hash: sha,
     };
-    println!("totayl cycle count: {}", env::get_cycle_count());
+    println!("total cycle count: {}", env::get_cycle_count());
     env::commit(&out);
 }
