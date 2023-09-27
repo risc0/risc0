a65616e24e0da3524c6f458595b2a87703ac7d71
Roy Rotstein <royrotstein@Roys-MacBook-Pro-2.local>
2023-09-27 13:59:39 -0700

    fixed typos

--- examples/smartcore-ml/methods/guest/src/main.rs
+++ examples/smartcore-ml/methods/guest/src/main.rs
@@ -1,10 +1,43 @@
 #![no_main]
-
-
 use risc0_zkvm::guest::env;
+use smartcore::linalg::basic::matrix::DenseMatrix;
+use smartcore::tree::decision_tree_classifier::*;
+use rmp_serde::from_slice;
+
 
 risc0_zkvm::guest::entry!(main);
 
 pub fn main() {
-    // TODO: Implement your guest code here
+    // Import the trained model and the input data.  First we read in the length of the model and data arrays.
+    let model_length: usize = env::read();
+    let data_length: usize = env::read();
+
+    // Next, we construct buffers for the model and the input data using the corresponding lengths of their respective byte arrays.
+    let mut model_bytes: Vec<u8> = vec![0;model_length];
+    let mut data_bytes: Vec<u8> = vec![0;data_length];
+
+   // The byte arrays are read in from the host
+    env::read_slice(&mut model_bytes);
+    env::read_slice(&mut data_bytes);
+    
+    // Deserialize the model bytes array into a Forust-ML GradientBooster object.  We MUST explicitly declare the type in order for deserialization to be successful.
+    let trained_model: DecisionTreeClassifier<f64, u32, DenseMatrix<f64>, Vec<u32>>  = from_slice(&model_bytes).expect("Unable to deserialize byte array");
+
+    // Deserialize the input data bytes array into a DenseMatrix.
+    let x_data: DenseMatrix<f64> = from_slice(&data_bytes).expect("Unable to deserialize byte array");    
+
+    // We call the predict() function on our trained model to perform inference.  
+    let y_hat = trained_model.predict(&x_data).unwrap();
+
+    //  This line is optional and can be commented out, but it's useful to see the output of the computation before the proving step begins.
+    println!("answer: {:#?}", &y_hat);
+    
+    // This is also optional, though it' quite useful for benchmarking the various operations in the guest code.  
+    // env::get_cycle_count() can be called anywhere in the guest, multiple times.  So if we are interested in knowing how many
+    // cycles the inference computation takes, we can calculate total cycles before and after model.predict() and the difference between 
+    // the two values equals the total cycle count for that section of the guest code.
+    println!("Total cycles for guest code execution: {}", env::get_cycle_count());
+
+    // We commit the output to the journal.
+    env::commit(&y_hat);
 }
