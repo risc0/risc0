f14630aaa34132ccd08b3abd55fe06494ce895b0
Blaine Malone <blainemalone01@gmail.com>
2023-10-02 19:56:09 -0400

    fix: Added Private input struct for zk_multisig.

--- examples/multisig/methods/guest/src/bin/zk_multisig.rs
+++ examples/multisig/methods/guest/src/bin/zk_multisig.rs
@@ -16,34 +16,26 @@
 
 use hex::FromHex;
 use k256::ecdsa::{RecoveryId, Signature, VerifyingKey};
+use multisig_core::PrivateInput;
 use risc0_zkvm::guest::env;
 use rs_merkle::{algorithms::Sha256, Hasher, MerkleProof, MerkleTree};
+use serde::{Deserialize, Serialize};
 use sha3::{Digest, Keccak256};
-// use serde::{Deserialize, Serialize};
 
 risc0_zkvm::guest::entry!(main);
 
 const TRUSTED_MERKLE_ROOT: &str =
     "1962fbc17a4440d383f544724e9a943773c0cc6eade89b66b724c31cf33d3f62";
 
-// TODO: Use encapsulation to simplify arguments passed to guest code.
-// #[derive(Debug, Serialize, Deserialize)]
-// pub struct PrivateInput {
-//     pub signature_one_input: String,
-//     pub signature_two_input: String,
-//     pub merkle_proof_one_input: String,
-//     pub merkle_proof_two_input: String,
-//     pub signed_message_input: String,
-//     pub all_leaves_input: String
-// }
-
 pub fn main() {
-    let signature_one_input: String = env::read(); // <ecdsa_sig_address_one + recovery_id>
-    let signature_two_input: String = env::read(); // <ecdsa_sig_address_two + recovery_id>
-    let merkle_proof_one_input: String = env::read(); // <proof_hashes + index_of_leaf>
-    let merkle_proof_two_input: String = env::read(); // <proof_hashes + index_of_leaf>
-    let signed_message_input: String = env::read(); // <signed_message>
-    let all_leaves_input: String = env::read(); // <merkle_leaves>
+    let private_input: PrivateInput = env::read();
+    
+    let signature_one_input: String = private_input.signature_one_input; // <ecdsa_sig_address_one + recovery_id>
+    let signature_two_input: String = private_input.signature_two_input; // <ecdsa_sig_address_two + recovery_id>
+    let merkle_proof_one_input: String = private_input.merkle_proof_one_input; // <proof_hashes + index_of_leaf>
+    let merkle_proof_two_input: String = private_input.merkle_proof_two_input; // <proof_hashes + index_of_leaf>
+    let signed_message_input: String = private_input.signed_message_input; // <signed_message>
+    let all_leaves_input: String = private_input.all_leaves_input; // <merkle_leaves>
 
     let all_leaves_bytes =
         Vec::<u8>::from_hex(all_leaves_input.trim()).expect("Failed to decode hex string");
@@ -208,133 +200,136 @@ pub fn main() {
 
     let signed_message = Keccak256::new_with_prefix(signed_message_input);
 
-    // Recover address of first signature hash it and check if it's in the tree - use the index from the merkle proof to do this
-    let sig_one_rec_id_input = signature_one_input.chars().last().unwrap();
-    let sig_one_rec_id_str = sig_one_rec_id_input.to_string();
-    let sig_one_rec_id = sig_one_rec_id_str.parse::<u32>().unwrap() as u8;
-    let sanitized_signature_one_input = &signature_one_input[..signature_one_input.len() - 1];
+    // // Recover address of first signature hash it and check if it's in the tree - use the index from the merkle proof to do this
+    // let sig_one_rec_id_input = signature_one_input.chars().last().unwrap();
+    // let sig_one_rec_id_str = sig_one_rec_id_input.to_string();
+    // let sig_one_rec_id = sig_one_rec_id_str.parse::<u32>().unwrap() as u8;
+    // let sanitized_signature_one_input = &signature_one_input[..signature_one_input.len() - 1];
 
-    let signature_one_bytes = hex::decode(sanitized_signature_one_input).expect("Invalid hex string");
-    let signature = Signature::try_from(signature_one_bytes.as_slice());
+    // let signature_one_bytes =
+    //     hex::decode(sanitized_signature_one_input).expect("Invalid hex string");
+    // let signature = Signature::try_from(signature_one_bytes.as_slice());
 
-    let signature_res = match signature {
-        Ok(value) => value,
-        Err(e) => {
-            println!("❌ Failed to get signature: {:?}", e);
-            return;
-        }
-    };
+    // let signature_res = match signature {
+    //     Ok(value) => value,
+    //     Err(e) => {
+    //         println!("❌ Failed to get signature: {:?}", e);
+    //         return;
+    //     }
+    // };
 
-    let recid = RecoveryId::try_from(sig_one_rec_id);
-    let recid_res = match recid {
-        Ok(value) => value,
-        Err(e) => {
-            println!("❌ Failed to get RecoveryId: {:?}", e);
-            return;
-        }
-    };
+    // let recid = RecoveryId::try_from(sig_one_rec_id);
+    // let recid_res = match recid {
+    //     Ok(value) => value,
+    //     Err(e) => {
+    //         println!("❌ Failed to get RecoveryId: {:?}", e);
+    //         return;
+    //     }
+    // };
 
-    let recovered_key = VerifyingKey::recover_from_digest(
-        signed_message.clone(),
-        &signature_res,
-        recid_res,
-    );
+    // let recovered_key =
+    //     VerifyingKey::recover_from_digest(signed_message.clone(), &signature_res, recid_res);
 
-    let recovered_key_res = match recovered_key {
-        Ok(value) => value,
-        Err(e) => {
-            println!("❌ Failed to get RecoveredKey: {:?}", e);
-            return;
-        }
-    };
+    // let recovered_key_res = match recovered_key {
+    //     Ok(value) => value,
+    //     Err(e) => {
+    //         println!("❌ Failed to get RecoveredKey: {:?}", e);
+    //         return;
+    //     }
+    // };
 
-    let encoded_point_pub_key_one = recovered_key_res.to_encoded_point(false);
-    let encoded_point_pub_key_one_hex = encoded_point_pub_key_one.to_string().to_lowercase();
-    // let log_public_key_one = "Recovered Public Key One: ".to_owned() + &encoded_point_pub_key_one_hex;
-    // env::log(&log_public_key_one);
+    // let encoded_point_pub_key_one = recovered_key_res.to_encoded_point(false);
+    // let encoded_point_pub_key_one_hex = encoded_point_pub_key_one.to_string().to_lowercase();
+    // // let log_public_key_one = "Recovered Public Key One: ".to_owned() + &encoded_point_pub_key_one_hex;
+    // // env::log(&log_public_key_one);
 
-    let pubic_key_one_hash = Sha256::hash(encoded_point_pub_key_one_hex.as_bytes());
-    let pubic_key_one_hash_hex = hex::encode(pubic_key_one_hash);
-    let log_public_key_hash_one = "Recovered Public Key Hash One: ".to_owned() + &pubic_key_one_hash_hex;
-    env::log(&log_public_key_hash_one);
+    // let pubic_key_one_hash = Sha256::hash(encoded_point_pub_key_one_hex.as_bytes());
+    // let pubic_key_one_hash_hex = hex::encode(pubic_key_one_hash);
+    // let log_public_key_hash_one =
+    //     "Recovered Public Key Hash One: ".to_owned() + &pubic_key_one_hash_hex;
+    // env::log(&log_public_key_hash_one);
 
-    let actual_leaf_one = hex::encode(leaves[merkle_proof_one_index_value]);
-    if  actual_leaf_one != pubic_key_one_hash_hex {
-        let log_msg = "❌ Recovered public key from signature_one is not a signatory: ".to_owned() + &encoded_point_pub_key_one_hex;
-        env::log(&log_msg);
-        return;
-    } else {
-        let log_msg = "🎉 Recovered public key is a signatory: ".to_owned() + &encoded_point_pub_key_one_hex;
-        env::log(&log_msg);
-    }
+    // let actual_leaf_one = hex::encode(leaves[merkle_proof_one_index_value]);
+    // if actual_leaf_one != pubic_key_one_hash_hex {
+    //     let log_msg = "❌ Recovered public key from signature_one is not a signatory: ".to_owned()
+    //         + &encoded_point_pub_key_one_hex;
+    //     env::log(&log_msg);
+    //     return;
+    // } else {
+    //     let log_msg =
+    //         "🎉 Recovered public key is a signatory: ".to_owned() + &encoded_point_pub_key_one_hex;
+    //     env::log(&log_msg);
+    // }
 
-     // Recover address of second signature hash it and check if it's in the tree - use the index from the merkle proof to do this
-     let sig_two_rec_id_input = signature_two_input.chars().last().unwrap();
-     let sig_two_rec_id_str = sig_two_rec_id_input.to_string();
-     let sig_two_rec_id = sig_two_rec_id_str.parse::<u32>().unwrap() as u8;
-     let sanitized_signature_two_input = &signature_two_input[..signature_two_input.len() - 1];
+    // // Recover address of second signature hash it and check if it's in the tree - use the index from the merkle proof to do this
+    // let sig_two_rec_id_input = signature_two_input.chars().last().unwrap();
+    // let sig_two_rec_id_str = sig_two_rec_id_input.to_string();
+    // let sig_two_rec_id = sig_two_rec_id_str.parse::<u32>().unwrap() as u8;
+    // let sanitized_signature_two_input = &signature_two_input[..signature_two_input.len() - 1];
 
-     let signature_two_bytes = hex::decode(sanitized_signature_two_input).expect("Invalid hex string");
-     let signature = Signature::try_from(signature_two_bytes.as_slice());
+    // let signature_two_bytes =
+    //     hex::decode(sanitized_signature_two_input).expect("Invalid hex string");
+    // let signature = Signature::try_from(signature_two_bytes.as_slice());
 
-     let signature_res = match signature {
-         Ok(value) => value,
-         Err(e) => {
-             println!("❌ Failed to get signature: {:?}", e);
-             return;
-         }
-     };
+    // let signature_res = match signature {
+    //     Ok(value) => value,
+    //     Err(e) => {
+    //         println!("❌ Failed to get signature: {:?}", e);
+    //         return;
+    //     }
+    // };
 
-     let recid = RecoveryId::try_from(sig_two_rec_id);
-     let recid_res = match recid {
-         Ok(value) => value,
-         Err(e) => {
-             println!("❌ Failed to get RecoveryId: {:?}", e);
-             return;
-         }
-     };
+    // let recid = RecoveryId::try_from(sig_two_rec_id);
+    // let recid_res = match recid {
+    //     Ok(value) => value,
+    //     Err(e) => {
+    //         println!("❌ Failed to get RecoveryId: {:?}", e);
+    //         return;
+    //     }
+    // };
 
-     let recovered_key = VerifyingKey::recover_from_digest(
-         signed_message.clone(),
-         &signature_res,
-         recid_res,
-     );
+    // let recovered_key =
+    //     VerifyingKey::recover_from_digest(signed_message.clone(), &signature_res, recid_res);
 
-     let recovered_key_res = match recovered_key {
-         Ok(value) => value,
-         Err(e) => {
-             println!("❌ Failed to get RecoveredKey: {:?}", e);
-             return;
-         }
-     };
+    // let recovered_key_res = match recovered_key {
+    //     Ok(value) => value,
+    //     Err(e) => {
+    //         println!("❌ Failed to get RecoveredKey: {:?}", e);
+    //         return;
+    //     }
+    // };
 
-     let encoded_point_pub_key_two = recovered_key_res.to_encoded_point(false);
-     let encoded_point_pub_key_two_hex = encoded_point_pub_key_two.to_string().to_lowercase();
+    // let encoded_point_pub_key_two = recovered_key_res.to_encoded_point(false);
+    // let encoded_point_pub_key_two_hex = encoded_point_pub_key_two.to_string().to_lowercase();
 
-     let pubic_key_two_hash = Sha256::hash(encoded_point_pub_key_two_hex.as_bytes());
-     let pubic_key_two_hash_hex = hex::encode(pubic_key_two_hash);
-     let log_public_key_hash_two = "Recovered Public Key Hash Two: ".to_owned() + &pubic_key_two_hash_hex;
-     env::log(&log_public_key_hash_two);
+    // let pubic_key_two_hash = Sha256::hash(encoded_point_pub_key_two_hex.as_bytes());
+    // let pubic_key_two_hash_hex = hex::encode(pubic_key_two_hash);
+    // let log_public_key_hash_two =
+    //     "Recovered Public Key Hash Two: ".to_owned() + &pubic_key_two_hash_hex;
+    // env::log(&log_public_key_hash_two);
 
-     let actual_leaf_two = hex::encode(leaves[merkle_proof_two_index_value]);
-     if  actual_leaf_two != pubic_key_two_hash_hex {
-         let log_msg = "❌ Recovered public key from signature_two is not a signatory: ".to_owned() + &encoded_point_pub_key_two_hex;
-         env::log(&log_msg);
-         return;
-     } else {
-        let log_msg = "🎉 Recovered public key is a signatory: ".to_owned() + &encoded_point_pub_key_one_hex;
-        env::log(&log_msg);
-    }
+    // let actual_leaf_two = hex::encode(leaves[merkle_proof_two_index_value]);
+    // if actual_leaf_two != pubic_key_two_hash_hex {
+    //     let log_msg = "❌ Recovered public key from signature_two is not a signatory: ".to_owned()
+    //         + &encoded_point_pub_key_two_hex;
+    //     env::log(&log_msg);
+    //     return;
+    // } else {
+    //     let log_msg =
+    //         "🎉 Recovered public key is a signatory: ".to_owned() + &encoded_point_pub_key_one_hex;
+    //     env::log(&log_msg);
+    // }
 
-     // Check that both recovered keys are not the same
-     if pubic_key_one_hash_hex == pubic_key_two_hash_hex {
-        let log_msg = "Cannot provide two signatures from the same signatory: ".to_owned() + &pubic_key_one_hash_hex;
-        env::log(&log_msg);
-        return;
-     } else {
-        let log_msg = "\n\n🎉 Congratulations, you've satisfied the multisig quorum. Now generating proof... 🎉\n ".to_owned();
-        env::log(&log_msg);
-     }
+    // // Check that both recovered keys are not the same
+    // if pubic_key_one_hash_hex == pubic_key_two_hash_hex {
+    //     let log_msg = "Cannot provide two signatures from the same signatory: ".to_owned()
+    //         + &pubic_key_one_hash_hex;
+    //     env::log(&log_msg);
+    //     return;
+    // } else {
+    //     let log_msg = "\n\n🎉 Congratulations, you've satisfied the multisig quorum. Now generating proof... 🎉\n ".to_owned();
+    //     env::log(&log_msg);
+    // }
 
     let final_hash = signed_message.finalize();
     let final_hash_bytes = final_hash.as_slice();
--- examples/multisig/src/main.rs
+++ examples/multisig/src/main.rs
@@ -13,36 +13,41 @@
 // limitations under the License.
 
 use multisig_methods::{ZK_MULTISIG_ELF, ZK_MULTISIG_ID};
-use risc0_zkvm::{
-    default_prover,
-    serde::{from_slice, to_vec},
-    sha::Digest,
-    ExecutorEnv, Receipt,
-};
+use risc0_zkvm::{default_prover, serde, sha::Digest, ExecutorEnv, Receipt};
+
+use multisig_core::PrivateInput;
 
 fn is_multisig_quorum_satisfied(
-    signature_one: &str,
-    signature_two: &str,
-    merkle_proof_one: &str,
-    merkle_proof_two: &str,
-    transaction_hash: &str,
-    all_leaves: &str,
+    signature_one_input: &str,
+    signature_two_input: &str,
+    merkle_proof_one_input: &str,
+    merkle_proof_two_input: &str,
+    transaction_hash_input: &str,
+    all_leaves_input: &str,
 ) -> Result<(Digest, Receipt), String> {
+    let input = PrivateInput {
+        signature_one_input: signature_one_input.to_string(),
+        signature_two_input: signature_two_input.to_string(),
+        merkle_proof_one_input: merkle_proof_one_input.to_string(),
+        merkle_proof_two_input: merkle_proof_two_input.to_string(),
+        signed_message_input: transaction_hash_input.to_string(),
+        all_leaves_input: all_leaves_input.to_string(),
+    };
+
+    let input_bytes_result = serde::to_vec(&input);
+    if let Err(err) = input_bytes_result {
+        return Err(err.to_string());
+    }
+    
     let env = ExecutorEnv::builder()
-        .add_input(&to_vec(signature_one).unwrap())
-        .add_input(&to_vec(signature_two).unwrap())
-        .add_input(&to_vec(merkle_proof_one).unwrap())
-        .add_input(&to_vec(merkle_proof_two).unwrap())
-        .add_input(&to_vec(transaction_hash).unwrap())
-        .add_input(&to_vec(all_leaves).unwrap())
+        .add_input(input_bytes_result.unwrap().as_slice())
         .build()
         .unwrap();
 
     let prover = default_prover();
-
     let receipt = prover.prove_elf(env, ZK_MULTISIG_ELF).unwrap();
 
-    match from_slice::<Vec<u8>, _>(&receipt.journal) {
+    match serde::from_slice::<Vec<u8>, _>(&receipt.journal) {
         Ok(digest_arg) => {
             let digest = digest_arg.try_into().unwrap();
             Ok((digest, receipt))
@@ -92,66 +97,59 @@ fn main() {
 mod tests {
     use multisig_methods::ZK_MULTISIG_ID;
 
-    const SIGNATURE_ONE: &str = "b3828c6eff4f8e90296b6ca2844534269c74643dd4b2dc3b628b5f3b8efab20f6e6542ba028d31a1a5cd55c8d1ebeda7efe010c1091f2034fee01327be94855a0";
-    const SIGNATURE_TWO: &str = "55ee88d8a8d8b9263b429fedec257e65a29268e99e3d3cbf423db1e7c100779c5900dae93a0979c4172b2c262b553e8aa11908f744315ca3782bca86235a6fbc1";
-    const MERKLE_PROOF_ONE: &str = "e4083e5df4eef0b44a0e9d561ba9bd2ad76a2be3d021b7f2f3725e525352e988f6884e6b2078e95a3142df3d428978ecc0a4142facc5fb5e902933fe3a3b6e327f24885f6fad72d78165a75c88f961258d1b0f51910c1719dd8d6bac27a438cf0";
-    const MERKLE_PROOF_TWO: &str = "65be2cac622d5019dc04889aec418a2e9c724c456e91e43835b116350a72a42efa15a8b74d6795f59cf6fe1e3e2a1fc78c7ed024aae23976503aa4f2d4f7dc0d7f24885f6fad72d78165a75c88f961258d1b0f51910c1719dd8d6bac27a438cf3";
+    const SIGNATURE_ONE: &str = "b3828c6eff4f8e90296b6ca2844534269c74643dd4b2dc3b628b5f3b8efab20f6e6542ba028d31a1a5cd55c8d1ebeda7efe010c1091f2034fee01327be94855a";
+    const SIGNATURE_ONE_REC_ID: &str = "0";
+    const SIGNATURE_TWO: &str = "55ee88d8a8d8b9263b429fedec257e65a29268e99e3d3cbf423db1e7c100779c5900dae93a0979c4172b2c262b553e8aa11908f744315ca3782bca86235a6fbc";
+    const SIGNATURE_TWO_REC_ID: &str = "1";
+    const MERKLE_PROOF_ONE: &str = "e4083e5df4eef0b44a0e9d561ba9bd2ad76a2be3d021b7f2f3725e525352e988f6884e6b2078e95a3142df3d428978ecc0a4142facc5fb5e902933fe3a3b6e327f24885f6fad72d78165a75c88f961258d1b0f51910c1719dd8d6bac27a438cf";
+    const MERKLE_PROOF_ONE_INDEX: &str = "0";
+    const MERKLE_PROOF_TWO: &str = "65be2cac622d5019dc04889aec418a2e9c724c456e91e43835b116350a72a42efa15a8b74d6795f59cf6fe1e3e2a1fc78c7ed024aae23976503aa4f2d4f7dc0d7f24885f6fad72d78165a75c88f961258d1b0f51910c1719dd8d6bac27a438cf";
+    const MERKLE_PROOF_TWO_INDEX: &str = "3";
     const ALL_LEAVES: &str = "2ded6691f90f8ee99fd9bd72d39b35fd829d8c6654cda03ec709a02674aa334ae4083e5df4eef0b44a0e9d561ba9bd2ad76a2be3d021b7f2f3725e525352e98865be2cac622d5019dc04889aec418a2e9c724c456e91e43835b116350a72a42e1b5810aeb251583acc192df9638a3204798fb95126e91185a39c36728b83eb2cc5304c10ee245490ee283ebc082cbcfaad838858f4b2e24150c927ff475a2f0b4b53708aa80dacfc89b939d2944e42a2561515c58a995f255ba80844afb93fd8";
     const TRANSACTION_DATA: &str = "{\"this\": \"would\", \"contain\": \"transaction\", \"data\": \"in\", \"practice\": \"maybe\", \"an\": \"erc4337\", \"user\": \"operation\"}";
 
     #[test]
-    fn verify_multisig_successful() { // This test takes ~35-40mins to execute - faster with 'metal' feature flag. 
-        match super::is_multisig_quorum_satisfied(
-            SIGNATURE_ONE,
-            SIGNATURE_TWO,
-            MERKLE_PROOF_ONE,
-            MERKLE_PROOF_TWO,
+    fn verify_multisig_successful() {
+        // This test takes ~35-40mins to execute - faster with 'metal' feature flag.
+        let result = super::is_multisig_quorum_satisfied(
+            &(SIGNATURE_ONE.to_string() + SIGNATURE_ONE_REC_ID),
+            &(SIGNATURE_TWO.to_string() + SIGNATURE_TWO_REC_ID),
+            &(MERKLE_PROOF_ONE.to_string() + MERKLE_PROOF_ONE_INDEX),
+            &(MERKLE_PROOF_TWO.to_string() + MERKLE_PROOF_TWO_INDEX),
             TRANSACTION_DATA,
             ALL_LEAVES,
-        ) {
-            Ok((digest, receipt)) => match receipt.verify(ZK_MULTISIG_ID) {
-                Ok(_) => {
-                    assert_eq!(
-                            hex::encode(digest.as_bytes()),
-                            "6f1237d5c028319a116a3f3cf774b9f64c6fee243432653132547c3c87e7c485",
-                            "The hash should match the Keccak256 hash of the transactions data to be approved."
-                        );
-                }
-                Err(_err) => {
-                    assert!(false);
-                }
-            },
-            Err(_err) => {
-                assert!(false);
-            }
+        );
+
+        if let Err(_err) = result {
+            assert!(false);
+        } else {
+            let (digest, _receipt) = result.unwrap();
+            assert_eq!(
+                hex::encode(digest.as_bytes()),
+                "6f1237d5c028319a116a3f3cf774b9f64c6fee243432653132547c3c87e7c485",
+                "The hash should match the Keccak256 hash of the transactions data to be approved."
+            );
         }
     }
 
     #[test]
-    fn verify_multisig_failure() { // Same signatory can't provide two signatures.
-        match super::is_multisig_quorum_satisfied(
+    fn verify_multisig_failure() {
+        // Same signatory can't provide two signatures.
+        let result = super::is_multisig_quorum_satisfied(
             SIGNATURE_ONE,
             SIGNATURE_ONE,
             MERKLE_PROOF_ONE,
             MERKLE_PROOF_ONE,
             TRANSACTION_DATA,
             ALL_LEAVES,
-        ) {
-            Ok((digest, receipt)) => match receipt.verify(ZK_MULTISIG_ID) {
-                Ok(_) => {
-                    println!("shouldn't have got here!");
-                    println!("The data that Keccak256 hashes to {} was signed by 2 unique members of the multisig.", digest);
-                    assert!(false);
-                }
-                Err(_err) => {
-                    assert!(false);
-                }
-            },
-            Err(err) => {
-                eprintln!("{}", err);
-                assert_eq!(err, "Verification failed");
-                assert!(true);
-            }
+        );
+
+        if let Err(err) = result {
+            assert_eq!(err, "Verification failed");
+        } else {
+            let (_digest, receipt) = result.unwrap();
+            let verification_result = receipt.verify(ZK_MULTISIG_ID);
+            assert!(verification_result.is_err());
         }
     }
 }
