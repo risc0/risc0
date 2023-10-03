0b35d6819fbf809d331c08a341f6aa2eebb7dd4d
Matheus <cardosaum@pm.me>
2023-10-03 23:56:44 +0700

    chore: Add copyright headers

--- tools/crates-validator/src/gen_profiles/mod.rs
+++ tools/crates-validator/src/gen_profiles/mod.rs
@@ -1,5 +1,18 @@
-// TODO(Cardosaum): Check how to export only the functions that are needed
+// Copyright 2023 RISC Zero, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
+// TODO(Cardosaum): Check how to export only the functions that are needed
 use std::{
     collections::{btree_map::Entry, BTreeMap, HashMap, HashSet},
     fs::File,
