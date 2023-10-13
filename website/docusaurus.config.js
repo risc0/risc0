af3bad5706ec28bf9d96ad4c9c995ef5f4d0b451
Frank Laub <frank@risczero.com>
2023-10-12 22:43:29 -0700

    poke

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,5 +1,5 @@
 // @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
+// Note: type annotations allow type checking and IDEs autocompletion!
 
 const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
