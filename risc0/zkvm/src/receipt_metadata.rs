677da5cebf05b2ce6d2066ce42ebb39a9cc55f3d
Victor Graf <victor@risczero.com>
2023-10-17 15:32:19 -0700

    add explaination to MaybePruned

--- risc0/zkvm/src/receipt_metadata.rs
+++ risc0/zkvm/src/receipt_metadata.rs
@@ -31,134 +31,6 @@ use crate::{
     SystemState,
 };
 
-/// A value that may either be the source value directly, or a hash digest of
-/// the value.
-#[derive(Clone, Deserialize, Serialize)]
-pub enum MaybePruned<T>
-where
-    T: Clone + Serialize,
-{
-    /// Unpruned value.
-    Value(T),
-    /// Pruned value, which is a hash [Digest] of the value.
-    Pruned(Digest),
-}
-
-impl<T> MaybePruned<T>
-where
-    T: Clone + Serialize,
-{
-    /// Unwrap the value, or return an error.
-    pub fn value(self) -> Result<T, PrunedValueError> {
-        match self {
-            MaybePruned::Value(value) => Ok(value),
-            MaybePruned::Pruned(digest) => Err(PrunedValueError(digest)),
-        }
-    }
-
-    /// Unwrap the value as a reference, or return an error.k
-    pub fn as_value(&self) -> Result<&T, PrunedValueError> {
-        match self {
-            MaybePruned::Value(ref value) => Ok(value),
-            MaybePruned::Pruned(ref digest) => Err(PrunedValueError(digest.clone())),
-        }
-    }
-}
-
-impl<T> From<T> for MaybePruned<T>
-where
-    T: Clone + Serialize,
-{
-    fn from(value: T) -> Self {
-        Self::Value(value)
-    }
-}
-
-impl<T> Digestible for MaybePruned<T>
-where
-    T: Digestible + Clone + Serialize,
-{
-    fn digest(&self) -> Digest {
-        match self {
-            MaybePruned::Value(ref val) => val.digest(),
-            MaybePruned::Pruned(digest) => digest.clone(),
-        }
-    }
-}
-
-impl<T> Default for MaybePruned<T>
-where
-    T: Digestible + Default + Clone + Serialize,
-{
-    fn default() -> Self {
-        MaybePruned::Value(Default::default())
-    }
-}
-
-impl<T> MaybePruned<Option<T>>
-where
-    T: Clone + Serialize,
-{
-    /// Returns true is the value is None, or the value is pruned as the zero
-    /// digest.
-    pub fn is_none(&self) -> bool {
-        match self {
-            MaybePruned::Value(Some(_)) => false,
-            MaybePruned::Value(None) => true,
-            MaybePruned::Pruned(digest) => digest == &Digest::zero(),
-        }
-    }
-
-    /// Returns true is the value is Some(_), or the value is pruned as a
-    /// non-zero digest.
-    pub fn is_some(&self) -> bool {
-        !self.is_none()
-    }
-}
-
-#[cfg(test)]
-impl<T> PartialEq for MaybePruned<T>
-where
-    T: Clone + Serialize + PartialEq,
-{
-    fn eq(&self, other: &Self) -> bool {
-        match (self, other) {
-            (Self::Value(a), Self::Value(b)) => a == b,
-            (Self::Pruned(a), Self::Pruned(b)) => a == b,
-            _ => false,
-        }
-    }
-}
-
-impl<T> fmt::Debug for MaybePruned<T>
-where
-    T: Clone + Serialize + risc0_binfmt::Digestible + fmt::Debug,
-{
-    /// Format [MaybePruned] values are if they were a struct with value and
-    /// digest fields. Digest field is always provided so that divergent
-    /// trees of [MaybePruned] values can be compared.
-    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut builder = fmt.debug_struct("MaybePruned");
-        if let MaybePruned::Value(value) = self {
-            builder.field("value", value);
-        }
-        builder.field("digest", &self.digest()).finish()
-    }
-}
-
-/// Error returned when the source value was pruned, and is not available.
-#[derive(Debug, Clone)]
-pub struct PrunedValueError(pub Digest);
-
-impl fmt::Display for PrunedValueError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "value is pruned: {}", &self.0)
-    }
-}
-
-#[cfg(feature = "std")]
-impl std::error::Error for PrunedValueError {}
-
 /// Data associated with a receipt which is used for both input and
 /// output of global state.
 #[derive(Clone, Debug, Serialize, Deserialize)]
@@ -443,6 +315,140 @@ impl MaybePruned<Assumptions> {
         }
     }
 }
+
+/// Either a source value of a hash [Digest] of the source value.
+///
+/// This type supports creating "Merkle-ized structs". Each field of a Merkle-ized struct can have
+/// either the full value, or it can be "pruned" and replaced with a digest committing to that
+/// value. One way to think of this is as a special Merkle tree of a predefined shape. Each field
+/// is a child node. Any field/node in the tree can be opened by providing the Merkle inclusion
+/// proof. When a subtree is pruned, the digest commits to the value of all contained fields.
+/// [ReceiptMetadata] is the motivating example of this type of Merkle-ized struct.
+#[derive(Clone, Deserialize, Serialize)]
+pub enum MaybePruned<T>
+where
+    T: Clone + Serialize,
+{
+    /// Unpruned value.
+    Value(T),
+    /// Pruned value, which is a hash [Digest] of the value.
+    Pruned(Digest),
+}
+
+impl<T> MaybePruned<T>
+where
+    T: Clone + Serialize,
+{
+    /// Unwrap the value, or return an error.
+    pub fn value(self) -> Result<T, PrunedValueError> {
+        match self {
+            MaybePruned::Value(value) => Ok(value),
+            MaybePruned::Pruned(digest) => Err(PrunedValueError(digest)),
+        }
+    }
+
+    /// Unwrap the value as a reference, or return an error.k
+    pub fn as_value(&self) -> Result<&T, PrunedValueError> {
+        match self {
+            MaybePruned::Value(ref value) => Ok(value),
+            MaybePruned::Pruned(ref digest) => Err(PrunedValueError(digest.clone())),
+        }
+    }
+}
+
+impl<T> From<T> for MaybePruned<T>
+where
+    T: Clone + Serialize,
+{
+    fn from(value: T) -> Self {
+        Self::Value(value)
+    }
+}
+
+impl<T> Digestible for MaybePruned<T>
+where
+    T: Digestible + Clone + Serialize,
+{
+    fn digest(&self) -> Digest {
+        match self {
+            MaybePruned::Value(ref val) => val.digest(),
+            MaybePruned::Pruned(digest) => digest.clone(),
+        }
+    }
+}
+
+impl<T> Default for MaybePruned<T>
+where
+    T: Digestible + Default + Clone + Serialize,
+{
+    fn default() -> Self {
+        MaybePruned::Value(Default::default())
+    }
+}
+
+impl<T> MaybePruned<Option<T>>
+where
+    T: Clone + Serialize,
+{
+    /// Returns true is the value is None, or the value is pruned as the zero
+    /// digest.
+    pub fn is_none(&self) -> bool {
+        match self {
+            MaybePruned::Value(Some(_)) => false,
+            MaybePruned::Value(None) => true,
+            MaybePruned::Pruned(digest) => digest == &Digest::zero(),
+        }
+    }
+
+    /// Returns true is the value is Some(_), or the value is pruned as a
+    /// non-zero digest.
+    pub fn is_some(&self) -> bool {
+        !self.is_none()
+    }
+}
+
+#[cfg(test)]
+impl<T> PartialEq for MaybePruned<T>
+where
+    T: Clone + Serialize + PartialEq,
+{
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Self::Value(a), Self::Value(b)) => a == b,
+            (Self::Pruned(a), Self::Pruned(b)) => a == b,
+            _ => false,
+        }
+    }
+}
+
+impl<T> fmt::Debug for MaybePruned<T>
+where
+    T: Clone + Serialize + risc0_binfmt::Digestible + fmt::Debug,
+{
+    /// Format [MaybePruned] values are if they were a struct with value and
+    /// digest fields. Digest field is always provided so that divergent
+    /// trees of [MaybePruned] values can be compared.
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut builder = fmt.debug_struct("MaybePruned");
+        if let MaybePruned::Value(value) = self {
+            builder.field("value", value);
+        }
+        builder.field("digest", &self.digest()).finish()
+    }
+}
+
+/// Error returned when the source value was pruned, and is not available.
+#[derive(Debug, Clone)]
+pub struct PrunedValueError(pub Digest);
+
+impl fmt::Display for PrunedValueError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "value is pruned: {}", &self.0)
+    }
+}
+
+#[cfg(feature = "std")]
+impl std::error::Error for PrunedValueError {}
 
 // TODO(victor): Add tests that show every combination of pruned struct returns
 // the same digest. Check that the digest of an empty assumptions list is all
