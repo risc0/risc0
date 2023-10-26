cadd4ae8105e52940b6af36cbba80bede0dd0b77
Erik Kaneda <erik@risczero.com>
2023-10-26 16:39:41 -0700

    template: add input to guest and write to journal from guest

--- templates/rust-starter/methods/guest/src/main.rs
+++ templates/rust-starter/methods/guest/src/main.rs
@@ -10,4 +10,12 @@ risc0_zkvm::guest::entry!(main);
 
 pub fn main() {
     // TODO: Implement your guest code here
+
+    // read the input
+    let input: u32 = env::read();
+
+    // TODO: do something with the input
+
+    // write public output to the journal
+    env::commit(&input);
 }
