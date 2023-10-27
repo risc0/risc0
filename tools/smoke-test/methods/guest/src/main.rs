c4729e75d81121641325a72c9804a616d1ce394c
Frank Laub <flaub@risc0.com>
2023-10-27 22:51:19 +0000

    Fix missing LFS issue
    * Add smoke-test that can be used for e2e integration testing of binary installations
    
    TODO: add to CI workflow

---
+++ tools/smoke-test/methods/guest/src/main.rs
@@ -0,0 +1,32 @@
+// Copyright 2023 RISC Zero, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#![no_main]
+#![no_std]
+
+use core::hint::black_box;
+use risc0_zkvm::guest::env;
+
+risc0_zkvm::guest::entry!(main);
+
+pub fn main() {
+    let iterations: u32 = env::read();
+
+    let mut sum = 0;
+    for _ in 0..iterations {
+        black_box(sum += 1);
+    }
+
+    env::commit(&sum);
+}
