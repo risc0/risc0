99b529b738230f0af97d2a733ea3a40568ab0249
Frank Laub <frank@risczero.com>
2023-11-01 21:20:06 -0700

    yarn fmt

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -76,15 +76,15 @@ export default async function createConfigAsync() {
         {
           createRedirects(path) {
             if (path.includes("/api/bonsai")) {
-              return [path.replace("/api/bonsai", "/bonsai"),]
+              return [path.replace("/api/bonsai", "/bonsai")];
             }
             if (path.includes("/api/zkvm")) {
-              return [path.replace("/api/zkvm", "/zkvm")]
+              return [path.replace("/api/zkvm", "/zkvm")];
             }
             return undefined;
           },
-        }
-      ]
+        },
+      ],
     ],
 
     stylesheets: [
