c4448b17d6de34a3d828d21c63dfec0c550d3a1d
Angelo Capossele <angelocapossele@gmail.com>
2023-11-04 02:49:47 +0000

    Fix benchmarks (#1089)
    Refactor benchmarks after the changes introduced by #1055 
    
    Resolves #1013
    
    ---------
    
    Co-authored-by: Victor Graf <victor@risczero.com>
    Co-authored-by: Erik Kaneda <erik@risczero.com>
    Co-authored-by: Frank Laub <flaub@risc0.com>
    Co-authored-by: Matheus Cardoso <45436839+Cardosaum@users.noreply.github.com>
    Co-authored-by: Victor Graf <nategraf1@gmail.com>

--- benchmarks/src/benches/fibonacci.rs
+++ benchmarks/src/benches/fibonacci.rs
@@ -12,24 +12,26 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
-use risc0_zkvm::{ExecutorEnv, ExitCode, MemoryImage, Receipt, Session};
+use risc0_zkvm::{sha::DIGEST_WORDS, ExecutorEnv, ExecutorImpl, MemoryImage, Receipt, Session};
 
-use crate::{exec_compute, get_image, Benchmark};
+use crate::{get_cycles, get_image, Benchmark};
 
-pub struct Job<'a> {
+pub struct Job {
     pub spec: u32,
-    pub env: ExecutorEnv<'a>,
     pub image: MemoryImage,
-    pub session: Session,
+    pub session: Option<Session>,
 }
 
-pub fn new_jobs() -> Vec<<Job<'static> as Benchmark>::Spec> {
+pub fn new_jobs() -> Vec<<Job as Benchmark>::Spec> {
     vec![10, 50, 90]
 }
 
-impl Benchmark for Job<'_> {
+const METHOD_ID: [u32; DIGEST_WORDS] = risc0_benchmark_methods::FIBONACCI_ID;
+const METHOD_PATH: &'static str = risc0_benchmark_methods::FIBONACCI_PATH;
+
+impl Benchmark for Job {
     const NAME: &'static str = "fibonacci";
     type Spec = u32;
     type ComputeOut = u64;
@@ -46,26 +48,19 @@ impl Benchmark for Job<'_> {
     fn proof_size_bytes(proof: &Self::ProofType) -> u32 {
         (proof
             .inner
-            .flat()
+            .composite()
             .unwrap()
+            .segments
             .iter()
             .fold(0, |acc, segment| acc + segment.get_seal_bytes().len())) as u32
     }
 
     fn new(spec: Self::Spec) -> Self {
-        let image = get_image(risc0_benchmark_methods::FIBONACCI_PATH);
-
-        let env = ExecutorEnv::builder()
-            .write(&spec)
-            .unwrap()
-            .build()
-            .unwrap();
-
-        let session = Session::new(vec![], vec![], ExitCode::Halted(0));
+        let image = get_image(METHOD_PATH);
+        let session = None;
 
         Job {
             spec,
-            env,
             image,
             session,
         }
@@ -86,20 +81,29 @@ impl Benchmark for Job<'_> {
     }
 
     fn exec_compute(&mut self) -> (u32, u32, Duration) {
-        let (cycles, insn_cycles, elapsed, session) =
-            exec_compute(self.image.clone(), self.env.clone());
-        self.session = session;
-        (cycles, insn_cycles, elapsed)
+        let env = ExecutorEnv::builder()
+            .write(&self.spec)
+            .unwrap()
+            .build()
+            .unwrap();
+        let mut exec = ExecutorImpl::new(env, self.image.clone()).unwrap();
+        let start = Instant::now();
+        let session = exec.run().unwrap();
+        let elapsed = start.elapsed();
+        let segments = session.resolve().unwrap();
+        let (exec_cycles, prove_cycles) = get_cycles(segments);
+        self.session = Some(session);
+        (prove_cycles as u32, exec_cycles as u32, elapsed)
     }
 
     fn guest_compute(&mut self) -> (Self::ComputeOut, Self::ProofType) {
-        let receipt = self.session.prove().expect("receipt");
+        let receipt = self.session.as_ref().unwrap().prove().expect("receipt");
         let result = receipt.journal.decode().unwrap();
         (result, receipt)
     }
 
     fn verify_proof(&self, _output: &Self::ComputeOut, proof: &Self::ProofType) -> bool {
-        let result = proof.verify(risc0_benchmark_methods::FIBONACCI_ID);
+        let result = proof.verify(METHOD_ID);
 
         match result {
             Ok(_) => true,
