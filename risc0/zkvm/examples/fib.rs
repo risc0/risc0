a4e3953048b39ab30381591ada4d3bd30f1798fa
Victor Graf <victor@risczero.com>
2023-11-07 13:12:01 -0800

    remove unsused dep

--- risc0/zkvm/examples/fib.rs
+++ risc0/zkvm/examples/fib.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::rc::Rc;
-
 use clap::Parser;
 use risc0_zkvm::{
     get_prover_server, ExecutorEnv, ExecutorImpl, ProverOpts, ProverServer, VerifierContext,
