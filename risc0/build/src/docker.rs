d55365fee6491ee5828cfb32b6153693e5e3cfb4
Matheus <cardosaum@pm.me>
2023-11-09 01:08:01 +0700

    Update image ID in risc0_zkvm_methods_guest/multi_test

--- risc0/build/src/docker.rs
+++ risc0/build/src/docker.rs
@@ -233,7 +233,7 @@ mod test {
         build("../../risc0/zkvm/methods/guest/Cargo.toml");
         compare_image_id(
             "risc0_zkvm_methods_guest/multi_test",
-            "b3108007988a3f1b5d33d5f1dd327b9c498a34d00eb60753f21b4dfe97700c9b",
+            "56d94c5ac3b5d9968dba89b3b01f417adfcc93eb69549ec6422a89ba9b4b2bcc",
         );
         compare_image_id(
             "risc0_zkvm_methods_guest/hello_commit",
