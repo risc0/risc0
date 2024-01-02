a83c6372f9791805aaf91d109a920ae9db335f37
Erik Kaneda <erik@risczero.com>
2024-01-02 10:25:47 -0800

    Update copyright to 2024
    Happy New Year! I've omitted the circuit from this and will make a separate
    update after updating WIP and bootstrapping.

--- xtask/src/bootstrap_fault.rs
+++ xtask/src/bootstrap_fault.rs
@@ -1,4 +1,4 @@
-// Copyright 2023 RISC Zero, Inc.
+// Copyright 2024 RISC Zero, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -30,7 +30,7 @@ impl BootstrapFault {
         );
 
         let rust_code = format!(
-            r##"// Copyright 2023 RISC Zero, Inc.
+            r##"// Copyright 2024 RISC Zero, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
