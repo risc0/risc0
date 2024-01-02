44e78c44769632134ae9672905d191320cdcd166
Erik Kaneda <erik@risczero.com>
2024-01-02 12:48:04 -0800

    Update copyright to 2024 (#1287)
    Happy New Year! I've omitted the circuit from this PR but I will make a
    separate update after updating WIP and bootstrapping. It looks like tests in
    doc-test/version-0.18 is failing due to the `risc0-zkvm-methods` crate
    failing to build. I'm going to omit changes to that directory for the time being.

--- xtask/src/bootstrap.rs
+++ xtask/src/bootstrap.rs
@@ -1,4 +1,4 @@
-// Copyright 2023 RISC Zero, Inc.
+// Copyright 2024 RISC Zero, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -129,7 +129,7 @@ impl Bootstrap {
         tracing::info!("writing control ids to {CONTROL_ID_PATH_RECURSION}");
         let mut cntlf = std::fs::File::create(CONTROL_ID_PATH_RECURSION).unwrap();
         let license = r#"
-        // Copyright 2023 RISC Zero, Inc.
+        // Copyright 2024 RISC Zero, Inc.
         //
         // Licensed under the Apache License, Version 2.0 (the "License");
         // you may not use this file except in compliance with the License.
