02caaaa1a6ccd39a02fd5d379e01065eb8d282da
Tim Zerrell <tim.zerrell@risczero.com>
2024-01-12 22:30:41 -0800

    Fix Euclidean algorithm mul type

--- risc0/core/src/inverse.rs
+++ risc0/core/src/inverse.rs
@@ -49,21 +49,21 @@ impl<T: Elem> ExtensionField<T> {
         c
     }
 
-    fn mul(&self, a: &[T], b: &[T]) -> Vec<T> {
-        let mut c = self.naive_mul(a, b);
+    // fn mul(&self, a: &[T], b: &[T]) -> Vec<T> {
+    //     let mut c = self.naive_mul(a, b);
 
-        // Reduce the degree using the irreducible polynomial
-        let upper = 2 * self.degree - 2;
-        for i in (self.degree..=upper).rev() {
-            let x = c[i];
-            for j in 0..self.degree {
-                c[i - self.degree] += x * self.irreducible[j];
-            }
-            c[i] = T::ZERO;
-        }
-        c.truncate(self.degree);
-        c
-    }
+    //     // Reduce the degree using the irreducible polynomial
+    //     let upper = 2 * self.degree - 2;
+    //     for i in (self.degree..=upper).rev() {
+    //         let x = c[i];
+    //         for j in 0..self.degree {
+    //             c[i - self.degree] += x * self.irreducible[j];
+    //         }
+    //         c[i] = T::ZERO;
+    //     }
+    //     c.truncate(self.degree);
+    //     c
+    // }
 
     fn mul_elem(&self, a: T, b: &[T]) -> Vec<T> {
         let mut c = self.zero();
@@ -82,10 +82,10 @@ impl<T: Elem> ExtensionField<T> {
 
         while Self::degree(&r1) > 0 {
             let quot = self.div(&r0, &r1);
-            let r2 = f.sub(&r0, &f.mul(&quot, &r1));
+            let r2 = f.sub(&r0, &f.naive_mul(&quot, &r1));
             r0 = r1;
             r1 = r2;
-            let t2 = f.sub(&t0, &f.mul(&quot, &t1));
+            let t2 = f.sub(&t0, &f.naive_mul(&quot, &t1));
             t0 = t1;
             t1 = t2;
         }
