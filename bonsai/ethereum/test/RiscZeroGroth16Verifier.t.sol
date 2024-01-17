1451010ffcb30de8e1dedea11b70882e4bebbbb0
capossele <angelocapossele@gmail.com>
2024-01-17 00:25:27 +0000

    fmt

--- bonsai/ethereum/test/RiscZeroGroth16Verifier.t.sol
+++ bonsai/ethereum/test/RiscZeroGroth16Verifier.t.sol
@@ -55,10 +55,7 @@ contract RiscZeroGroth16VerifierTest is Test {
     IRiscZeroVerifier internal verifier;
 
     function setUp() external {
-        verifier = new RiscZeroGroth16Verifier(
-            ControlID.CONTROL_ID_0,
-            ControlID.CONTROL_ID_1
-        );
+        verifier = new RiscZeroGroth16Verifier(ControlID.CONTROL_ID_0, ControlID.CONTROL_ID_1);
     }
 
     function testVerifyKnownGoodReceipt() external view {
