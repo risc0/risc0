496eb8362c14cdeae9f3c1c869416adb67acd297
Erik Kaneda <erik@risczero.com>
2024-01-31 18:32:12 -0800

    reproducible builds: update dockerfile to use new toolchain
    This change adds approperiate changes to download a newer version of
    cargo-risczero and to build using a newer toolchain version.

--- risc0/build/src/docker.rs
+++ risc0/build/src/docker.rs
@@ -128,7 +128,7 @@ fn create_dockerfile(
     .join(" ");
 
     let build = DockerFile::new()
-        .from_alias("build", "risczero/risc0-guest-builder:v0.17")
+        .from_alias("build", "risczero/risc0-guest-builder:v2024-01-31.1")
         .workdir("/src")
         .copy(".", ".")
         .env(manifest_env)
