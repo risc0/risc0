d3c8b2698b9df5cb4a48f1609bbf536b4f9840a2
Tim Zerrell <tim.zerrell@risczero.com>
2024-02-07 23:56:03 +0000

    Add compress step
    Co-authored-by: capossele <angelocapossele@gmail.com>

--- xtask/src/bootstrap_groth16.rs
+++ xtask/src/bootstrap_groth16.rs
@@ -205,7 +205,8 @@ fn generate_receipt() -> (Receipt, Digest) {
     let prover = get_prover_server(&opts).unwrap();
     let receipt = prover.prove_session(&ctx, &session).unwrap();
     let claim = receipt.get_claim().unwrap();
-    let succinct_receipt = receipt.inner.succinct().unwrap();
+    let composite_receipt = receipt.inner.composite().unwrap();
+    let succinct_receipt = prover.compress(composite_receipt).unwrap();
     let journal = session.journal.unwrap().bytes;
 
     tracing::info!("identity_p254");
