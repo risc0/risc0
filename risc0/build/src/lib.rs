ad185c7846bd87e032893248834d862358276b2e
Victor Graf <victor@risczero.com>
2024-02-15 14:55:52 -0800

    Revert "make guest-list always-on"
    This reverts commit b6483880a775b1d82149f722d5aaffa401ca6b2d.

--- risc0/build/src/lib.rs
+++ risc0/build/src/lib.rs
@@ -50,6 +50,7 @@ impl Risc0Metadata {
 }
 
 /// Represents an item in the generated list of compiled guest binaries
+#[cfg(feature = "guest-list")]
 #[derive(Debug, Clone)]
 pub struct GuestListEntry<'a> {
     /// The name of the guest binary
@@ -109,6 +110,7 @@ pub const {upper}_PATH: &str = r#"{elf_path}"#;
         )
     }
 
+    #[cfg(feature = "guest-list")]
     fn guest_list_entry(&self) -> String {
         let upper = self.name.to_uppercase().replace('-', "_");
         format!(
@@ -532,7 +534,9 @@ pub fn embed_methods_with_options(
     let methods_path = out_dir.join("methods.rs");
     let mut methods_file = File::create(&methods_path).unwrap();
 
+    #[cfg(feature = "guest-list")]
     let mut guest_list_entries = Vec::new();
+    #[cfg(feature = "guest-list")]
     methods_file
         .write_all(b"use risc0_build::GuestListEntry;\n")
         .unwrap();
@@ -568,10 +572,12 @@ pub fn embed_methods_with_options(
                 .write_all(method.rust_def().as_bytes())
                 .unwrap();
 
+            #[cfg(feature = "guest-list")]
             guest_list_entries.push(method.guest_list_entry());
             out_methods.push(method);
         }
     }
+    #[cfg(feature = "guest-list")]
     methods_file
         .write_all(
             format!(
