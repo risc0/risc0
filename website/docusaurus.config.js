5b961d3a2fefed93a06b406d2de4c0babfe8d03c
Paul <paul@risc0.com>
2024-02-29 08:42:25 -0800

    prettier

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -83,9 +83,7 @@ export default async function createConfigAsync() {
             }
             return undefined;
           },
-          redirects: [
-            { from: "/tech_faq", to: "/faq" },
-          ],
+          redirects: [{ from: "/tech_faq", to: "/faq" }],
         },
       ],
     ],
