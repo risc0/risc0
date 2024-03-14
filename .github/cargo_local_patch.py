dcd0dd90448ef9d192ad656978c02d839d78ea5e
capossele <angelocapossele@gmail.com>
2024-03-14 18:07:06 +0000

    use dep mapping

--- .github/cargo_local_patch.py
+++ .github/cargo_local_patch.py
@@ -10,23 +10,14 @@ def modify_dependency(details, new_path):
         details.pop('rev', None)
         details.pop('branch', None)
 
-def process_dependencies(dependencies, base_path):
+def process_dependencies(dependencies, base_path, dep_path_mapping):
     for dep, details in dependencies.items():
-        prefix = None
-        path = None
-        if dep.startswith("risc0-ethereum"):
-            prefix = "risc0-ethereum"
-            path = os.path.join(base_path, "../risc0-ethereum")
-        elif dep.startswith("risc0"):
-            prefix = "risc0"
-            path = os.path.join(base_path,"../risc0/risc0")
-        elif dep.startswith("bonsai"):
-            prefix = "bonsai"
-            path = os.path.join(base_path,"../risc0/bonsai")
-
-        if prefix:
-            new_path = os.path.join(path, dep.replace(prefix + '-', ''))
+        # Check if the current dependency is in our mapping
+        if dep in dep_path_mapping:
+            # Construct the new path using the base_path and the relative path from the mapping
+            new_path = os.path.join(base_path, dep_path_mapping[dep])
             modify_dependency(details, new_path)
+
 
 def calculate_base_path(cargo_toml_path):
     depth = len(os.path.normpath(cargo_toml_path).split(os.sep)) - 1
@@ -35,7 +26,7 @@ def calculate_base_path(cargo_toml_path):
     else:
         return os.path.join(*[".."] * depth)
 
-def main(cargo_toml_path):
+def main(cargo_toml_path, dep_path_mapping):
     base_path = calculate_base_path(cargo_toml_path)
 
     with open(cargo_toml_path, 'r') as file:
@@ -43,10 +34,10 @@ def main(cargo_toml_path):
 
     if 'dependencies' in data:
         dependencies = data['dependencies']
-        process_dependencies(dependencies, base_path)
+        process_dependencies(dependencies, base_path, dep_path_mapping)
 
     if 'workspace' in data and 'dependencies' in data['workspace']:
-        process_dependencies(data['workspace']['dependencies'], base_path)
+        process_dependencies(data['workspace']['dependencies'], base_path, dep_path_mapping)
 
     with open(cargo_toml_path, 'w') as file:
         toml.dump(data, file)
@@ -56,4 +47,29 @@ if __name__ == "__main__":
     parser.add_argument("cargo_toml_path", help="Path to the Cargo.toml file to modify")
     args = parser.parse_args()
 
-    main(os.path.normpath(args.cargo_toml_path))
+    # Define risc0, risc0-ethereum and bonsai dependencies and their paths here
+    # Add more dependencies and paths as needed
+    dep_path_mapping = {
+        "bonsai-rest-api-mock": "../risc0/bonsai/rest-api-mock",
+        "bonsai-sdk": "../risc0/bonsai/sdk",
+        "risc0-binfmt": "../risc0/risc0/binfmt",
+        "risc0-build": "../risc0/risc0/build",
+        "risc0-build-kernel": "../risc0/risc0/build_kernel",
+        "risc0-circuit-recursion": "../risc0/risc0/circuit/recursion",
+        "risc0-circuit-recursion-sys": "../risc0/risc0/circuit/recursion-sys",
+        "risc0-circuit-rv32im": "../risc0/risc0/circuit/rv32im",
+        "risc0-circuit-rv32im-sys": "../risc0/risc0/circuit/rv32im-sys",
+        "risc0-core": "../risc0/risc0/core",
+        "risc0-groth16": "../risc0/risc0/groth16",
+        "risc0-r0vm": "../risc0/risc0/r0vm",
+        "risc0-sys": "../risc0/risc0/sys",
+        "risc0-zkp": "../risc0/risc0/zkp",
+        "risc0-zkvm": "../risc0/risc0/zkvm",
+        "risc0-zkvm-platform": "../risc0/risc0/zkvm/platform",
+        "risc0-ethereum-contracts": "../risc0-ethereum/contracts",
+        "risc0-build-ethereum": "../risc0-ethereum/build",
+        "risc0-forge-ffi": "../risc0-ethereum/ffi",
+        "risc0-ethereum-relay": "../risc0-ethereum/relay",
+    }
+
+    main(os.path.normpath(args.cargo_toml_path), dep_path_mapping)
