6eae739410a3ed7776f6c9c34be645b9f75c45e6
capossele <angelocapossele@gmail.com>
2024-03-15 17:10:24 +0000

    make cargo_local_patch script more reliable

--- .github/cargo_local_patch.py
+++ .github/cargo_local_patch.py
@@ -1,75 +1,99 @@
 import argparse
 import toml
 import os
+import fnmatch
+
+def find_cargo_toml_files(start_path):
+    matches = []
+    for root, _, filenames in os.walk(start_path):
+        for filename in fnmatch.filter(filenames, 'Cargo.toml'):
+            matches.append(os.path.join(root, filename))
+    return matches
 
 def modify_dependency(details, new_path):
-    if 'path' not in details:
+    # If details is a string, it's in the simple format. Convert it to a dictionary.
+    if isinstance(details, str):
+        details = {'version': details}  # Convert to dictionary with version key
+    # Now details is guaranteed to be a dictionary
+    # Skip if the depdendency uses the workspace definition
+    if details.get('workspace') is not True:
         details['path'] = new_path
         details.pop('version', None)
         details.pop('git', None)
         details.pop('rev', None)
         details.pop('branch', None)
+    return details
 
+# Modify the list of dependencies in-place, setting a path reference for those in the mapping.
 def process_dependencies(dependencies, base_path, dep_path_mapping):
-    for dep, details in dependencies.items():
-        # Check if the current dependency is in our mapping
+    for dep, details in list(dependencies.items()):
+        # Check if a dep starts with "risc0-" or "bonsai-" and is not in the mapping.
+        # This should make sure to patch all the dependencies or fail if new are found.
+        if (dep.startswith("risc0-") or dep.startswith("bonsai-")) and dep not in dep_path_mapping:
+            raise ValueError(f"Dependency '{dep}' starts with 'risc0-' or 'bonsai-' but is not in dep_path_mapping.")
         if dep in dep_path_mapping:
-            # Construct the new path using the base_path and the relative path from the mapping
             new_path = os.path.join(base_path, dep_path_mapping[dep])
-            modify_dependency(details, new_path)
+            if not os.path.exists(new_path):
+                raise FileNotFoundError(f"The path {new_path} does not exist.")
+            new_details = modify_dependency(details, new_path)
+            dependencies[dep] = new_details
 
-
-def calculate_base_path(cargo_toml_path):
-    depth = len(os.path.normpath(cargo_toml_path).split(os.sep)) - 1
-    if depth == 0:
-        return "."
-    else:
-        return os.path.join(*[".."] * depth)
-
-def main(cargo_toml_path, dep_path_mapping):
-    base_path = calculate_base_path(cargo_toml_path)
+def process_cargo_file(cargo_toml_path, dep_path_mapping):
+    base_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../")
 
     with open(cargo_toml_path, 'r') as file:
         data = toml.load(file)
 
+    # Process regular dependencies
     if 'dependencies' in data:
-        dependencies = data['dependencies']
-        process_dependencies(dependencies, base_path, dep_path_mapping)
-
+        process_dependencies(data['dependencies'], base_path, dep_path_mapping)
+    # Process development dependencies
+    if 'dev-dependencies' in data:
+        process_dependencies(data['dev-dependencies'], base_path, dep_path_mapping)
+    # Process build dependencies
+    if 'build-dependencies' in data:
+        process_dependencies(data['build-dependencies'], base_path, dep_path_mapping)
+    # Process workspace dependencies, if present
     if 'workspace' in data and 'dependencies' in data['workspace']:
         process_dependencies(data['workspace']['dependencies'], base_path, dep_path_mapping)
 
     with open(cargo_toml_path, 'w') as file:
         toml.dump(data, file)
 
+def main(directory, dep_path_mapping):
+    cargo_toml_files = find_cargo_toml_files(directory)
+    for cargo_toml_path in cargo_toml_files:
+        process_cargo_file(cargo_toml_path, dep_path_mapping)
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Modify dependencies in Cargo.toml")
-    parser.add_argument("cargo_toml_path", help="Path to the Cargo.toml file to modify")
+    parser = argparse.ArgumentParser(description="Modify dependencies in Cargo.toml files within a directory.")
+    parser.add_argument("directory", help="Directory to search for Cargo.toml files")
     args = parser.parse_args()
 
     # Define risc0, risc0-ethereum and bonsai dependencies and their paths here
     # Add more dependencies and paths as needed
     dep_path_mapping = {
-        "bonsai-rest-api-mock": "../risc0/bonsai/rest-api-mock",
-        "bonsai-sdk": "../risc0/bonsai/sdk",
-        "risc0-binfmt": "../risc0/risc0/binfmt",
-        "risc0-build": "../risc0/risc0/build",
-        "risc0-build-kernel": "../risc0/risc0/build_kernel",
-        "risc0-circuit-recursion": "../risc0/risc0/circuit/recursion",
-        "risc0-circuit-recursion-sys": "../risc0/risc0/circuit/recursion-sys",
-        "risc0-circuit-rv32im": "../risc0/risc0/circuit/rv32im",
-        "risc0-circuit-rv32im-sys": "../risc0/risc0/circuit/rv32im-sys",
-        "risc0-core": "../risc0/risc0/core",
-        "risc0-groth16": "../risc0/risc0/groth16",
-        "risc0-r0vm": "../risc0/risc0/r0vm",
-        "risc0-sys": "../risc0/risc0/sys",
-        "risc0-zkp": "../risc0/risc0/zkp",
-        "risc0-zkvm": "../risc0/risc0/zkvm",
-        "risc0-zkvm-platform": "../risc0/risc0/zkvm/platform",
-        "risc0-ethereum-contracts": "../risc0-ethereum/contracts",
-        "risc0-build-ethereum": "../risc0-ethereum/build",
-        "risc0-forge-ffi": "../risc0-ethereum/ffi",
-        "risc0-ethereum-relay": "../risc0-ethereum/relay",
+        "bonsai-rest-api-mock": "risc0/bonsai/rest-api-mock",
+        "bonsai-sdk": "risc0/bonsai/sdk",
+        "risc0-binfmt": "risc0/risc0/binfmt",
+        "risc0-build": "risc0/risc0/build",
+        "risc0-build-kernel": "risc0/risc0/build_kernel",
+        "risc0-circuit-recursion": "risc0/risc0/circuit/recursion",
+        "risc0-circuit-recursion-sys": "risc0/risc0/circuit/recursion-sys",
+        "risc0-circuit-rv32im": "risc0/risc0/circuit/rv32im",
+        "risc0-circuit-rv32im-sys": "risc0/risc0/circuit/rv32im-sys",
+        "risc0-core": "risc0/risc0/core",
+        "risc0-groth16": "risc0/risc0/groth16",
+        "risc0-r0vm": "risc0/risc0/r0vm",
+        "risc0-sys": "risc0/risc0/sys",
+        "risc0-zkp": "risc0/risc0/zkp",
+        "risc0-zkvm": "risc0/risc0/zkvm",
+        "risc0-zkvm-platform": "risc0/risc0/zkvm/platform",
+        "risc0-ethereum-contracts": "risc0-ethereum/contracts",
+        "risc0-build-ethereum": "risc0-ethereum/build",
+        "risc0-forge-ffi": "risc0-ethereum/ffi",
+        "risc0-ethereum-relay": "risc0-ethereum/relay",
+        "risc0-ethereum-relay-test-methods": "risc0-ethereum/relay/tests/methods",
     }
 
-    main(os.path.normpath(args.cargo_toml_path), dep_path_mapping)
+    main(os.path.normpath(args.directory), dep_path_mapping)
