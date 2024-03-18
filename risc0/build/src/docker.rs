56d568f6b163db4fa191b0373c34d32a9c3735d0
Matheus Cardoso <cardosaum@pm.me>
2024-03-18 11:32:45 -0300

    Update image_id in docker test

--- risc0/build/src/docker.rs
+++ risc0/build/src/docker.rs
@@ -239,7 +239,7 @@ mod test {
         build("../../risc0/zkvm/methods/guest/Cargo.toml");
         compare_image_id(
             "risc0_zkvm_methods_guest/multi_test",
-            "acfdb2b8c96e32000437d05465077ee25dfeb26d478d0aca2449111343424a83",
+            "68f4b60f7850dc29068af99b94f0c94443e0138f727c132fd8ce1c272f81f5ef",
         );
     }
 }
