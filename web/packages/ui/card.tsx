6c875197dfc056fdd3880ee0f3d453be2b56a352
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-03-30 15:11:25 -0400

    chore: add sharp

--- web/packages/ui/card.tsx
+++ web/packages/ui/card.tsx
@@ -11,7 +11,7 @@ const CardHeader = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(({
 
 const CardTitle = forwardRef<HTMLParagraphElement, HTMLAttributes<HTMLHeadingElement>>(
   ({ className, ...rest }, ref) => (
-    <h3 ref={ref} className={cn("font-semibold leading-none tracking-tight", className)} {...rest} />
+    <h2 ref={ref} className={cn("font-semibold leading-none tracking-tight", className)} {...rest} />
   ),
 );
 
