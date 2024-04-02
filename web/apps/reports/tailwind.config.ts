ef2d4674bd985344837cf1f81d107b33cadb71ec
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-04-02 19:54:25 -0400

    chore: fix tailwind path

--- web/apps/reports/tailwind.config.ts
+++ web/apps/reports/tailwind.config.ts
@@ -1,9 +1,1 @@
-import risc0UiConfig from "@risc0/ui/config/tailwind.config";
-import type { Config } from "tailwindcss";
-
-const config = {
-  ...risc0UiConfig,
-  content: ["./node_modules/@risc0/ui/**/*.{ts,tsx}", "./**/*.{ts,tsx}"],
-} satisfies Config;
-
-export default config;
+export * from "@risc0/ui/config/tailwind.config";
