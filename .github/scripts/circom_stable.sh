cf35b6124629dec218456f8d89e1c81cdeedf3d3
Tim Zerrell <tim.zerrell@risczero.com>
2024-04-16 20:20:23 +0000

    Fix typo

--- .github/scripts/circom_stable.sh
+++ .github/scripts/circom_stable.sh
@@ -3,6 +3,7 @@ RISC0_LIB_EXPECTED_HASH="38d3937558dae8f9de3d0bdc534c76c9adc6c7ffe37cc4393507332
 if [ $RISC0_LIB_HASH != $RISC0_LIB_EXPECTED_HASH ]; then
     echo "Unexpected SHA-256 of risc0.circom: $RISC0_LIB_HASH"
     exit 1
+fi
     
 STARK_VERIFY_HASH=$(shasum -a 256 compact_proof/groth16/stark_verify.circom | awk '{ print $1 }')
 STARK_VERIFY_EXPECTED_HASH="a3789471909ba1a13cca783dc5269b25b6d41295abe60234a8075f750017518c"
