a4ddfde7c256b4a8ac66e3640b34c2eb692298a3
Tim Zerrell <tim.zerrell@risczero.com>
2024-04-17 21:11:41 +0000

    Use git hash-object instead of shasum

--- .github/scripts/circom_stable.sh
+++ .github/scripts/circom_stable.sh
@@ -1,13 +1,13 @@
-RISC0_LIB_HASH=$(shasum -a 256 compact_proof/groth16/risc0.circom | awk '{ print $1 }')
-RISC0_LIB_EXPECTED_HASH="38d3937558dae8f9de3d0bdc534c76c9adc6c7ffe37cc43935073324e1e53bf3"
+RISC0_LIB_HASH=$(git hash-object -- compact_proof/groth16/risc0.circom | awk '{ print $1 }')
+RISC0_LIB_EXPECTED_HASH="87af73e7d1b24bff3df350136ec6637245a2330a"
 if [ $RISC0_LIB_HASH != $RISC0_LIB_EXPECTED_HASH ]; then
-    echo "Unexpected SHA-256 of risc0.circom: $RISC0_LIB_HASH"
+    echo "Unexpected hash of risc0.circom: $RISC0_LIB_HASH"
     exit 1
 fi
     
-STARK_VERIFY_HASH=$(shasum -a 256 compact_proof/groth16/stark_verify.circom | awk '{ print $1 }')
-STARK_VERIFY_EXPECTED_HASH="a3789471909ba1a13cca783dc5269b25b6d41295abe60234a8075f750017518c"
+STARK_VERIFY_HASH=$(git hash-object -- compact_proof/groth16/stark_verify.circom | awk '{ print $1 }')
+STARK_VERIFY_EXPECTED_HASH="81557c4f15ce43f5c7c840c287e76abd3a676cd5"
 if [ $STARK_VERIFY_HASH != $STARK_VERIFY_EXPECTED_HASH ]; then
-    echo "Unexpected SHA-256 of stark_verify.circom: $STARK_VERIFY_HASH"
+    echo "Unexpected hash of stark_verify.circom: $STARK_VERIFY_HASH"
     exit 1
 fi
