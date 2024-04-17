df9234cdf475c0e2b47bdfb01a02ed41a9a35618
Erik Kaneda <erik@risczero.com>
2024-04-17 14:44:01 -0700

    run cargo fmt

--- risc0/zkvm/src/host/recursion/prove/mod.rs
+++ risc0/zkvm/src/host/recursion/prove/mod.rs
@@ -260,8 +260,8 @@ mod cuda {
 mod metal {
     pub use risc0_circuit_recursion::metal::MetalCircuitHal;
     pub use risc0_zkp::hal::metal::{
-        MetalHalPoseidon, MetalHalPoseidon2, MetalHalSha256, MetalHashPoseidon,
-        MetalHashPoseidon2, MetalHashSha256,
+        MetalHalPoseidon, MetalHalPoseidon2, MetalHalSha256, MetalHashPoseidon, MetalHashPoseidon2,
+        MetalHashSha256,
     };
 
     use super::{HalPair, Rc};
