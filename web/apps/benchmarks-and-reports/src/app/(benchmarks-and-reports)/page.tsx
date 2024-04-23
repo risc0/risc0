a47fe300a752d9aa33f40f5dcc2c5ed21941e467
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-04-23 10:08:35 -0400

    chore: max-width

--- web/apps/benchmarks-and-reports/src/app/(benchmarks-and-reports)/page.tsx
+++ web/apps/benchmarks-and-reports/src/app/(benchmarks-and-reports)/page.tsx
@@ -5,7 +5,7 @@ import { REPORTS } from "./_utils/constants";
 
 export default function ReportsPage() {
   return (
-    <div className="container grid max-w-screen-xl grid-cols-1 gap-4 pt-4 lg:grid-cols-2">
+    <div className="container grid max-w-screen-3xl grid-cols-1 gap-4 pt-4 lg:grid-cols-2">
       {REPORTS.map(({ label, href, description }, index) => (
         <Link key={href} href={href} className="group transition-opacity hover:opacity-70">
           <Card className="group-hover:-translate-y-1 flex h-full min-h-44 w-full flex-col items-center justify-between gap-1 px-8 py-4 shadow-sm transition-transform md:flex-row md:gap-12">
