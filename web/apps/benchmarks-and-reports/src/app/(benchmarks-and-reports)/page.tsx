ded4466b7fc3bd332351e46d05e6ebc855ea3dc5
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-04-23 11:46:31 -0400

    chore: optimize svgs

--- web/apps/benchmarks-and-reports/src/app/(benchmarks-and-reports)/page.tsx
+++ web/apps/benchmarks-and-reports/src/app/(benchmarks-and-reports)/page.tsx
@@ -8,7 +8,7 @@ export default function ReportsPage() {
     <div className="container grid max-w-screen-3xl grid-cols-1 gap-4 pt-4 lg:grid-cols-2">
       {REPORTS.map(({ label, href, description }, index) => (
         <Link key={href} href={href} className="group transition-opacity hover:opacity-70">
-          <Card className="group-hover:-translate-y-1 flex h-full min-h-44 w-full flex-col items-center justify-between gap-1 px-8 py-4 shadow-sm transition-transform md:flex-row md:gap-12">
+          <Card className="group-hover:-translate-y-1 flex h-full min-h-44 w-full flex-col items-center justify-between gap-1 px-8 py-4 shadow-sm transition-transform md:flex-row md:gap-12 dark:bg-neutral-900">
             <div>
               <CardTitle className="text-xl">{label}</CardTitle>
               <CardDescription className="text-sm">{description}</CardDescription>
