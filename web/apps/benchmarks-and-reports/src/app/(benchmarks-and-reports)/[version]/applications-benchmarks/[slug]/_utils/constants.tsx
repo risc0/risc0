0118b2532f6ce70b3be7bcb8168f5ad9eac6e3bd
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-05-10 14:27:49 -0400

    chore: remove bogus import

--- web/apps/benchmarks-and-reports/src/app/(benchmarks-and-reports)/[version]/applications-benchmarks/[slug]/_utils/constants.tsx
+++ web/apps/benchmarks-and-reports/src/app/(benchmarks-and-reports)/[version]/applications-benchmarks/[slug]/_utils/constants.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-
 export const FILENAMES_TO_TITLES = {
   "macOS-apple_m2_pro.csv": "Metal on Apple M2 Pro",
   "Linux-nvidia_rtx_a5000.csv": "CUDA on NVIDIA RTX 3090 Ti",
