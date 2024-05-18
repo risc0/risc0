07a8cda600a5157544e9f786e71f810e218b6a20
Frank Laub <frank@risczero.com>
2024-05-18 00:06:24 -0700

    Update csv

--- web/apps/benchmarks-and-reports/src/app/(benchmarks-and-reports)/[version]/applications-benchmarks/[slug]/_utils/constants.tsx
+++ web/apps/benchmarks-and-reports/src/app/(benchmarks-and-reports)/[version]/applications-benchmarks/[slug]/_utils/constants.tsx
@@ -1,6 +1,6 @@
 export const FILENAMES_TO_TITLES = {
   "macOS-apple_m2_pro.csv": "Metal on Apple M2 Pro",
-  "Linux-nvidia_rtx_a5000.csv": "CUDA on NVIDIA RTX 3090 Ti",
+  "Linux-nvidia_rtx_3090_ti.csv": "CUDA on NVIDIA RTX 3090 Ti",
   "macOS-cpu.csv": "CPU only on Apple M2 Pro",
   "Linux-cpu.csv": "CPU only on c6i.8xlarge",
 } as const;
