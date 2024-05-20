e1c6dfd9f3a94f582d571775e89030f0a6a093ac
BatAdmin <batadmin@BatMac.local>
2024-05-20 13:30:46 -0600

    test pulling files from server of runner

--- .github/scripts/iam_risc0_cache.sh
+++ .github/scripts/iam_risc0_cache.sh
@@ -1,2 +1,2 @@
-cp ../../../../_aws/certificate.pem $HOME/.local/aws/certificate.pem
-cp ../../../../_aws/runner_key.pem $HOME/.local/aws/runner_key.pem
+cp ../../../_aws/certificate.pem $HOME/.local/aws/certificate.pem
+cp ../../../_aws/runner_key.pem $HOME/.local/aws/runner_key.pem
