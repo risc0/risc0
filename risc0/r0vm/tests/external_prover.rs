580d05c98c515acf783dc224f67937c5efc10ceb
Erik Kaneda <erik@risczero.com>
2024-05-21 14:46:10 -0700

    tests: add call to compress

--- risc0/r0vm/tests/external_prover.rs
+++ risc0/r0vm/tests/external_prover.rs
@@ -14,10 +14,10 @@
 
 use anyhow::Result;
 use assert_cmd::cargo::cargo_bin;
-use risc0_zkvm::{ExecutorEnv, ExternalProver, ProveInfo, Prover};
+use risc0_zkvm::{ExecutorEnv, ExternalProver, Prover, ProverOpts, Receipt};
 use risc0_zkvm_methods::{multi_test::MultiTestSpec, MULTI_TEST_ELF, MULTI_TEST_ID};
 
-fn prove_nothing() -> Result<ProveInfo> {
+fn prove_nothing() -> Result<Receipt> {
     let env = ExecutorEnv::builder()
         .write(&MultiTestSpec::DoNothing)
         .unwrap()
@@ -25,11 +25,13 @@ fn prove_nothing() -> Result<ProveInfo> {
         .unwrap();
     let r0vm_path = cargo_bin("r0vm");
     let prover = ExternalProver::new("r0vm", r0vm_path);
-    prover.prove(env, MULTI_TEST_ELF)
+    let receipt = prover.prove(env, MULTI_TEST_ELF)?.receipt;
+    prover.compress(&ProverOpts::succinct(), &receipt)
 }
 
 #[test_log::test]
 fn basic_proof() {
-    let receipt = prove_nothing().unwrap().receipt;
+    let receipt = prove_nothing().unwrap();
     receipt.verify(MULTI_TEST_ID).unwrap();
+    receipt.inner.succinct().unwrap();
 }
