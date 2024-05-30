4396170e8ef8469de6a62381d12dbd18a149b03f
Austin Abell <austinabell8@gmail.com>
2024-05-30 11:20:28 -0400

    fix spacing

--- examples/c-guest/guest/main.c
+++ examples/c-guest/guest/main.c
@@ -7,7 +7,7 @@ union u32_cast {
 };
 
 int main() {
-// TODO introduce entropy into memory image (for zk)
+  // TODO introduce entropy into memory image (for zk)
   sha256_state* hasher = init_sha256();
 
   // Read two u32 values from the host, assuming LE byte order.
