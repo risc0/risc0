7ac13b0ed5fdad93b5475cfa36dc24ee0c684780
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-06-07 06:37:03 -0400

    fix: force dynamic

--- web/apps/benchmarks-and-reports/src/app/(benchmarks-and-reports)/[version]/page.tsx
+++ web/apps/benchmarks-and-reports/src/app/(benchmarks-and-reports)/[version]/page.tsx
@@ -3,14 +3,7 @@ import Image from "next/image";
 import { ProgressBarLink } from "shared/client/providers/progress-bar-provider";
 import { useRedirectIfWrongVersion } from "~/hooks/use-redirect-if-wrong-version";
 import type { Version } from "~/types/version";
-import { VERSIONS } from "~/versions";
 import { REPORTS } from "./_utils/constants";
-
-export function generateStaticParams() {
-  return VERSIONS.map(({ value }) => ({
-    version: value,
-  }));
-}
 
 export default function ReportsPage({
   params,
