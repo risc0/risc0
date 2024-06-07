2ac47490b6c21242e6cb453a2ea42e01545249cf
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-06-06 22:20:44 -0400

    fix: last try for tonight3

--- web/apps/benchmarks-and-reports/src/app/(benchmarks-and-reports)/[version]/applications-benchmarks/[slug]/_actions/fetch-applications-benchmarks-commit-hash.ts
+++ web/apps/benchmarks-and-reports/src/app/(benchmarks-and-reports)/[version]/applications-benchmarks/[slug]/_actions/fetch-applications-benchmarks-commit-hash.ts
@@ -6,6 +6,9 @@ import type { Version } from "~/types/version";
 export async function fetchApplicationsBenchmarksCommitHash({ version }: { version: Version }) {
   const [error, response] = await tryFetch(
     `https://raw.githubusercontent.com/risc0/ghpages/${version}/dev/benchmarks/COMMIT_HASH.txt`,
+    {
+      next: { revalidate: 10 }, // 3 minutes cache
+    },
   );
 
   // error handling
