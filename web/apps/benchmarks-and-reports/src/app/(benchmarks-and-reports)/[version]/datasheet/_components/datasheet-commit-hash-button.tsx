c5a6ddf52536b722d8cb9e31e8e67eeb5740f6f0
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-06-08 08:06:26 -0400

    fix: last update tooltip

--- web/apps/benchmarks-and-reports/src/app/(benchmarks-and-reports)/[version]/datasheet/_components/datasheet-commit-hash-button.tsx
+++ web/apps/benchmarks-and-reports/src/app/(benchmarks-and-reports)/[version]/datasheet/_components/datasheet-commit-hash-button.tsx
@@ -1,14 +1,20 @@
+import { Tooltip, TooltipContent, TooltipTrigger } from "@risc0/ui/tooltip";
 import { truncate } from "@risc0/ui/utils/truncate";
 import { CopyButton } from "shared/client/components/copy-button";
 import type { Version } from "~/types/version";
 import { fetchDatasheetCommitHash } from "../_actions/fetch-datasheet-commit-hash";
 
 export async function DatasheetCommitHashButton({ version }: { version: Version }) {
-  const commitHash = await fetchDatasheetCommitHash({ version });
+  const { data: commitHash, updatedAt } = await fetchDatasheetCommitHash({ version });
 
   return (
-    <CopyButton size="sm" variant="ghost" value={commitHash}>
-      Commit Hash<span className="hidden sm:inline">: {truncate(commitHash, 15)}</span>
-    </CopyButton>
+    <Tooltip>
+      <TooltipTrigger>
+        <CopyButton size="sm" variant="ghost" value={commitHash}>
+          Commit Hash<span className="hidden sm:inline">: {truncate(commitHash, 15)}</span>
+        </CopyButton>
+      </TooltipTrigger>
+      <TooltipContent>Last Update: {new Date(updatedAt).toLocaleString()}</TooltipContent>
+    </Tooltip>
   );
 }
