8d1c18f947a4f3fe60c8b9439183169518395e34
Morgan Gibbs <morganseangibbs87@gmail.com>
2024-06-17 16:42:43 -0600

    initial add for contstants.ts

---
+++ web/apps/benchmarks-and-reports/src/app/(benchmarks-and-reports)/[version]/datasheet/_utils/constants.tsx
@@ -0,0 +1,76 @@
+import Link from "next/link";
+import type { Version } from "~/types/version";
+
+export const DATASHEET_FILENAMES_TO_TITLES: {
+	[K in Version]: {
+		[key: string]: string;
+	};
+} = {
+	main: {
+		"macOS-apple_m2_pro.json": "Metal on Apple M2 Pro",
+		"Linux-nvidia_rtx_3090_ti.json": "CUDA on NVIDIA RTX 3090 Ti",
+		"macOS-cpu.json": "CPU only on Apple M2 Pro",
+		"Linux-cpu.json": (
+			<>
+				CPU only on{" "}
+				<Link
+					rel="noopener noreferrer"
+					target="_blank"
+					href="https://instances.vantage.sh/aws/ec2/c6i.8xlarge"
+					className="link"
+				>
+					c6i.8xlarge
+				</Link>
+			</>
+		),
+		"Linux-g6.xlarge.json": (
+			<>
+				CUDA on{" "}
+				<Link
+					rel="noopener noreferrer"
+					target="_blank"
+					href="https://instances.vantage.sh/aws/ec2/g6.xlarge"
+					className="link"
+				>
+					g6.xlarge
+				</Link>
+			</>
+		),
+	},
+	"release-0.21": {
+		"macOS-apple_m2_pro.json": "Metal on Apple M2 Pro",
+		"Linux-nvidia_rtx_a5000.json": "CUDA on NVIDIA RTX A5000",
+		"macOS-cpu.json": "CPU only on Apple M2 Pro",
+		"Linux-cpu.json": (
+			<>
+				CPU only on{" "}
+				<Link
+					rel="noopener noreferrer"
+					target="_blank"
+					href="https://instances.vantage.sh/aws/ec2/c6i.8xlarge"
+					className="link"
+				>
+					c6i.8xlarge
+				</Link>
+			</>
+		),
+	},
+	"release-1.0": {
+		"macOS-apple_m2_pro.json": "Metal on Apple M2 Pro",
+		"Linux-nvidia_rtx_3090_ti.json": "CUDA on NVIDIA RTX 3090 Ti",
+		"macOS-cpu.json": "CPU only on Apple M2 Pro",
+		"Linux-cpu.json": (
+			<>
+				CPU only on{" "}
+				<Link
+					rel="noopener noreferrer"
+					target="_blank"
+					href="https://instances.vantage.sh/aws/ec2/c6i.8xlarge"
+					className="link"
+				>
+					c6i.8xlarge
+				</Link>
+			</>
+		),
+	},
+} as const;
