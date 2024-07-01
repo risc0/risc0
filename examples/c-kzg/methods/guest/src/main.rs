c120ebbb7cb8dcd028fd9443834b287365577f0e
Erik Kaneda <erik@risczero.com>
2024-07-01 16:36:18 -0700

    add license headers

--- examples/c-kzg/methods/guest/src/main.rs
+++ examples/c-kzg/methods/guest/src/main.rs
@@ -1,5 +1,17 @@
-use c_kzg::{Bytes32, Bytes48, KzgProof};
-use c_kzg::{KzgCommitment, KzgSettings};
+// Copyright 2024 RISC Zero, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+use c_kzg::{Bytes32, Bytes48, KzgCommitment, KzgProof, KzgSettings};
 use c_kzg_core::Proof;
 use risc0_zkvm::guest::env;
 use sha2::{Digest as _, Sha256};
