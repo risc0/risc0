52411915806b40f686481db3c0ad3677fbca04fe
hans <hmrtn@pm.me>
2024-07-17 12:32:53 -0700

    ZKVM-484: revamp `rzup` cli  (#1961)
    This PR is a Rust revamp of the original `rzup` bash scripts. The goal
    of this revamp is to have `rzup` act similarly to `rusutp` for toolchain
    management, and to eventually supersede `cargo risczero install` and
    `cargo risczero build-toolchain`.
    
    The features and necessary requirements are likely to change with
    suggestions and updates. Below is running list the progress.
    
    To try it out: `cargo run --bin rzup` shows the info previewed below. 
    
    To install the sane defaults (rust, cpp, cargo-risczero): `rzup install`
    
    To install specifics: 
    - cargo-risczero: `rzup extension install cargo-risczero` or `rzup
    install cargo-risczero`
    - rust: `rzup toolchain install rust` or `rzup install rust`
    - cpp: `rzup toolchain install cpp` or `rzup install cpp`
    
    To check for updates of cargo-risczero and the active toolchains: `rzup
    check`
    
    To show all installed things and meta: `rzup show`
    
    To set new defaults (i.e. active) toolchains: `rzup default rust
    <TOOLCHAIN_NAME>`, `rzup default cpp <TOOLCHAIN_NAME>`
    
    To use different cargo-risczero versions (and zkVM): `rzup extension use
    cargo-risczero <VERSION>`
    
    Each of these have more subcommands that you can see with `--help`.
    
    Todo: 
    - [x] quick start install `rzup install` (latest rust toolchain, cpp, &
    cargo risczero)
      - [x] cargo-risczero: extension installation / checking / updating
      - [x] List installed toolchains
      - [x] Install specific toolchains (cpp/rust)
      - [x] Build rust toolchain 
      - [x] Show toolchain / extension / rzup info*
      - [x] rust toolchain update checks 
      - [x] cargo risczero update check
      - [x] pretty print 
      - [x] c++ toolchain update check 
      - [x] Self (rzup) ~~check~~, update, etc 
      - [x] Set default toolchain + `settings.toml` persistence (?)
    - [x] `rzup-init.sh` script for detecting the target and downloading
    correct `rzup`
      - [x] Github token support to avoid throttling
      - [x] true verbosity 
      - [x] help/discussion coverage 
      - [x] Improve error handling
    
    Preview: 
    ```
    rzup 0.2.1
    
        ██████  █▀▀▀██
        ██████  █ ▄▀ █        Prove anything.
        ██████  ██▄▄▄█       Verify everywhere.
        ██████
        ██████   RISC
        ██████   ZERO
    
    Contribute      : https://github.com/risc0/
    Documentation   : https://dev.risczero.com/
    Chat & Support  : https://discord.com/invite/risczero/
    
    
    Usage: rzup [OPTIONS] [COMMAND]
    
    Commands:
      show       Show the active and installed toolchains and extensions
      install    Install toolchains or extensions
      update     Update toolchains or extensions
      check      Check for updates to toolchains and extensions
      default    Set the default toolchains
      toolchain  Modify or query the installed toolchains
      extension  Modify or query the installed extensions
      help       Print this message or the help of the given subcommand(s)
    
    Options:
      -v, --verbose
              Enable verbose output
    
      -h, --help
              Print help
    
      -V, --version
              Print version
    
    Discussion:
        rzup installs RISC Zero toolchains and extensions from the official
        release channels, enabling you to easily switch between different
        versions and keep them updated.
    
        If you are new to RISC Zero, consider visitng our documentation
        to learn more.
    ```
    
    ---------
    
    Co-authored-by: Erik Kaneda <erik@risczero.com>

---
+++ rzup/src/cli/default.rs
@@ -0,0 +1,46 @@
+// Copyright 2024 RISC Zero, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::{
+    toolchain::Toolchain,
+    utils::{find_active_toolchain_name, rzup_home, CPP_TOOLCHAIN_NAME, RUSTUP_TOOLCHAIN_NAME},
+};
+use anyhow::Result;
+
+pub fn handler(toolchain: Option<Toolchain>, name: Option<String>) -> Result<()> {
+    match (toolchain, name) {
+        // Set Default
+        (Some(toolchain), Some(name)) => {
+            // TODO: Use regex to match path rather than require specific name
+            let toolchain_path = rzup_home()?.join("toolchains").join(name);
+            toolchain.link(&toolchain_path)?;
+        }
+        // Show specific default toolchain (rust/cpp)
+        (Some(toolchain), None) => {
+            let active_toolchain_name = match toolchain {
+                Toolchain::Rust => find_active_toolchain_name(RUSTUP_TOOLCHAIN_NAME)?,
+                Toolchain::Cpp => find_active_toolchain_name(CPP_TOOLCHAIN_NAME)?,
+            };
+            eprintln!("{} (default)", active_toolchain_name);
+        }
+        // Show all default (active) toolchains
+        (None, _) => {
+            let rust_toolchain_name = find_active_toolchain_name(RUSTUP_TOOLCHAIN_NAME)?;
+            let cpp_toolchain_name = find_active_toolchain_name(CPP_TOOLCHAIN_NAME)?;
+            eprintln!("Rust: {} (default)", rust_toolchain_name);
+            eprintln!("C++: {} (default)", cpp_toolchain_name);
+        }
+    }
+    Ok(())
+}
---
+++ rzup/src/cli/extension.rs
@@ -0,0 +1,100 @@
+// Copyright 2024 RISC Zero, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::path::PathBuf;
+
+use crate::{cli, extension::Extension, utils::find_installed_extensions};
+use anyhow::Result;
+use clap::Subcommand;
+use regex::Regex;
+
+#[derive(Debug, Subcommand)]
+#[command(
+    arg_required_else_help = true,
+    subcommand_required = true,
+    after_help = cli::help::EXTENSION_HELP
+)]
+pub enum ExtensionSubcmd {
+    /// List all installed extensions
+    List,
+    /// Install an extension (i.e cargo-risczero v1.0.1)
+    #[command(aliases = ["add"])]
+    Install {
+        /// The extension to install (e.g., cargo-risczero)
+        extension: Extension,
+        /// The version of the extension to install (e.g., v1.0.1)
+        version: Option<String>,
+        /// Force installation, removing existing directories
+        #[arg(short, long)]
+        force: bool,
+    },
+    /// Use an installed extension version
+    Use {
+        /// The extension to use (e.g., cargo-risczero)
+        extension: Extension,
+        /// The version of the extension to use (e.g., v1.0.1)
+        version: String,
+    },
+    /// Uninstall an installed extension
+    Uninstall {
+        /// The extension to uninstall (e.g., cargo-risczero)
+        extension: Extension,
+    },
+}
+
+pub async fn handler(subcmd: ExtensionSubcmd) -> Result<()> {
+    match subcmd {
+        ExtensionSubcmd::Install {
+            extension,
+            version,
+            force,
+        } => extension.install(version.as_deref(), force).await,
+        ExtensionSubcmd::List => {
+            let extensions = find_installed_extensions()?;
+            for extension in extensions {
+                eprintln!("{}", extension.file_name().unwrap().to_string_lossy());
+            }
+            Ok(())
+        }
+        ExtensionSubcmd::Use { extension, version } => {
+            let extension_path = parse_extenstion_version(extension, version)?;
+            extension.link(&extension_path)
+        }
+        ExtensionSubcmd::Uninstall { extension } => extension.unlink(),
+    }
+}
+
+fn parse_extenstion_version(extension: Extension, version: String) -> Result<PathBuf> {
+    let extensions = find_installed_extensions()?;
+    let version_pattern = match extension {
+        Extension::CargoRiscZero => {
+            format!(r"^(v)?{}(?:-.+)?$", regex::escape(&version))
+        }
+    };
+
+    let re = Regex::new(&version_pattern)?;
+
+    for ext in extensions {
+        if let Some(dir_name) = ext.file_name().and_then(|name| name.to_str()) {
+            if re.is_match(dir_name) {
+                return Ok(ext);
+            }
+        }
+    }
+    Err(anyhow::anyhow!(format!(
+        "No matching {} found for version {}. \n\nFor more information, try '--help'.",
+        extension.to_str(),
+        version
+    )))
+}
