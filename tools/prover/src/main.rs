b6b070ef2daefba68da846376b3f2404a344507b
Frank Laub <frank@risczero.com>
2024-07-20 17:42:42 -0700

    Example of simple prover service

---
+++ tools/prover/src/main.rs
@@ -0,0 +1,64 @@
+// Copyright 2024 RISC Zero, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+mod plan;
+mod task_mgr;
+mod worker;
+
+use risc0_zkvm::{ExecutorEnv, ExecutorImpl, InnerReceipt, NullSegmentRef, Receipt};
+use risc0_zkvm_methods::{FIB_ELF, FIB_ID};
+
+use self::{plan::Planner, task_mgr::TaskManager};
+
+fn main() {
+    println!("Submitting proof request...");
+
+    let mut task_manager = TaskManager::new();
+    let mut planner = Planner::default();
+
+    let iterations = 100;
+    let env = ExecutorEnv::builder()
+        .write_slice(&[iterations])
+        .segment_limit_po2(15)
+        .build()
+        .unwrap();
+    let mut exec = ExecutorImpl::from_elf(env, FIB_ELF).unwrap();
+    let session = exec
+        .run_with_callback(|segment| {
+            planner.enqueue_segment(segment.index).unwrap();
+            task_manager.add_segment(segment);
+            while let Some(task) = planner.next_task() {
+                task_manager.add_task(task.clone());
+            }
+            Ok(Box::new(NullSegmentRef))
+        })
+        .unwrap();
+
+    planner.finish().unwrap();
+
+    println!("Plan:");
+    println!("{planner:?}");
+
+    while let Some(task) = planner.next_task() {
+        task_manager.add_task(task.clone());
+    }
+
+    let root_receipt = task_manager.run();
+    let receipt = Receipt::new(
+        InnerReceipt::Succinct(root_receipt),
+        session.journal.unwrap().bytes.clone(),
+    );
+    receipt.verify(FIB_ID).unwrap();
+    println!("Receipt verified!");
+}
---
+++ tools/prover/src/worker.rs
@@ -0,0 +1,72 @@
+// Copyright 2024 RISC Zero, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use risc0_zkvm::{
+    ApiClient, Asset, AssetRequest, ProverOpts, ReceiptClaim, Segment, SuccinctReceipt,
+};
+
+use crate::task_mgr::{Job, JobKind};
+
+#[derive(Default)]
+pub struct Worker;
+
+impl workerpool::Worker for Worker {
+    type Input = Job;
+    type Output = Job;
+
+    fn execute(&mut self, job: Job) -> Job {
+        println!("{:?}", job.task);
+        let receipt = match job.kind {
+            JobKind::Segment(segment) => self.prove_and_lift(segment),
+            JobKind::Join(left, right) => self.join(left, right),
+            JobKind::Receipt(receipt) => receipt,
+        };
+        Job {
+            task: job.task,
+            kind: JobKind::Receipt(receipt),
+        }
+    }
+}
+
+impl Worker {
+    fn prove_and_lift(&self, segment: Segment) -> SuccinctReceipt<ReceiptClaim> {
+        let opts = ProverOpts::default();
+        let client = ApiClient::new().unwrap();
+        let segment = bincode::serialize(&segment).unwrap();
+
+        let segment_asset = Asset::Inline(segment.into());
+        let segment_receipt = client
+            .prove_segment(&opts, segment_asset, AssetRequest::Inline)
+            .unwrap();
+
+        let segment_receipt_asset = segment_receipt.try_into().unwrap();
+        client
+            .lift(&opts, segment_receipt_asset, AssetRequest::Inline)
+            .unwrap()
+    }
+
+    fn join(
+        &self,
+        left: SuccinctReceipt<ReceiptClaim>,
+        right: SuccinctReceipt<ReceiptClaim>,
+    ) -> SuccinctReceipt<ReceiptClaim> {
+        let opts = ProverOpts::default();
+        let client = ApiClient::new().unwrap();
+        let left_asset = left.try_into().unwrap();
+        let right_asset = right.try_into().unwrap();
+        client
+            .join(&opts, left_asset, right_asset, AssetRequest::Inline)
+            .unwrap()
+    }
+}
