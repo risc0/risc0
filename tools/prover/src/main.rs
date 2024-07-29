3e4fb03b978bc7747cb7966bf230842f57add217
Frank Laub <flaub@risc0.com>
2024-07-29 01:42:52 -0700

    Update tools/prover/src/main.rs
    Co-authored-by: Victor Graf <victor@risczero.com>

--- tools/prover/src/main.rs
+++ tools/prover/src/main.rs
@@ -12,6 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//! This is an example of how the public 1.0 API can be used to build a proving service.
+//! It's not meant to be used in production since it doesn't handle failures.
+//! This is also not an optimal implementation; many performance improvements could be made.
+
 mod plan;
 mod task_mgr;
 mod worker;
