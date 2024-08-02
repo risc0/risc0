098f2aecfea8849e5a21723ed3d83392d68783ae
Frank Laub <frank@risczero.com>
2024-08-02 12:25:27 -0700

    Update image id

--- risc0/build/src/docker.rs
+++ risc0/build/src/docker.rs
@@ -250,7 +250,7 @@ mod test {
         build("../../risc0/zkvm/methods/guest/Cargo.toml");
         compare_image_id(
             "risc0_zkvm_methods_guest/hello_commit",
-            "54fe9f56a338ab625933df086fbb2fd08526f39b29d3e41f6d6e5f15b00d7cbe",
+            "06921f365c4e7c321d2c89eeab51cfbcd371b6052d58f303d8199338268e5f7c",
         );
     }
 }
