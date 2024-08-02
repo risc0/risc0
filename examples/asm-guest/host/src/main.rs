c5fd6713c845de452f8c567021dd060b08a75126
Austin Abell <austinabell8@gmail.com>
2024-08-02 15:27:28 -0400

    basic assembly guest example

---
+++ examples/asm-guest/host/src/main.rs
@@ -0,0 +1,45 @@
+// Copyright 2024 RISC Zero, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use risc0_zkvm::{compute_image_id, default_prover, ExecutorEnv};
+use std::fs;
+
+fn main() -> anyhow::Result<()> {
+    // Load built gcc program and compute it's image ID.
+    // TODO have the image ID be calculated at compile time, to avoid potential vulnerabilities
+    let consensus_elf = fs::read("./guest/out/fib")?;
+    let consensus_id = compute_image_id(&consensus_elf)?;
+    // Initialize tracing. In order to view logs, run `RUST_LOG=info cargo run`
+    tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::filter::EnvFilter::from_default_env())
+        .init();
+
+    let env = ExecutorEnv::builder()
+        .write_slice(&8u32.to_le_bytes())
+        .build()?;
+    let prover = default_prover();
+
+    // Produce a receipt by proving the specified ELF binary.
+    let receipt = prover.prove(env, &consensus_elf)?.receipt;
+
+    // Deserializing as by default this will just expect LE bytes
+    let value: u32 = receipt.journal.decode()?;
+    println!("value: {}", value);
+
+    // The receipt was verified at the end of proving, but the below code is an
+    // example of how someone else could verify this receipt.
+    receipt.verify(consensus_id)?;
+
+    Ok(())
+}
