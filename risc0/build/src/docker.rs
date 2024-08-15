70b45b10cc62378c3d9d7534481ad35e644250ae
Frank Laub <flaub@risc0.com>
2024-08-14 22:09:27 -0700

    cargo machete & cleanups (#2236)

--- risc0/build/src/docker.rs
+++ risc0/build/src/docker.rs
@@ -250,7 +250,7 @@ mod test {
         build("../../risc0/zkvm/methods/guest/Cargo.toml");
         compare_image_id(
             "risc0_zkvm_methods_guest/hello_commit",
-            "30bb779b4d5b22d1a2bf0e4d7ed9f89c89264440913e3478f2d084b0b4638c19",
+            "5a659b602e872ef22ad74493a29a66c8a40dd6589ccf7ac8445eaf9b1b955e81",
         );
     }
 }
