99a8eb2dce1852ea32ae94bdc46c886ce82518e5
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-08-21 20:47:54 -0400

    fix: fmt

--- website/src/components/Mermaid/index.js
+++ website/src/components/Mermaid/index.js
@@ -1,11 +1,9 @@
 import React, { useEffect, useId } from "react";
 import mermaid from "../../../node_modules/mermaid/dist/mermaid.esm.min.mjs";
 import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
-import { useColorMode } from "@docusaurus/theme-common";
 
 export default function Mermaid({ definition, height = 280 }) {
   const id = useId();
-  const isDarkTheme = useColorMode().colorMode === "dark";
 
   useEffect(() => {
     if (ExecutionEnvironment.canUseDOM) {
@@ -43,16 +41,13 @@ export default function Mermaid({ definition, height = 280 }) {
         })
         .catch(console.error);
     }
-  }, [isDarkTheme]);
+  }, []);
 
   return (
-    <>
-      <div>{isDarkTheme ? "Dark" : "Light"}</div>
-      <div
-        id={`${id}-mermaid`}
-        className="mermaid-graph"
-        style={{ height: Number(height), borderRadius: 12 }}
-      />
-    </>
+    <div
+      id={`${id}-mermaid`}
+      className="mermaid-graph"
+      style={{ height: Number(height), borderRadius: 12 }}
+    />
   );
 }
