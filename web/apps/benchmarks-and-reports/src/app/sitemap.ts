b59c5a9c0219a941bf2e711fda8eefd41eda6949
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-09-10 14:46:01 -0400

    fix: cleanup sitemap

--- web/apps/benchmarks-and-reports/src/app/sitemap.ts
+++ web/apps/benchmarks-and-reports/src/app/sitemap.ts
@@ -1,17 +1,34 @@
 import { VERSIONS } from "~/versions";
+import { APPLICATIONS_BENCHMARKS_FILENAMES_TO_TITLES } from "./(benchmarks-and-reports)/[version]/applications-benchmarks/[slug]/_utils/constants";
 
 export const baseUrl = "https://reports.risczero.com";
 
-// @TODO: add individual tabs for app benchmarks?
 export default async function sitemap() {
-  const routes = [
-    "/benchmarks",
+  const response = await fetch("https://risc0.github.io/ghpages/dev/bench/data.js", {
+    cache: "no-store",
+  });
+  const text = await response.text();
+  const data = JSON.parse(text.replace("window.BENCHMARK_DATA = ", "").trim());
+  const benchmarksSlugs = Object.keys(data.entries);
+
+  return [
+    // crates validation
     "/crates-validation",
-    ...VERSIONS.flatMap(({ value }) => [`/${value}`, `/${value}/applications-benchmarks`, `/${value}/datasheet`]),
+
+    // datasheet
+    ...VERSIONS.flatMap(({ value }) => [`/${value}`, `/${value}/datasheet`]),
+
+    // benchmarks
+    ...benchmarksSlugs.map((slug) => `/benchmarks/${slug}`),
+
+    // applications benchmarks
+    ...VERSIONS.flatMap(({ value }) =>
+      Object.keys(APPLICATIONS_BENCHMARKS_FILENAMES_TO_TITLES[value]).map(
+        (filename) => `/${value}/applications-benchmarks/${filename.replace(".csv", "")}`,
+      ),
+    ),
   ].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split("T")[0],
   }));
-
-  return [...routes];
 }
