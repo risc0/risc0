da74b771b8e0efd961fe3d9c63329f0d52ab31f8
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-09-26 16:52:01 -0400

    fix: lint

--- web/packages/shared/vitest.config.ts
+++ web/packages/shared/vitest.config.ts
@@ -4,11 +4,11 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-	plugins: [react()],
-	test: {
-		environment: "happy-dom",
-		globals: true,
-		restoreMocks: true,
-		setupFiles: "./setup-tests.ts",
-	},
+  plugins: [react()],
+  test: {
+    environment: "happy-dom",
+    globals: true,
+    restoreMocks: true,
+    setupFiles: "./setup-tests.ts",
+  },
 });
