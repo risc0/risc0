1c9179b82de998709be1b03362eafc08096abb30
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-10-18 09:24:56 -0400

    fix: build, bump next to 15 RC2

--- web/apps/benchmarks-and-reports/src/app/(benchmarks-and-reports)/[version]/applications-benchmarks/page.tsx
+++ web/apps/benchmarks-and-reports/src/app/(benchmarks-and-reports)/[version]/applications-benchmarks/page.tsx
@@ -2,13 +2,15 @@ import { redirect } from "next/navigation";
 import type { Version } from "~/types/version";
 import { getFirstApplicationBenchmark } from "./_utils/get-first-application-benchmark";
 
-export default function ApplicationsBenchmarksPage({
-  params,
-}: {
-  params: {
-    version: Version;
-  };
+export default async function ApplicationsBenchmarksPage(props: {
+	params: Promise<{
+		version: Version;
+	}>;
 }) {
-  // redirect to the first application benchmark
-  redirect(`/${params.version}/applications-benchmarks/${getFirstApplicationBenchmark(params.version)}`);
+	const params = await props.params;
+
+	// redirect to the first application benchmark
+	redirect(
+		`/${params.version}/applications-benchmarks/${getFirstApplicationBenchmark(params.version)}`,
+	);
 }
--- web/apps/benchmarks-and-reports/src/app/sitemap.ts
+++ web/apps/benchmarks-and-reports/src/app/sitemap.ts
@@ -5,28 +5,27 @@ import { fetchBenchmarks } from "./(benchmarks-and-reports)/benchmarks/_actions/
 export const baseUrl = "https://reports.risczero.com";
 
 export default async function sitemap() {
-	const data = await fetchBenchmarks();
-	const benchmarksSlugs = Object.keys(data.entries);
+  const data = await fetchBenchmarks();
+  const benchmarksSlugs = Object.keys(data.entries);
 
-	return [
-		// crates validation
-		"/crates-validation",
+  return [
+    // crates validation
+    "/crates-validation",
 
-		// datasheet
-		...VERSIONS.flatMap(({ value }) => [`/${value}`, `/${value}/datasheet`]),
+    // datasheet
+    ...VERSIONS.flatMap(({ value }) => [`/${value}`, `/${value}/datasheet`]),
 
-		// benchmarks
-		...benchmarksSlugs.map((slug) => `/benchmarks/${slug}`),
+    // benchmarks
+    ...benchmarksSlugs.map((slug) => `/benchmarks/${slug}`),
 
-		// applications benchmarks
-		...VERSIONS.flatMap(({ value }) =>
-			Object.keys(APPLICATIONS_BENCHMARKS_FILENAMES_TO_TITLES[value]).map(
-				(filename) =>
-					`/${value}/applications-benchmarks/${filename.replace(".csv", "")}`,
-			),
-		),
-	].map((route) => ({
-		url: `${baseUrl}${route}`,
-		lastModified: new Date().toISOString().split("T")[0],
-	}));
+    // applications benchmarks
+    ...VERSIONS.flatMap(({ value }) =>
+      Object.keys(APPLICATIONS_BENCHMARKS_FILENAMES_TO_TITLES[value]).map(
+        (filename) => `/${value}/applications-benchmarks/${filename.replace(".csv", "")}`,
+      ),
+    ),
+  ].map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date().toISOString().split("T")[0],
+  }));
 }
