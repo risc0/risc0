503563a78daa52d96ecdbb54876c49ebc5858c4d
nils m <nils@risczero.com>
2024-10-25 22:16:21 +0000

    update reproducible build hash

--- risc0/build/src/docker.rs
+++ risc0/build/src/docker.rs
@@ -265,7 +265,7 @@ mod test {
         build("../../risc0/zkvm/methods/guest/Cargo.toml");
         compare_image_id(
             "risc0_zkvm_methods_guest/hello_commit",
-            "c069e76f368118776f05ff35f7d02d02ca6eaf4a1abcf63e0df27d30ce024417",
+            "5e95ee67893f25399250181749cf26baab984b896c9ce04ac6e7cdf367e250f6",
         );
     }
 }
