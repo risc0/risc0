31597c27633350b7752a12ab8a2d73a238b27ca0
Tim Zerrell <tim.zerrell@risczero.com>
2024-10-24 22:35:17 -0700

    Remove bigint EC circuits, add unbatched RSA (#2443)
    Removes the current draft of bigint elliptic curve circuits, which won't
    be performant in their current form, and adds an unbatched RSA circuit.

--- risc0/circuit/bigint/src/rsa.rs
+++ risc0/circuit/bigint/src/rsa.rs
@@ -21,7 +21,7 @@ use num_bigint::BigUint;
 use crate::{BigIntClaim, BigIntProgram};
 
 // Re-export program info
-pub use crate::generated::{RSA_256_X1, RSA_256_X2, RSA_3072_X15};
+pub use crate::generated::{RSA_256_X1, RSA_256_X2, RSA_3072_X1, RSA_3072_X15};
 
 /// Construct a bigint claim that (S^e = M (mod N)), where e = 65537.
 pub fn claim(prog_info: &BigIntProgram, n: BigUint, s: BigUint, m: BigUint) -> BigIntClaim {
