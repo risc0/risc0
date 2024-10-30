54febb7df36f8406d9393cbc3184920a24e9db21
Frank Laub <flaub@risc0.com>
2024-10-30 09:38:05 -0700

    Split syscalls into separate modules (#2460)

---
+++ risc0/zkvm/src/host/server/exec/syscall/args.rs
@@ -0,0 +1,58 @@
+// Copyright 2024 RISC Zero, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::cmp::min;
+
+use anyhow::{anyhow, bail, Result};
+use risc0_zkvm_platform::{
+    syscall::{
+        nr::{SYS_ARGC, SYS_ARGV},
+        reg_abi::REG_A3,
+    },
+    WORD_SIZE,
+};
+
+use super::{Syscall, SyscallContext};
+
+#[derive(Clone)]
+pub(crate) struct SysArgs(pub Vec<String>);
+
+impl Syscall for SysArgs {
+    fn syscall(
+        &mut self,
+        syscall: &str,
+        ctx: &mut dyn SyscallContext,
+        to_guest: &mut [u32],
+    ) -> Result<(u32, u32)> {
+        if syscall == SYS_ARGC.as_str() {
+            Ok((self.0.len().try_into()?, 0))
+        } else if syscall == SYS_ARGV.as_str() {
+            // Get the arg or return an error if out of bounds.
+            let arg_index = ctx.load_register(REG_A3);
+            let arg_val = self.0.get(arg_index as usize).ok_or_else(|| {
+                anyhow!(
+                    "guest requested index {arg_index} from argv of len {}",
+                    self.0.len()
+                )
+            })?;
+
+            let nbytes = min(to_guest.len() * WORD_SIZE, arg_val.as_bytes().len());
+            let to_guest_u8s: &mut [u8] = bytemuck::cast_slice_mut(to_guest);
+            to_guest_u8s[0..nbytes].clone_from_slice(&arg_val.as_bytes()[0..nbytes]);
+            Ok((arg_val.as_bytes().len() as u32, 0))
+        } else {
+            bail!("Unknown syscall {syscall}")
+        }
+    }
+}
