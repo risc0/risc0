821b09d8f75140ad76d496f95361061c78a8bb3d
Tim Zerrell <tim.zerrell@risczero.com>
2024-10-30 10:08:40 -0700

    WIP Use RSA syscall

--- risc0/circuit/bigint/examples/rsa.rs
+++ risc0/circuit/bigint/examples/rsa.rs
@@ -17,6 +17,7 @@ use num_traits::Num as _;
 use risc0_circuit_bigint::zkr::register_zkrs;
 use risc0_circuit_bigint_test_methods::RSA_ELF;
 use risc0_zkvm::{ExecutorEnv, ExecutorImpl};
+use risc0_zkvm_platform::syscall::sys_rsa;
 
 fn main() {
     tracing_subscriber::fmt()
@@ -25,11 +26,39 @@ fn main() {
 
     register_zkrs();
 
+    // TODO: This whole thing is a bit hacky, clean up
     // Parameters for a message `m` with signature `s` under the RSA public key modulus `n`.
     let n = from_hex("9c98f9aacfc0b73c916a824db9afe39673dcb56c42dffe9de5b86d5748aca4d5");
     let s = from_hex("de67116c809a5cc876cebb5e8c72d998f983a4d61b499dd9ae23b789a7183677");
-    let m = from_hex("1fb897fac8aa8870b936631d3af1a17930c8af0ca4376b3056677ded52adf5aa");
-    let claims = vec![[n, s, m]];
+    let n_claim = n.clone();
+    let s_claim = s.clone();
+    // let m = from_hex("1fb897fac8aa8870b936631d3af1a17930c8af0ca4376b3056677ded52adf5aa");
+    // println!("The bitwidth of n is {}", n.bits());
+    let mut n = n.to_u32_digits();  // Ugh `mut` what a hack TODO
+    n.resize(96, 0); // TODO: need more care
+    let n: [u32; 96] = n.try_into().expect("modulus should fit in 96 32-bit words");
+    let mut s = s.to_u32_digits();  // Ugh `mut` what a hack TODO
+    s.resize(96, 0); // TODO: need more care
+    let s: [u32; 96] = s.try_into().expect("base should fit in 96 32-bit words");
+    // TODO: Feels a bit hacky
+    const fn zero() -> u32 {
+        0
+    }
+    let mut m = [zero(); 96];  // TODO: Feels a bit hacky
+    unsafe {
+        sys_rsa(&mut m, &s, &n);  // TODO: Actually manage the 3 inputs (first is buffer for output) and call
+    }
+    // let m_claim = BigUint::from_bytes_le(m.map(|elem| elem.to_le_bytes()).flatten());
+    // TODO: This code should be replaced with the `flatten` code above once `flatten` is stable
+    let m_words = m.map(|elem| elem.to_le_bytes());
+    let mut m_claim = Vec::<u8>::new();  // TODO: Clean up style
+    for word in m_words {
+        for byte in word {
+            m_claim.push(byte);
+        }
+    }
+    let m_claim = BigUint::from_bytes_le(&m_claim);
+    let claims = vec![[n_claim, s_claim, m_claim]];
 
     let env = ExecutorEnv::builder()
         .write(&claims)
--- risc0/circuit/bigint/src/rsa.rs
+++ risc0/circuit/bigint/src/rsa.rs
@@ -40,3 +40,65 @@ pub fn claim_dig(prog_info: &BigIntProgram, n: &BigUintDig, s: &BigUintDig, m: &
 
     BigIntClaim::from_biguints(prog_info, &[n, s, m])
 }
+
+
+// TODO: Better name
+/// Compute M = S^e (mod N), where e = 65537, using num-bigint-dig, and return the `claim` to prove this
+#[cfg(feature = "bigint-dig-shim")]
+pub fn claim_dig(prog_info: &BigIntProgram, n: &BigUintDig, s: &BigUintDig) -> Result<BigIntClaim> {
+    // TODO: Investigate how expensive this shim is
+    let mut n = n.to_u32_digits();
+    let mut s = s.to_u32_digits();
+    // TODO: don't use magic number 96
+    if (n.len() > 96 || s.len() > 96) {
+        return Err("TODO: Message for oversized inputs");
+    }
+    n.resize(96, 0);
+    s.resize(96, 0);
+    let n: [u32; 96] = n.try_into()?;
+    let s: [u32; 96] = s.try_into()?;
+    n.map(|elem| elem.to_le());
+    s.map(|elem| elem.to_le());
+    // TODO: Nicer way to provide an empty output buffer?
+    const fn zero() -> u32 { 0 }
+    let mut m = [zero(); 96];
+    // TODO: Write up the unsafe
+    unsafe {
+        sys_rsa(&mut m, &s, &n);
+    }
+
+            let n = BigUint::from_bytes_le(&n.to_bytes_le());
+            let s = BigUint::from_bytes_le(&s.to_bytes_le());
+
+                let n_claim = n.clone();
+                let s_claim = s.clone();
+                // let m = from_hex("1fb897fac8aa8870b936631d3af1a17930c8af0ca4376b3056677ded52adf5aa");
+                // println!("The bitwidth of n is {}", n.bits());
+                let mut n = n.to_u32_digits();  // Ugh `mut` what a hack TODO
+                n.resize(96, 0); // TODO: need more care
+                let n: [u32; 96] = n.try_into().expect("modulus should fit in 96 32-bit words");
+                let mut s = s.to_u32_digits();  // Ugh `mut` what a hack TODO
+                s.resize(96, 0); // TODO: need more care
+                let s: [u32; 96] = s.try_into().expect("base should fit in 96 32-bit words");
+                // TODO: Feels a bit hacky
+                const fn zero() -> u32 {
+                    0
+                }
+                let mut m = [zero(); 96];  // TODO: Feels a bit hacky
+                unsafe {
+                    sys_rsa(&mut m, &s, &n);  // TODO: Actually manage the 3 inputs (first is buffer for output) and call
+                }
+                // let m_claim = BigUint::from_bytes_le(m.map(|elem| elem.to_le_bytes()).flatten());
+                // TODO: This code should be replaced with the `flatten` code above once `flatten` is stable
+                let m_words = m.map(|elem| elem.to_le_bytes());
+                let mut m_claim = Vec::<u8>::new();  // TODO: Clean up style
+                for word in m_words {
+                    for byte in word {
+                        m_claim.push(byte);
+                    }
+                }
+                let m_claim = BigUint::from_bytes_le(&m_claim);
+                let claims = vec![[n_claim, s_claim, m_claim]];
+
+    BigIntClaim::from_biguints(prog_info, &[n, s, m])
+}
