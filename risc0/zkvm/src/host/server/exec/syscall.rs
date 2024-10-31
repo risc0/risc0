d478fa2a8349df9c7bd2d3ec79309e829532a8b2
Tim Zerrell <tim.zerrell@risczero.com>
2024-10-31 13:19:00 -0700

    Clean up RSA syscall comments

--- risc0/zkvm/src/host/server/exec/syscall.rs
+++ risc0/zkvm/src/host/server/exec/syscall.rs
@@ -243,42 +243,33 @@ impl Syscall for SysRandom {
 
 pub(crate) struct SysRSA;
 impl Syscall for SysRSA {
-    fn syscall(  // TODO: Wrong variables
+    fn syscall(
         &mut self,
         _syscall: &str,
         ctx: &mut dyn SyscallContext,
         to_guest: &mut [u32],
     ) -> Result<(u32, u32)> {
-        // TODO: Wrong code
         assert!(WORD_SIZE == 4);  // The inputs on the other side of this syscall assume u32 words
         tracing::debug!("SYS_RSA");
+        // Load inputs into BigUint
         let base_ptr = ByteAddr(ctx.load_register(REG_A3));
         let modulus_ptr = ByteAddr(ctx.load_register(REG_A4));
-        // TODO: WIDTH_BYTES type
         let base = ctx.load_region(base_ptr, rsa::WIDTH_BYTES.try_into()?)?;
         let modulus = ctx.load_region(modulus_ptr, rsa::WIDTH_BYTES.try_into()?)?;
-        // let mut result = vec![0u8; rsa::WIDTH_BYTES];   // TODO: This indirection may be unnecessary
-        // TODO: Calculate result here
         let base = BigUint::from_bytes_le(&base);
         let modulus = BigUint::from_bytes_le(&modulus);
+        // Compute result
         let result = base.modpow(&BigUint::from(rsa::RSA_EXPONENT), &modulus);
+        // `to_guest` should be exactly WIDTH_BYTES, so get the size right then copy into there
         let mut result = result.to_le_bytes();
+        // modpow shouldn't return higher than modulus, which fits in WIDTH_BYTES
         assert!(result.len() <= rsa::WIDTH_BYTES);
+        // Must return full width, even if result is smaller
         result.resize(rsa::WIDTH_BYTES, 0);
-        // TODO: End of result calc section
-        bytemuck::cast_slice_mut(to_guest).clone_from_slice(result.as_slice());   // TODO: This indirection may be unnecessary
+        bytemuck::cast_slice_mut(to_guest).clone_from_slice(result.as_slice());
         Ok((0, 0))
     }
 }
-
-
-
-
-
-
-
-
-
 
 #[derive(Clone)]
 pub(crate) struct Args(pub Vec<String>);
