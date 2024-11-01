a754afd9172742ba2007bcaa31b0e8959e33050e
Mars Saxman <mars@risczero.com>
2024-11-01 16:53:54 -0700

    remove unneeded/erroneous use alloc directive

--- risc0/circuit/rv32im/src/prove/bytecode/file.rs
+++ risc0/circuit/rv32im/src/prove/bytecode/file.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use alloc::vec::Vec;
 use num_traits::FromPrimitive;
 use std;
 use std::io::{Error, Read, Seek};
