23597a855f0949afa96b3d8080b27e19bd41f6a9
Mars Saxman <mars@risczero.com>
2024-11-01 16:45:37 -0700

    move bytecode evaluator into 'prove' subdirectory

--- risc0/circuit/rv32im/src/lib.rs
+++ risc0/circuit/rv32im/src/lib.rs
@@ -15,7 +15,6 @@
 #![doc = include_str!("../README.md")]
 #![cfg_attr(not(feature = "std"), no_std)]
 
-pub mod bytecode;
 pub mod control_id;
 mod info;
 pub mod layout;
--- risc0/circuit/rv32im/src/prove/mod.rs
+++ risc0/circuit/rv32im/src/prove/mod.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+pub mod bytecode;
 pub mod emu;
 pub mod engine;
 pub mod hal;
