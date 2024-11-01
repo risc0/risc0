56e23eeb007e0b05ac5f5fb8b8e0bc6ebf0e2299
Mars Saxman <mars@risczero.com>
2024-11-01 16:22:46 -0700

    add import for Vec

--- risc0/circuit/rv32im/src/bytecode/file.rs
+++ risc0/circuit/rv32im/src/bytecode/file.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use alloc::vec::Vec;
 use num_traits::FromPrimitive;
 use std;
 use std::io::{Error, Read, Seek};
