1e66182040f3784b2cbf6f326e63f396fd1672ba
Tim Zerrell <tim.zerrell@risczero.com>
2024-11-06 09:52:35 -0800

    Update reproducible image ID
    i.e. due to merge

--- risc0/build/src/docker.rs
+++ risc0/build/src/docker.rs
@@ -265,7 +265,7 @@ mod test {
         build("../../risc0/zkvm/methods/guest/Cargo.toml");
         compare_image_id(
             "risc0_zkvm_methods_guest/hello_commit",
-            "ab2f20249c1aba5f69f88bb2d4d454feab0208efb8407988b945c6518b07c771",
+            "85243297abf6d0a1009b83b20f37100c2a577723bccb59f5f213bb0dce3be3c5",
         );
     }
 }
