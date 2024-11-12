66781bbb0cb649ea04efb8e2defbdc72f83e870e
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-11-12 13:11:36 -0500

    fmt

--- web/apps/benchmarks-and-reports/src/app/layout.tsx
+++ web/apps/benchmarks-and-reports/src/app/layout.tsx
@@ -9,58 +9,42 @@ import type { PropsWithChildren } from "react";
 import { Providers } from "shared/client/providers/providers";
 
 export const metadata: Metadata = {
-	title: {
-		template: "%s | RISC Zero Benchmarks & Reports",
-		default: "RISC Zero Benchmarks & Reports",
-	},
-	keywords: [
-		"risczero",
-		"zero knowledge proofs",
-		"zkvm",
-		"benchmarks",
-		"zk",
-		"reports",
-		"risc0",
-		"risc0.com",
-	],
-	metadataBase: new URL("https://reports.risczero.com"),
-	description:
-		"Get to market fast with dramatically lower development costs on the first general purpose zkVM",
-	openGraph: {
-		images: [
-			{
-				url: "https://reports.risczero.com/api/og?title=RISC%20Zero%20Benchmarks%20%26%20Reports",
-			},
-		],
-	},
-	icons: [
-		{
-			rel: "icon",
-			url: "/favicon.png",
-		},
-	],
+  title: {
+    template: "%s | RISC Zero Benchmarks & Reports",
+    default: "RISC Zero Benchmarks & Reports",
+  },
+  keywords: ["risczero", "zero knowledge proofs", "zkvm", "benchmarks", "zk", "reports", "risc0", "risc0.com"],
+  metadataBase: new URL("https://reports.risczero.com"),
+  description: "Get to market fast with dramatically lower development costs on the first general purpose zkVM",
+  openGraph: {
+    images: [
+      {
+        url: "https://reports.risczero.com/api/og?title=RISC%20Zero%20Benchmarks%20%26%20Reports",
+      },
+    ],
+  },
+  icons: [
+    {
+      rel: "icon",
+      url: "/favicon.png",
+    },
+  ],
 };
 
 const fontMono = JetBrains_Mono({
-	subsets: ["latin"],
-	variable: "--font-jetbrains-mono",
+  subsets: ["latin"],
+  variable: "--font-jetbrains-mono",
 });
 
 export default function RootLayout({ children }: PropsWithChildren) {
-	return (
-		<html lang="en" suppressHydrationWarning className="h-full">
-			<body
-				className={cn("flex min-h-full flex-col font-sans", fontMono.variable)}
-			>
-				<Providers>{children}</Providers>
-				<SpeedInsights />
+  return (
+    <html lang="en" suppressHydrationWarning className="h-full">
+      <body className={cn("flex min-h-full flex-col font-sans", fontMono.variable)}>
+        <Providers>{children}</Providers>
+        <SpeedInsights />
 
-				<Script
-					defer
-					data-domain="reports.risczero.com"
-					src="https://plausible.io/js/script.outbound-links.js"
-				/>
-			</body>
-		</html>
-	);
+        <Script defer data-domain="reports.risczero.com" src="https://plausible.io/js/script.outbound-links.js" />
+      </body>
+    </html>
+  );
 }
