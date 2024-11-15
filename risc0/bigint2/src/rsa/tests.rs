86d0780fcedf9f0f6a5e98a6adc4ee8cbec62cc4
Austin Abell <austinabell8@gmail.com>
2024-11-15 16:02:51 -0500

    update test to split buffer overrun, add small mod test

--- risc0/bigint2/src/rsa/tests.rs
+++ risc0/bigint2/src/rsa/tests.rs
@@ -49,10 +49,31 @@ fn modpow_65537() {
 }
 
 #[test]
-fn modpow_65537_small_buffers() {
+fn modpow_65537_small_base() {
+    const MODULUS: &[u8] = b"a79633c36a2e9a91cac9505c300edb9a0aa8105f115856024fe27dac4910ab2fad99fe3700ddf532a3c86c5171d28177d55b3112fe73f46917a58006ac747ae396557fe84e62ba0cefd55858258e2aecd756a855ca7bd81be968fc3b908a310508828275065ab91b6f8f7964ef5684ee7fd5188c097a99323dfbe1302ab8723d97f9d1a30b89b9fb7504e8fe1e85f858ae2f07818ff8c89f7f9dbd051e52f5dae0eb33ab85d826906817f6712003c866f878874c503ec4ee02457a3c7012c6c77f66bfe23518d2eddbbe9fc3dbb8ad944d9a38e333c7caa86138f5121b89563067c4bac9c32a26cd7c21830315f8e1c5b3783104f24648f7bf8556500b04d8bf11bc7e777aa5e0653c5af9b085c2c08a16f7d2b4bc323d1b32a5cc8f4ab39f374d30efa01f8009aeb60358e5f22345fea8a123e46f394bcc669d183348b6eb29fa5f972ebe90aa64684973f4e67c0ea7d82c5a05c4a837a0a63c86b201c54cbcb1dbabf26a5bbd533b0ec91892716b7deb54c061d2e8696391f210e16b4714dd";
+
     let base = BigUint::from(1u32);
-    let modulus = BigUint::from(2_147_483_647u32);
+    let modulus = BigUint::parse_bytes(MODULUS, 16).unwrap();
     let expected = BigUint::from(1u32);
+
+    let session = execute_modpow_session(&base, &modulus).unwrap();
+    assert_eq!(session.exit_code, ExitCode::Halted(0));
+    let result: BigUint = session.journal.as_ref().unwrap().decode().unwrap();
+    assert_eq!(result, expected);
+
+    let prover = get_prover_server(&ProverOpts::fast()).unwrap();
+    prover
+        .prove_session(&VerifierContext::default(), &session)
+        .unwrap();
+}
+
+#[test]
+fn modpow_65537_small_mod() {
+    const BASE: &[u8] = b"00024f2f68423c422bb8b7b6ea3f3382a379e635f9501bd9e1f09922407b5c4326fb959eadd237ed80972767deeb416749b137fc5c80876126f154f2c129a04c05970e08396dbd5d1285b4fb3a63b80855101bc3bbc9d90dd0500efd79989267ab36d4e2a361761c6ea1e172c741ec59b0aee1f1956b3ba947ed0b0cc2d45b47c041ee2a47bcc53345bd4714831b5d3125ad9a940f8efcc30ae51fc37ee6e3e4e2bd5f43ff762d7dc23017cd67a56ed00f30f09661839a10c2a487ebc1314c5bf77f0f305d040649741f357b6bb478bd72864845c3b62691c6f80199c923d2efd2e9f75751e26dc26495d3ff848d53c0424b5451d644a9903fb8c93bab50227e59c4e05029c1c50d434bd368dc0880910548804093bdd59da572d5513e57c078354f73aa35ba72d39e22e351449849d158a0ec8bc0069cd09f3dbcb7f02c089a054c35dbf7469089a43c679c3eb7f972c6cd569dcc8bf2540b1188083b77c8517a635ea037ec1b46c0f1cec0770c8d25959f80ae0068656c6c6f20776f726c64";
+
+    let base = BigUint::parse_bytes(BASE, 16).unwrap();
+    let modulus = BigUint::from(2147483647u32);
+    let expected = BigUint::from(1205382585u32);
 
     let session = execute_modpow_session(&base, &modulus).unwrap();
     assert_eq!(session.exit_code, ExitCode::Halted(0));
