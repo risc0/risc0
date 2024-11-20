e2b9f4e9ce2ea79525d9f9bba562fc1abf1ba7fd
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-11-20 14:13:15 -0500

    fmt

--- website/src/components/Mermaid/index.js
+++ website/src/components/Mermaid/index.js
@@ -4,57 +4,57 @@ import mermaid from "mermaid";
 import React, { useEffect, useId } from "react";
 
 export default function Mermaid({ definition, height = 280 }) {
-	const id = useId();
-	const { isDarkTheme } = useColorMode();
+  const id = useId();
+  const { isDarkTheme } = useColorMode();
 
-	// biome-ignore lint/correctness/useExhaustiveDependencies: ignore
-	useEffect(() => {
-		if (ExecutionEnvironment.canUseDOM) {
-			import("svg-pan-zoom")
-				.then((module) => {
-					const svgPanZoom = module.default;
-					const drawDiagram = async () => {
-						const element = document.querySelector(
-							`#${CSS.escape(id)}-mermaid`,
-						);
+  // biome-ignore lint/correctness/useExhaustiveDependencies: ignore
+  useEffect(() => {
+    if (ExecutionEnvironment.canUseDOM) {
+      import("svg-pan-zoom")
+        .then((module) => {
+          const svgPanZoom = module.default;
+          const drawDiagram = async () => {
+            const element = document.querySelector(
+              `#${CSS.escape(id)}-mermaid`,
+            );
 
-						mermaid.initialize({
-							theme: isDarkTheme ? "dark" : "default",
-						});
+            mermaid.initialize({
+              theme: isDarkTheme ? "dark" : "default",
+            });
 
-						const { svg } = await mermaid.render(
-							"mermaid-svg-id",
-							definition.replace(/```mermaid/, ""),
-						);
-						element.innerHTML = svg.replace(/[ ]*max-width:[ 0-9\.]*px;/i, "");
+            const { svg } = await mermaid.render(
+              "mermaid-svg-id",
+              definition.replace(/```mermaid/, ""),
+            );
+            element.innerHTML = svg.replace(/[ ]*max-width:[ 0-9\.]*px;/i, "");
 
-						svgPanZoom(document.getElementById("mermaid-svg-id"), {
-							zoomEnabled: true,
-							controlIconsEnabled: true,
-							fit: true,
-							center: true,
-							dblClickZoomEnabled: false,
-						});
-					};
+            svgPanZoom(document.getElementById("mermaid-svg-id"), {
+              zoomEnabled: true,
+              controlIconsEnabled: true,
+              fit: true,
+              center: true,
+              dblClickZoomEnabled: false,
+            });
+          };
 
-					drawDiagram();
-				})
-				.catch(console.error);
-		}
-	}, [isDarkTheme]);
+          drawDiagram();
+        })
+        .catch(console.error);
+    }
+  }, [isDarkTheme]);
 
-	return (
-		<div
-			id={`${id}-mermaid`}
-			className="mermaid-graph"
-			style={{
-				height: Number(height),
-				borderRadius: 12,
-				cursor: "grab",
-				"&:active": {
-					cursor: "grabbing",
-				},
-			}}
-		/>
-	);
+  return (
+    <div
+      id={`${id}-mermaid`}
+      className="mermaid-graph"
+      style={{
+        height: Number(height),
+        borderRadius: 12,
+        cursor: "grab",
+        "&:active": {
+          cursor: "grabbing",
+        },
+      }}
+    />
+  );
 }
