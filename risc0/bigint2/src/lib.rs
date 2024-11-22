3a8c878ebf6e731ba09e3f1c04baaeade7d12ff9
Tim Zerrell <tim.zerrell@risczero.com>
2024-11-22 12:09:43 -0800

    Clean up unstable to modules per review

--- risc0/bigint2/src/lib.rs
+++ risc0/bigint2/src/lib.rs
@@ -12,8 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[stability::unstable]
 pub mod ec;
 pub mod ffi;
+#[stability::unstable]
 pub mod rsa;
 
 pub(crate) const WORD_SIZE: usize = 4;
