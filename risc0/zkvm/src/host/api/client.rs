f573a90aa4214ae54963657d88be202fd26f9833
Parker Thompson <parker@risczero.com>
2024-11-23 09:41:22 -0800

    Added mode to have client spoof pre versions

--- risc0/zkvm/src/host/api/client.rs
+++ risc0/zkvm/src/host/api/client.rs
@@ -508,10 +508,26 @@ impl Client {
         result
     }
 
+    fn get_client_version() -> Result<semver::Version> {
+        #[cfg(not(feature = "r0vm-ver-compat"))]
+        {
+            get_version().map_err(|err| anyhow!(err))
+        }
+        #[cfg(feature = "r0vm-ver-compat")]
+        {
+            let mut client_version = get_version().map_err(|err| anyhow!(err))?;
+            if !client_version.pre.is_empty() {
+                client_version.pre = semver::Prerelease::EMPTY;
+            }
+            client_version
+        };
+    }
+
     fn connect(&self) -> Result<ConnectionWrapper> {
         let mut conn = self.connector.connect()?;
 
-        let client_version = get_version().map_err(|err| anyhow!(err))?;
+        let client_version = Self::get_client_version()?;
+
         let request = pb::api::HelloRequest {
             version: Some(client_version.clone().into()),
         };
