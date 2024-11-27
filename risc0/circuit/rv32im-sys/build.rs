093f46925a0366d72df505256d35c54a033ddad0
Frank Laub <flaub@risc0.com>
2024-11-27 14:36:47 -0800

    Support for rv32im-v2 CUDA kernels (#2565)
    Co-authored-by: nils m <nils@risczero.com>
    Co-authored-by: Tim Zerrell <tim.zerrell@risczero.com>

--- risc0/circuit/rv32im-sys/build.rs
+++ risc0/circuit/rv32im-sys/build.rs
@@ -29,6 +29,7 @@ fn main() {
 }
 
 fn build_cpu_kernels() {
+    println!("cargo:rerun-if-changed=cxx");
     KernelBuild::new(KernelType::Cpp)
         .files([
             "cxx/bigint.cpp",
@@ -58,14 +59,7 @@ fn build_cuda_kernels() {
             "kernels/cuda/step_verify_bytes.cu",
             "kernels/cuda/step_verify_mem.cu",
         ])
-        .deps([
-            "kernels/cuda/bigint.cu",
-            "kernels/cuda/context.h",
-            "kernels/cuda/extern.h",
-            "kernels/cuda/extern.cuh",
-            "kernels/cuda/kernels.h",
-            "kernels/cuda/layout.cu.inc",
-        ])
+        .deps(["kernels/cuda"])
         .include(env::var("DEP_RISC0_SYS_CUDA_ROOT").unwrap())
         .include(env::var("DEP_RISC0_SYS_CXX_ROOT").unwrap())
         .include(env::var("DEP_SPPARK_ROOT").unwrap())
--- risc0/circuit/rv32im/src/prove/hal/cpp.rs
+++ risc0/circuit/rv32im/src/prove/hal/cpp.rs
@@ -22,7 +22,7 @@ use risc0_circuit_rv32im_sys::ffi::{
     risc0_circuit_string_free, risc0_circuit_string_ptr, RawAccumContext, RawError,
 };
 use risc0_core::field::baby_bear::{BabyBear, BabyBearElem, BabyBearExtElem};
-use risc0_zkp::{adapter::PolyFp, hal::cpu::SyncSlice};
+use risc0_zkp::{adapter::PolyFp, field::Elem, hal::cpu::SyncSlice};
 
 use crate::CircuitImpl;
 
@@ -45,7 +45,19 @@ impl PolyFp<BabyBear> for CircuitImpl {
         args: &[&[BabyBearElem]],
     ) -> BabyBearExtElem {
         let args: Vec<*const BabyBearElem> = args.iter().map(|x| (*x).as_ptr()).collect();
-        unsafe { risc0_circuit_rv32im_poly_fp(cycle, steps, mix.as_ptr(), args.as_ptr()) }
+        self.ffi_wrap(|err| unsafe {
+            let mut result = BabyBearExtElem::ZERO;
+            risc0_circuit_rv32im_poly_fp(
+                err,
+                cycle,
+                steps,
+                mix.as_ptr(),
+                args.as_ptr(),
+                &mut result,
+            );
+            result
+        })
+        .unwrap()
     }
 }
 
