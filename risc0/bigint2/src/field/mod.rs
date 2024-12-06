d3c121bdfc01f6784d2a3bc96ae1650bee613437
Tim Zerrell <tim.zerrell@risczero.com>
2024-12-05 16:00:35 -0800

    Add prime field bigint tests

--- risc0/bigint2/src/field/mod.rs
+++ risc0/bigint2/src/field/mod.rs
@@ -21,11 +21,15 @@ use crate::ffi::{sys_bigint2_3, sys_bigint2_4};
 use crate::WORD_SIZE;
 
 pub const FIELD_256_WIDTH_WORDS: usize = 256 / (WORD_SIZE * 8);
+pub const EXT_DEGREE_2: usize = 2;  // TODO
 
 const MODADD_256_BLOB: &[u8] = include_bytes_aligned!(4, "modadd_256.blob");
 const MODINV_256_BLOB: &[u8] = include_bytes_aligned!(4, "modinv_256.blob");
 const MODMUL_256_BLOB: &[u8] = include_bytes_aligned!(4, "modmul_256.blob");
 const MODSUB_256_BLOB: &[u8] = include_bytes_aligned!(4, "modsub_256.blob");
+const EXTFIELDADD_256_BLOB: &[u8] = include_bytes_aligned!(4, "extfieldadd_256.blob");
+const EXTFIELDMUL_256_BLOB: &[u8] = include_bytes_aligned!(4, "extfieldmul_256.blob");
+const EXTFIELDSUB_256_BLOB: &[u8] = include_bytes_aligned!(4, "extfieldsub_256.blob");
 
 pub fn modadd_256(
     lhs: &[u32; FIELD_256_WIDTH_WORDS],
@@ -85,6 +89,57 @@ pub fn modsub_256(
     unsafe {
         sys_bigint2_4(
             MODSUB_256_BLOB.as_ptr(),
+            lhs.as_ptr() as *const u32,
+            rhs.as_ptr() as *const u32,
+            prime.as_ptr() as *const u32,
+            result.as_mut_ptr() as *mut u32,
+        );
+    }
+}
+
+pub fn extfieldadd_256(
+    lhs: &[[u32; FIELD_256_WIDTH_WORDS]; EXT_DEGREE_2],
+    rhs: &[[u32; FIELD_256_WIDTH_WORDS]; EXT_DEGREE_2],
+    prime: &[u32; FIELD_256_WIDTH_WORDS],
+    result: &mut [[u32; FIELD_256_WIDTH_WORDS]; EXT_DEGREE_2],
+) {
+    unsafe {
+        sys_bigint2_4(
+            EXTFIELDADD_256_BLOB.as_ptr(),
+            lhs.as_ptr() as *const u32,
+            rhs.as_ptr() as *const u32,
+            prime.as_ptr() as *const u32,
+            result.as_mut_ptr() as *mut u32,
+        );
+    }
+}
+
+pub fn extfieldmul_256(
+    lhs: &[[u32; FIELD_256_WIDTH_WORDS]; EXT_DEGREE_2],
+    rhs: &[[u32; FIELD_256_WIDTH_WORDS]; EXT_DEGREE_2],
+    prime: &[u32; FIELD_256_WIDTH_WORDS],
+    result: &mut [[u32; FIELD_256_WIDTH_WORDS]; EXT_DEGREE_2],
+) {
+    unsafe {
+        sys_bigint2_4(
+            EXTFIELDMUL_256_BLOB.as_ptr(),
+            lhs.as_ptr() as *const u32,
+            rhs.as_ptr() as *const u32,
+            prime.as_ptr() as *const u32,
+            result.as_mut_ptr() as *mut u32,
+        );
+    }
+}
+
+pub fn extfieldsub_256(
+    lhs: &[[u32; FIELD_256_WIDTH_WORDS]; EXT_DEGREE_2],
+    rhs: &[[u32; FIELD_256_WIDTH_WORDS]; EXT_DEGREE_2],
+    prime: &[u32; FIELD_256_WIDTH_WORDS],
+    result: &mut [[u32; FIELD_256_WIDTH_WORDS]; EXT_DEGREE_2],
+) {
+    unsafe {
+        sys_bigint2_4(
+            EXTFIELDSUB_256_BLOB.as_ptr(),
             lhs.as_ptr() as *const u32,
             rhs.as_ptr() as *const u32,
             prime.as_ptr() as *const u32,
