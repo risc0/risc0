d02b9c62e49497a7d328e65afb4639a881c8243d
Jeremy Bruestle <jeremy@risczero.com>
2024-12-06 16:37:05 -0800

    Fix a bug in ConstExt (#2621)
    Current version has a bug where by reinterpreting u32s, they end up in
    Montgomery form: but actually they are coming for the user in normal
    form.
    
    ---------
    
    Co-authored-by: Frank Laub <flaub@risc0.com>

--- risc0/build/src/docker.rs
+++ risc0/build/src/docker.rs
@@ -265,7 +265,7 @@ mod test {
         build("../../risc0/zkvm/methods/guest/Cargo.toml");
         compare_image_id(
             "risc0_zkvm_methods_guest/hello_commit",
-            "e153f356b12b6ddc7592db48651e135e2a3a0abec26cf4a17560eddc60ddd964",
+            "8ee3c22783c3a07af15342bb85c0b9df3d1e0cb402c9ed34e503e32eb81d52bb",
         );
     }
 }
