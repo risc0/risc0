f944ecc5fc52324133e7fbfa9483054cdf8edf44
Tim Zerrell <tim.zerrell@risczero.com>
2024-12-13 13:22:11 -0800

    Drop field extension code (for now)

--- risc0/bigint2/src/field/mod.rs
+++ risc0/bigint2/src/field/mod.rs
@@ -21,15 +21,11 @@ use crate::ffi::{sys_bigint2_3, sys_bigint2_4};
 use crate::WORD_SIZE;
 
 pub const FIELD_256_WIDTH_WORDS: usize = 256 / (WORD_SIZE * 8);
-pub const EXT_DEGREE_2: usize = 2;  // TODO
 
 const MODADD_256_BLOB: &[u8] = include_bytes_aligned!(4, "modadd_256.blob");
 const MODINV_256_BLOB: &[u8] = include_bytes_aligned!(4, "modinv_256.blob");
 const MODMUL_256_BLOB: &[u8] = include_bytes_aligned!(4, "modmul_256.blob");
 const MODSUB_256_BLOB: &[u8] = include_bytes_aligned!(4, "modsub_256.blob");
-const EXTFIELDADD_256_BLOB: &[u8] = include_bytes_aligned!(4, "extfieldadd_256.blob");
-const EXTFIELDMUL_256_BLOB: &[u8] = include_bytes_aligned!(4, "extfieldmul_256.blob");
-const EXTFIELDSUB_256_BLOB: &[u8] = include_bytes_aligned!(4, "extfieldsub_256.blob");
 
 // TODO: All of these need guards against returning results >= prime/modulus
 
@@ -148,56 +144,4 @@ pub fn modsub_256(
     // An honest host will always return a result less than the modulus. A dishonest prover can
     // sometimes return a result greater than the modulus, so enforce that we're in the honest case.
     assert!(crate::is_less(&result, &modulus));
-}
-
-// TODO: Make it clearer that these functions are for degree 2 extensions
-pub fn extfieldadd_256(
-    lhs: &[[u32; FIELD_256_WIDTH_WORDS]; EXT_DEGREE_2],
-    rhs: &[[u32; FIELD_256_WIDTH_WORDS]; EXT_DEGREE_2],
-    modulus: &[u32; FIELD_256_WIDTH_WORDS],
-    result: &mut [[u32; FIELD_256_WIDTH_WORDS]; EXT_DEGREE_2],
-) {
-    unsafe {
-        sys_bigint2_4(
-            EXTFIELDADD_256_BLOB.as_ptr(),
-            lhs.as_ptr() as *const u32,
-            rhs.as_ptr() as *const u32,
-            modulus.as_ptr() as *const u32,
-            result.as_mut_ptr() as *mut u32,
-        );
-    }
-}
-
-pub fn extfieldmul_256(
-    lhs: &[[u32; FIELD_256_WIDTH_WORDS]; EXT_DEGREE_2],
-    rhs: &[[u32; FIELD_256_WIDTH_WORDS]; EXT_DEGREE_2],
-    modulus: &[u32; FIELD_256_WIDTH_WORDS],
-    result: &mut [[u32; FIELD_256_WIDTH_WORDS]; EXT_DEGREE_2],
-) {
-    unsafe {
-        sys_bigint2_4(
-            EXTFIELDMUL_256_BLOB.as_ptr(),
-            lhs.as_ptr() as *const u32,
-            rhs.as_ptr() as *const u32,
-            modulus.as_ptr() as *const u32,
-            result.as_mut_ptr() as *mut u32,
-        );
-    }
-}
-
-pub fn extfieldsub_256(
-    lhs: &[[u32; FIELD_256_WIDTH_WORDS]; EXT_DEGREE_2],
-    rhs: &[[u32; FIELD_256_WIDTH_WORDS]; EXT_DEGREE_2],
-    modulus: &[u32; FIELD_256_WIDTH_WORDS],
-    result: &mut [[u32; FIELD_256_WIDTH_WORDS]; EXT_DEGREE_2],
-) {
-    unsafe {
-        sys_bigint2_4(
-            EXTFIELDSUB_256_BLOB.as_ptr(),
-            lhs.as_ptr() as *const u32,
-            rhs.as_ptr() as *const u32,
-            modulus.as_ptr() as *const u32,
-            result.as_mut_ptr() as *mut u32,
-        );
-    }
 }
--- risc0/bigint2/src/field/tests.rs
+++ risc0/bigint2/src/field/tests.rs
@@ -17,7 +17,7 @@ extern crate num_bigint_dig as num_bigint;
 
 use num_bigint::BigUint;
 
-use risc0_bigint2_methods::{EXTFIELDADD_ELF, MODADD_ELF, MODINV_ELF, MODMUL_ELF, MODSUB_ELF};
+use risc0_bigint2_methods::{MODADD_ELF, MODINV_ELF, MODMUL_ELF, MODSUB_ELF};
 use risc0_zkvm::{
     get_prover_server, ExecutorEnv, ExecutorImpl, ExitCode, ProverOpts, VerifierContext,
 };
@@ -151,48 +151,6 @@ fn modsub() {
         .unwrap();
     assert_eq!(session.exit_code, ExitCode::Halted(0));
     let result: BigUint = session.journal.as_ref().unwrap().decode().unwrap();
-    assert_eq!(result, expected);
-
-    let prover = get_prover_server(&ProverOpts::fast()).unwrap();
-    let prove_info = prover
-        .prove_session(&VerifierContext::default(), &session)
-        .unwrap();
-    let elapsed = now.elapsed();
-    tracing::info!("Runtime: {}", elapsed.as_millis());
-    tracing::info!("User cycles: {}", prove_info.stats.user_cycles);
-}
-
-#[test]
-fn extfieldadd() {
-    const LHS0: &[u8] = b"04";
-    const LHS1: &[u8] = b"06";
-    const RHS0: &[u8] = b"03";
-    const RHS1: &[u8] = b"04";
-    const PRIME: &[u8] = b"07";
-    const EXPECTED0: &[u8] = b"00";
-    const EXPECTED1: &[u8] = b"03";
-
-    let lhs0 = BigUint::parse_bytes(LHS0, 16).unwrap();
-    let lhs1 = BigUint::parse_bytes(LHS1, 16).unwrap();
-    let rhs0 = BigUint::parse_bytes(RHS0, 16).unwrap();
-    let rhs1 = BigUint::parse_bytes(RHS1, 16).unwrap();
-    let prime = BigUint::parse_bytes(PRIME, 16).unwrap();
-    let expected0 = BigUint::parse_bytes(EXPECTED0, 16).unwrap();
-    let expected1 = BigUint::parse_bytes(EXPECTED1, 16).unwrap();
-    let expected = (expected0, expected1);
-
-    let env = ExecutorEnv::builder()
-            .write(&(lhs0, lhs1, rhs0, rhs1, prime))
-            .unwrap()
-            .build()
-            .unwrap();
-    let now = Instant::now();
-    let session = ExecutorImpl::from_elf(env, EXTFIELDADD_ELF)
-        .unwrap()
-        .run()
-        .unwrap();
-    assert_eq!(session.exit_code, ExitCode::Halted(0));
-    let result: (BigUint, BigUint) = session.journal.as_ref().unwrap().decode().unwrap();
     assert_eq!(result, expected);
 
     let prover = get_prover_server(&ProverOpts::fast()).unwrap();
