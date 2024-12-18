9e57f5e1a4d5ac99f4e671bc72687a902ff84fc6
Frank Laub <frank@risczero.com>
2024-12-18 01:54:15 +0000

    Clippy

--- risc0/circuit/rv32im-v2/src/prove/witgen/preflight.rs
+++ risc0/circuit/rv32im-v2/src/prove/witgen/preflight.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::u32;
-
 use anyhow::{anyhow, bail, Result};
 use derive_more::Debug;
 use num_traits::FromPrimitive as _;
