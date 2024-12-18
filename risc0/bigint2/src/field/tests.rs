7f0c62077d25778db82b7aecf69256e10c1e8e88
iddo <iddo@risczero.com>
2024-12-17 16:04:31 -0800

    added test for extfield subtract

--- risc0/bigint2/src/field/tests.rs
+++ risc0/bigint2/src/field/tests.rs
@@ -17,7 +17,7 @@ extern crate num_bigint_dig as num_bigint;
 
 use num_bigint::BigUint;
 
-use risc0_bigint2_methods::{EXTFIELDADD_ELF, MODADD_ELF, MODINV_ELF, MODMUL_ELF, MODSUB_ELF};
+use risc0_bigint2_methods::{EXTFIELDSUB_ELF, EXTFIELDADD_ELF, MODADD_ELF, MODINV_ELF, MODMUL_ELF, MODSUB_ELF};
 use risc0_zkvm::{
     get_prover_server, ExecutorEnv, ExecutorImpl, ExitCode, ProverOpts, VerifierContext,
 };
@@ -188,6 +188,48 @@ fn extfieldadd() {
             .unwrap();
     let now = Instant::now();
     let session = ExecutorImpl::from_elf(env, EXTFIELDADD_ELF)
+        .unwrap()
+        .run()
+        .unwrap();
+    assert_eq!(session.exit_code, ExitCode::Halted(0));
+    let result: (BigUint, BigUint) = session.journal.as_ref().unwrap().decode().unwrap();
+    assert_eq!(result, expected);
+
+    let prover = get_prover_server(&ProverOpts::fast()).unwrap();
+    let prove_info = prover
+        .prove_session(&VerifierContext::default(), &session)
+        .unwrap();
+    let elapsed = now.elapsed();
+    tracing::info!("Runtime: {}", elapsed.as_millis());
+    tracing::info!("User cycles: {}", prove_info.stats.user_cycles);
+}
+
+#[test]
+fn extfieldsub() {
+    const LHS0: &[u8] = b"02";
+    const LHS1: &[u8] = b"06";
+    const RHS0: &[u8] = b"03";
+    const RHS1: &[u8] = b"02";
+    const PRIME: &[u8] = b"07";
+    const EXPECTED0: &[u8] = b"06";
+    const EXPECTED1: &[u8] = b"04";
+
+    let lhs0 = BigUint::parse_bytes(LHS0, 16).unwrap();
+    let lhs1 = BigUint::parse_bytes(LHS1, 16).unwrap();
+    let rhs0 = BigUint::parse_bytes(RHS0, 16).unwrap();
+    let rhs1 = BigUint::parse_bytes(RHS1, 16).unwrap();
+    let prime = BigUint::parse_bytes(PRIME, 16).unwrap();
+    let expected0 = BigUint::parse_bytes(EXPECTED0, 16).unwrap();
+    let expected1 = BigUint::parse_bytes(EXPECTED1, 16).unwrap();
+    let expected = (expected0, expected1);
+
+    let env = ExecutorEnv::builder()
+            .write(&(lhs0, lhs1, rhs0, rhs1, prime))
+            .unwrap()
+            .build()
+            .unwrap();
+    let now = Instant::now();
+    let session = ExecutorImpl::from_elf(env, EXTFIELDSUB_ELF)
         .unwrap()
         .run()
         .unwrap();
