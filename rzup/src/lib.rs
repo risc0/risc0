6da598c7d19e775a6588c9bfe436b87ea31ac6d4
bobbobbio <bobbobbio@gmail.com>
2025-01-17 13:59:22 -0800

    Remove the "cli" feature.
    The idea behind this was good, but it had problems working correctly.
    The crate doesn't currently compile without the feature.
    
    For now, lets just not worry about the CLI only stuff being compiled
    into the library.

--- rzup/src/lib.rs
+++ rzup/src/lib.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 mod build;
-#[cfg(feature = "cli")]
 pub mod cli;
 mod components;
 mod distribution;
