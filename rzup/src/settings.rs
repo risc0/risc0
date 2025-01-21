66cb6c008d7eecf6700bd46826666581a65756f0
bobbobbio <bobbobbio@gmail.com>
2025-01-21 09:57:53 -0800

    When reading settings.toml ensure we consider future versions

--- rzup/src/settings.rs
+++ rzup/src/settings.rs
@@ -23,10 +23,11 @@ use std::collections::HashMap;
 use std::fs;
 
 #[derive(Debug, Serialize, Deserialize, Default, Clone)]
-#[serde(deny_unknown_fields)]
 pub struct Settings {
     #[serde(default)]
     default_versions: HashMap<String, String>,
+    #[serde(flatten)]
+    unknown_settings: toml::Table,
 }
 
 impl Settings {
@@ -49,8 +50,15 @@ impl Settings {
         let contents = fs::read_to_string(settings_path)
             .map_err(|e| RzupError::Environment(format!("Failed to read settings file: {e}")))?;
 
-        toml::from_str(&contents)
-            .map_err(|e| RzupError::Other(format!("invalid TOML in settings.toml file: {e}")))
+        let settings: Self = toml::from_str(&contents)
+            .map_err(|e| RzupError::Other(format!("invalid TOML in settings.toml file: {e}")))?;
+
+        for table_name in settings.unknown_settings.keys() {
+            env.emit(RzupEvent::Print {
+                message: format!("! Warning: settings.toml: unknown table `{table_name}`"),
+            })
+        }
+        Ok(settings)
     }
 
     pub(crate) fn save(&self, env: &Environment) -> Result<()> {
@@ -89,8 +97,7 @@ mod tests {
     use super::*;
     use tempfile::TempDir;
 
-    #[test]
-    fn test_settings_save_and_load() {
+    fn test_env() -> (TempDir, Environment) {
         let tmp_dir = TempDir::new().unwrap();
         let env = Environment::with_paths_and_token(
             tmp_dir.path().join(".risc0"),
@@ -99,6 +106,12 @@ mod tests {
             None,
         )
         .unwrap();
+        (tmp_dir, env)
+    }
+
+    #[test]
+    fn test_settings_save_and_load() {
+        let (_tmp_dir, env) = test_env();
         let mut settings = Settings::default();
 
         // Add some test data
@@ -118,14 +131,7 @@ mod tests {
 
     #[test]
     fn test_settings_load_invalid() {
-        let tmp_dir = TempDir::new().unwrap();
-        let env = Environment::with_paths_and_token(
-            tmp_dir.path().join(".risc0"),
-            tmp_dir.path().join(".rustup"),
-            tmp_dir.path().join(".cargo"),
-            None,
-        )
-        .unwrap();
+        let (_tmp_dir, env) = test_env();
 
         std::fs::write(env.settings_path(), "foobar").unwrap();
         let error_str = Settings::load(&env).unwrap_err().to_string();
@@ -133,14 +139,71 @@ mod tests {
             error_str.starts_with("invalid TOML in settings.toml file: TOML parse error at line 1"),
             "actual = {error_str}"
         );
+    }
+
+    fn run_and_assert_events<RetT>(
+        env: &mut Environment,
+        body: impl FnOnce(&mut Environment) -> RetT,
+        expected_events: Vec<RzupEvent>,
+    ) -> RetT {
+        let (event_send, event_recv) = std::sync::mpsc::channel();
+        env.set_event_handler(move |event| {
+            event_send.send(event).unwrap();
+        });
+
+        let ret = body(env);
+        env.set_event_handler(|_| {});
+
+        let mut events = vec![];
+        while let Ok(event) = event_recv.recv() {
+            if !matches!(event, RzupEvent::Debug { .. }) {
+                events.push(event);
+            }
+        }
+        assert_eq!(events, expected_events);
+
+        ret
+    }
+
+    /// A future version of `rzup` might have other sections, in that scenario we want to ignore
+    /// them, but produce a warning.
+    #[test]
+    fn unknown_section() {
+        let (_tmp_dir, mut env) = test_env();
 
         std::fs::write(env.settings_path(), "[other]\na = 12").unwrap();
-        let error_str = Settings::load(&env).unwrap_err().to_string();
-        assert!(
-            error_str.starts_with("invalid TOML in settings.toml file: TOML parse error at line 1")
-                && error_str.contains("unknown field `other`, expected `default_versions`"),
-            "actual = {error_str}"
+        let settings: Settings = run_and_assert_events(
+            &mut env,
+            |env| Settings::load(&env).unwrap(),
+            vec![RzupEvent::Print {
+                message: "! Warning: settings.toml: unknown table `other`".into(),
+            }],
         );
+
+        // The produced settings should be default
+        for c in Component::iter() {
+            assert_eq!(settings.get_default_version(&c), None);
+        }
+    }
+
+    /// A future version of `rzup` might have components unknown to this version, in that scenario
+    /// we want to make sure we ignore them.
+    #[test]
+    fn unknown_component_is_ignored() {
+        let (_tmp_dir, env) = test_env();
+
+        std::fs::write(
+            env.settings_path(),
+            "[default_versions]\n\
+            rust = \"1.81.0\"\n\
+            foobar = \"1.3.4\"\n",
+        )
+        .unwrap();
+        let settings = Settings::load(&env).unwrap();
+        let version = settings
+            .get_default_version(&Component::RustToolchain)
+            .unwrap();
+        assert_eq!(version, Version::new(1, 81, 0));
     }
 
     #[test]
