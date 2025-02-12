27be4be8c03c2f8b7a1e897fea312b6808127270
Tim Zerrell <tim.zerrell@risczero.com>
2025-02-12 11:37:15 -0800

    Refactor to use base pts to make logic clearer

--- examples/bn254/methods/guest/src/bin/bn254_verify.rs
+++ examples/bn254/methods/guest/src/bin/bn254_verify.rs
@@ -17,28 +17,23 @@ use risc0_zkvm::guest::env;
 
 fn main() {
     // TODO: switch away from example inputs
-    let inp: Vec<(Vec<u8>, Vec<u8>, Vec<u8>, Vec<u8>, Vec<u8>, Vec<u8>, Vec<u8>)> = env::read();
+    let inp: (Vec<u8>, Vec<u8>, Vec<u8>, Vec<u8>, Vec<u8>, Vec<u8>, Vec<u8>, Vec<u8>) = env::read();
+    let g1x = Fq::from_slice(&inp.0).expect("Normalized Fq expected for g1x");
+    let g1y = Fq::from_slice(&inp.1).expect("Normalized Fq expected for g1y");
+    let g2x_i = Fq::from_slice(&inp.2).expect("Normalized Fq expected for g2x_r");
+    let g2x_r = Fq::from_slice(&inp.3).expect("Normalized Fq expected for g2x_i");
+    let g2x = Fq2::new(g2x_r, g2x_i);
+    let g2y_i = Fq::from_slice(&inp.4).expect("Normalized Fq expected for g2y_r");
+    let g2y_r = Fq::from_slice(&inp.5).expect("Normalized Fq expected for g2y_i");
+    let g2y = Fq2::new(g2y_r, g2y_i);
+    let g1 = G1::from(AffineG1::new(g1x, g1y).expect("Point on G1 expected"));
+    let g2 = G2::from(AffineG2::new(g2x, g2y).expect("Point on G2 expected"));
+    let a_factor = Fr::from_slice(&inp.6).expect("Scalar factor expected");
+    let b_factor = Fr::from_slice(&inp.7).expect("Scalar factor expected");
+
     let mut pairs = Vec::new();
-    for entry in inp {
-        let g1x = Fq::from_slice(&entry.0).expect("Normalized Fq expected for g1x");
-        let g1y = Fq::from_slice(&entry.1).expect("Normalized Fq expected for g1y");
-        let g2x_i = Fq::from_slice(&entry.2).expect("Normalized Fq expected for g2x_r");
-        let g2x_r = Fq::from_slice(&entry.3).expect("Normalized Fq expected for g2x_i");
-        let g2x = Fq2::new(g2x_r, g2x_i);
-        let g2y_i = Fq::from_slice(&entry.4).expect("Normalized Fq expected for g2y_r");
-        let g2y_r = Fq::from_slice(&entry.5).expect("Normalized Fq expected for g2y_i");
-        let g2y = Fq2::new(g2y_r, g2y_i);
-        let g1 = G1::from(AffineG1::new(g1x, g1y).expect("Point on G1 expected"));
-        let g2 = G2::from(AffineG2::new(g2x, g2y).expect("Point on G2 expected"));
-        // TODO
-        let scalar = Fr::from_slice(&entry.6).expect("Scalar factor expected");
-        let scalar_inv = scalar.inverse().expect("Scalar is invertible");
-        let g1 = g1 * scalar;
-        let g2 = g2 * scalar_inv;
-        // / TODO
-        pairs.push((g1, g2));
-        // break;  // TODO
-    }
+    pairs.push((g1 * a_factor, g2 * b_factor));
+    pairs.push((g1, g2 * (-a_factor * b_factor)));
     let result = pairing_batch(&pairs);
     
     env::commit(&(result == Gt::one()));
