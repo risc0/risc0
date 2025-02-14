8b34f1c4de7d0dd93f0ebd3808a806eb4adfdefc
Erik Kaneda <erik@risczero.com>
2025-02-14 13:12:53 -0800

    wip: increase to 10k

--- risc0/circuit/rv32im-v2/src/lib.rs
+++ risc0/circuit/rv32im-v2/src/lib.rs
@@ -36,7 +36,7 @@ use self::zirgen::circuit::{Val, LAYOUT_GLOBAL};
 
 pub use self::zirgen::CircuitImpl;
 
-pub const MAX_INSN_CYCLES: usize = 2000; // TODO(flaub): calculate actual value
+pub const MAX_INSN_CYCLES: usize = 10000; // TODO(flaub): calculate actual value
 
 pub fn verify(seal: &[u32]) -> Result<(), VerificationError> {
     tracing::debug!("verify");
