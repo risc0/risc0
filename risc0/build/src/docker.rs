a37c33caf1f442d4146bdd78abda19274037fde4
Erik Kaneda <erik@risczero.com>
2025-02-24 20:49:05 -0800

    zkvm: bump version to 1.3.0 risc0-build: bump version to 2.0 (#2876)

--- risc0/build/src/docker.rs
+++ risc0/build/src/docker.rs
@@ -263,7 +263,7 @@ mod test {
         compare_image_id(
             &guest_list,
             "hello_commit",
-            "0048be2918d7dd3ccaa7c4dc318851c051042cb88fdcca2a89b3687b18c1c4a3",
+            "0a895ddb355382dcf1f7a39b16f30a8d7fcc3948b1fb1ad50874b24e0cf18666",
         );
     }
 }
