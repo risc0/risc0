bb11a1aa89e5a696a4e778fc31bbbdc191ae9207
Mars Saxman <mars@risczero.com>
2025-03-20 13:56:13 -0700

    remove x86 check, since the docker image no longer requires it

--- risc0/groth16/src/docker.rs
+++ risc0/groth16/src/docker.rs
@@ -20,7 +20,6 @@
 //! [gnark]: https://github.com/Consensys/gnark
 
 use std::{
-    env::consts::ARCH,
     path::Path,
     process::{Command, Stdio},
 };
@@ -33,9 +32,6 @@ use crate::{to_json, ProofJson, Seal};
 /// Groth16 a given seal of an `identity_p254` receipt into a Groth16 `Seal`.
 /// Requires running Docker on an x86 architecture.
 pub fn stark_to_snark(identity_p254_seal_bytes: &[u8]) -> Result<Seal> {
-    if !is_x86_architecture() {
-        bail!("stark_to_snark is only supported on x86 architecture.")
-    }
     if !is_docker_installed() {
         bail!("Please install docker first.")
     }
@@ -81,8 +77,4 @@ fn is_docker_installed() -> bool {
         .output()
         .map(|output| output.status.success())
         .unwrap_or(false)
-}
-
-fn is_x86_architecture() -> bool {
-    ARCH == "x86_64" || ARCH == "x86"
 }
