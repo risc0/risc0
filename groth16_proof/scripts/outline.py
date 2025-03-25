36a7c895f7fdefbe268a9a671a1bf365d0e9b5fc
Mars Saxman <mars@risczero.com>
2025-03-25 13:46:02 -0700

    unwrap large function to struct instead of using global vars

--- groth16_proof/scripts/outline.py
+++ groth16_proof/scripts/outline.py
@@ -16,11 +16,11 @@ source_file = sys.argv[1]
 dest_base, dest_ext = os.path.splitext(source_file)
 dest_dir = os.path.dirname(dest_base)
 global_decls = []
-global_defs = []
 main = []
-chunkdecls = []
 cur_chunk = []
+step_count = 0
 func_name = ""
+struct_name = ""
 with open(source_file, "r") as input:
     nesting = 0
     for line in input:
@@ -33,6 +33,9 @@ with open(source_file, "r") as input:
                 # top-level function declaration
                 main.append(line)
                 func_name = line.split(' ',1)[1].split('(',1)[0]
+                struct_name = f"{func_name}_state"
+                global_decls.append(f"struct {struct_name} {{\n")
+                main.append(f"{struct_name} state;\n")
             elif line.startswith("}"):
                 main.append(line)
             else:
@@ -40,35 +43,36 @@ with open(source_file, "r") as input:
                 stmt = line.rstrip('\n;')
                 type, decl = stmt.split(' ',1)
                 ident = decl.split(' ',1)[0]
-                global_decls.append(f"extern {type} {ident};\n")
+                global_decls.append(f"{type} {ident};\n")
                 if "=" in decl:
-                    main.append(f"{decl};\n")
-                global_defs.append(f"{type} {ident};\n")
+                    main.append(f"state.{decl};\n")
         else:
             cur_chunk.append(line)
             # If this line ended a block, and the chunk is big enough,
             # write it out and start the next chunk.
             if 2 == nesting and line.startswith("}") and len(cur_chunk) >= 8192:
-                chunkname = f"{func_name}_{len(chunkdecls)}"
+                step_count += 1
+                step_name = f"step_{step_count}"
+                method_name = f"{struct_name}::{step_name}"
                 param_decls = "(uint ctx_index,Circom_CalcWit* ctx)"
-                chunkdecls.append(f"void {chunkname}{param_decls};\n")
-                main.append(f"{chunkname}(ctx_index,ctx);\n")
-                chunk_path = os.path.join(dest_dir, chunkname + ".cpp")
+                global_decls.append(f"void {step_name}{param_decls};\n")
+                main.append(f"state.{step_name}(ctx_index,ctx);\n")
+                chunk_file_name = f"{func_name}_{step_name}.cpp"
+                chunk_path = os.path.join(dest_dir, chunk_file_name)
                 with open(chunk_path, "w") as output:
                     output.write(f'#include "{func_name}.h"\n')
-                    output.write(f'void {chunkname}{param_decls}{{\n')
+                    output.write(f'void {method_name}{param_decls}{{\n')
                     output.writelines(cur_chunk)
                     output.write("}\n")
                 cur_chunk = []
         nesting -= line.count("}")
 
 globals_path = os.path.join(dest_dir, func_name + ".h")
+global_decls.append("};\n")
 with open(globals_path, "w") as output:
     output.writelines(global_decls)
 
 main_path = os.path.join(dest_dir, func_name + ".cpp")
 with open(main_path, "w") as output:
     output.write(f'#include "{func_name}.h"\n')
-    output.writelines(global_defs)
-    output.writelines(chunkdecls)
     output.writelines(main)
