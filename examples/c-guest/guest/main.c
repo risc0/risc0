a84bb1944392eccd13358e550c529eb8a1efc3b9
Eric Tu <tu.eric@hotmail.com>
2025-03-27 14:50:09 -0400

    Building platform also builds headers

--- examples/c-guest/guest/main.c
+++ examples/c-guest/guest/main.c
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "platform.h"
+#include "out/platform/platform.h"
 #include <assert.h>
 #include <stdint.h>
 
