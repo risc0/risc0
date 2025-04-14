7002137f90a4187276b0c1ce3f8bfcd909a90649
bobbobbio <bobbobbio@gmail.com>
2025-04-14 16:48:40 -0700

    Bump the MAX_INSN_CYCLES for po2 >= 15
    This should help us avoid the large bigint program splitting issue. If a
    bigint program uses more cycles than this threshold, we may end up
    overflowing the segment and the execution errors. This threshold was too
    low, we had bigint programs that use more cycles in practice. After
    doing some worst-case analysis on the bigint programs we have current I
    came up with this new threshold.

--- risc0/circuit/rv32im/src/execute/executor.rs
+++ risc0/circuit/rv32im/src/execute/executor.rs
@@ -280,7 +280,6 @@ impl<'a, 'b, S: Syscall> Executor<'a, 'b, S> {
 
             let final_cycles = self.segment_cycles().next_power_of_two();
             let final_po2 = log2_ceil(final_cycles as usize);
-            let segment_threshold = (1 << final_po2) - max_insn_cycles as u32;
             let (pre_image, pre_digest, post_digest) = self.pager.commit();
             let req = ComputePartialImageRequest {
                 image: pre_image,
@@ -292,7 +291,7 @@ impl<'a, 'b, S: Syscall> Executor<'a, 'b, S> {
                 user_cycles: self.user_cycles,
                 pager_cycles: self.pager.cycles,
                 terminate_state: self.terminate_state,
-                segment_threshold,
+                segment_threshold: 0, // meaningless for final segment
                 pre_digest,
                 post_digest,
                 po2: final_po2 as u32,
