192c17cc965022ca5e4aa94e9f8241898ce378b7
Erik Kaneda <erik@risczero.com>
2025-04-21 18:58:19 -0700

    wip

--- risc0/zkvm/src/guest/mod.rs
+++ risc0/zkvm/src/guest/mod.rs
@@ -91,7 +91,122 @@ pub use crate::entry;
 #[cfg(target_os = "zkvm")]
 core::arch::global_asm!(include_str!("memset.s"));
 #[cfg(target_os = "zkvm")]
-core::arch::global_asm!(include_str!("memcpy.s"));
+#[no_mangle]
+/// TODO
+pub unsafe fn memcpy(mut dest: *mut u8, mut src: *const u8, mut n: usize) -> *mut u8 {
+    use core::{mem::size_of, ptr};
+
+    let orig_dest = dest;
+    let word_size = size_of::<usize>();
+    let align_mask = word_size - 1;
+
+    // Align dest
+    while (src as usize & align_mask) != 0 && n > 0 {
+        ptr::write(dest, ptr::read(src));
+        dest = dest.add(1);
+        src = src.add(1);
+        n -= 1;
+    }
+
+    if dest as usize % 4 == 0 {
+        // Bulk copy using word-sized ops
+        let mut d_word = dest as *mut usize;
+        let mut s_word = src as *const usize;
+
+        // Copy in larger unrolled chunks if available
+        while n >= 4 * word_size {
+            ptr::write(d_word, ptr::read(s_word));
+            ptr::write(d_word.add(1), ptr::read(s_word.add(1)));
+            ptr::write(d_word.add(2), ptr::read(s_word.add(2)));
+            ptr::write(d_word.add(3), ptr::read(s_word.add(3)));
+
+            d_word = d_word.add(4);
+            s_word = s_word.add(4);
+            n -= 4 * word_size;
+        }
+        dest = d_word as *mut u8;
+        src = s_word as *const u8;
+        if n & 8 == 8 {
+            *d_word = *s_word;
+            *(d_word.add(1)) = *(s_word.add(1));
+            d_word = d_word.add(2);
+            s_word = s_word.add(2);
+            dest = d_word as *mut u8;
+            src = s_word as *const u8;
+        }
+        if n & 4 == 4 {
+            ptr::write(d_word, ptr::read(s_word));
+            d_word = d_word.add(1);
+            s_word = s_word.add(1);
+            dest = d_word as *mut u8;
+            src = s_word as *const u8;
+        }
+        if n & 2 == 2 {
+            ptr::write(dest, ptr::read(src));
+            dest = dest.add(1);
+            src = src.add(1);
+            ptr::write(dest, ptr::read(src));
+            dest = dest.add(1);
+            src = src.add(1);
+        }
+        if n & 1 == 1 {
+            ptr::write(dest, ptr::read(src));
+        }
+        return orig_dest;
+    }
+    if (n >= 32) switch (dest as usize % 4) {
+        case 1:
+            w = *(u32 *)s;
+            *d++ = *s++;
+            *d++ = *s++;
+            *d++ = *s++;
+            n -= 3;
+            for (; n>=17; s+=16, d+=16, n-=16) {
+                x = *(u32 *)(s+1);
+                *(u32 *)(d+0) = (w LS 24) | (x RS 8);
+                w = *(u32 *)(s+5);
+                *(u32 *)(d+4) = (x LS 24) | (w RS 8);
+                x = *(u32 *)(s+9);
+                *(u32 *)(d+8) = (w LS 24) | (x RS 8);
+                w = *(u32 *)(s+13);
+                *(u32 *)(d+12) = (x LS 24) | (w RS 8);
+            }
+            break;
+        case 2:
+            w = *(u32 *)s;
+            *d++ = *s++;
+            *d++ = *s++;
+            n -= 2;
+            for (; n>=18; s+=16, d+=16, n-=16) {
+                x = *(u32 *)(s+2);
+                *(u32 *)(d+0) = (w LS 16) | (x RS 16);
+                w = *(u32 *)(s+6);
+                *(u32 *)(d+4) = (x LS 16) | (w RS 16);
+                x = *(u32 *)(s+10);
+                *(u32 *)(d+8) = (w LS 16) | (x RS 16);
+                w = *(u32 *)(s+14);
+                *(u32 *)(d+12) = (x LS 16) | (w RS 16);
+            }
+            break;
+        case 3:
+            w = *(u32 *)s;
+            *d++ = *s++;
+            n -= 1;
+            for (; n>=19; s+=16, d+=16, n-=16) {
+                x = *(u32 *)(s+3);
+                *(u32 *)(d+0) = (w LS 8) | (x RS 24);
+                w = *(u32 *)(s+7);
+                *(u32 *)(d+4) = (x LS 8) | (w RS 24);
+                x = *(u32 *)(s+11);
+                *(u32 *)(d+8) = (w LS 8) | (x RS 24);
+                w = *(u32 *)(s+15);
+                *(u32 *)(d+12) = (x LS 8) | (w RS 24);
+            }
+            break;
+        }
+
+    orig_dest
+}
 
 fn _fault() -> ! {
     #[cfg(target_os = "zkvm")]
