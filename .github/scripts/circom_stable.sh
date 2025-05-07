4fdb38f82ccedd9f951e0c17296aa9cee70b6a31
FT <140458077+zeevick10@users.noreply.github.com>
2025-05-07 09:14:38 +0200

    Update circom_stable.sh

--- .github/scripts/circom_stable.sh
+++ .github/scripts/circom_stable.sh
@@ -5,7 +5,7 @@ if [ $RISC0_LIB_HASH != $RISC0_LIB_EXPECTED_HASH ]; then
     exit 1
 fi
     
-STARK_VERIFY_HASH=$(git hash-object -- compact_proof/groth16/stark_verify.circom | awk '{ print $1 }')
+STARK_VERIFY_HASH=$(git hash-object -- groth16_proof/groth16/stark_verify.circom | awk '{ print $1 }')
 STARK_VERIFY_EXPECTED_HASH="81557c4f15ce43f5c7c840c287e76abd3a676cd5"
 if [ $STARK_VERIFY_HASH != $STARK_VERIFY_EXPECTED_HASH ]; then
     echo "Unexpected hash of stark_verify.circom: $STARK_VERIFY_HASH"
