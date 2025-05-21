52b50b886fb1d2123c775033affcf27be9e0d85a
Frank Laub <frank@risczero.com>
2025-05-21 00:18:36 -0700

    Fix

--- risc0/zkvm/compat/src/bin/local-r0vm.rs
+++ risc0/zkvm/compat/src/bin/local-r0vm.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    risc0_r0vm::main().await
+fn main() {
+    risc0_r0vm::main();
 }
