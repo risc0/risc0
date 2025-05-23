36e47f71f57b86285eeab010f94781bee4d7219b
Cohan Carpentier <cohan.carpentier@gmail.com>
2025-05-23 12:51:36 -0400

    add 3.0 to reports

--- web/apps/benchmarks-and-reports/src/versions.js
+++ web/apps/benchmarks-and-reports/src/versions.js
@@ -2,30 +2,34 @@
 // the one marked as latest will be the one that is shown when the user navigates to `/` by default
 // you should only have a single version marked as latest
 export const VERSIONS = [
-  {
-    label: "next",
-    value: "main", // value is the branch name on github
-  },
-  {
-    label: "2.0",
-    value: "release-2.0", // value is the branch name on github
-  },
-  {
-    label: "1.2",
-    value: "release-1.2", // value is the branch name on github
-  },
-  {
-    label: "1.1",
-    value: "release-1.1", // value is the branch name on github
-  },
-  {
-    label: "1.0",
-    value: "release-1.0", // value is the branch name on github
-  },
-  {
-    label: "0.21",
-    value: "release-0.21", // value is the branch name on github
-  },
+	{
+		label: "next",
+		value: "main", // value is the branch name on github
+	},
+	{
+		label: "3.0",
+		value: "release-3.0", // value is the branch name on github
+	},
+	{
+		label: "2.0",
+		value: "release-2.0", // value is the branch name on github
+	},
+	{
+		label: "1.2",
+		value: "release-1.2", // value is the branch name on github
+	},
+	{
+		label: "1.1",
+		value: "release-1.1", // value is the branch name on github
+	},
+	{
+		label: "1.0",
+		value: "release-1.0", // value is the branch name on github
+	},
+	{
+		label: "0.21",
+		value: "release-0.21", // value is the branch name on github
+	},
 ];
 
-export const latestVersion = "release-2.0";
+export const latestVersion = "release-3.0";
