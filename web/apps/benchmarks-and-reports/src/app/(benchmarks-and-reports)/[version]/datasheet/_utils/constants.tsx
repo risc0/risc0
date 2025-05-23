26e7f34a83175bbe047caf77fed8e5a9d67c96ad
Cohan Carpentier <cohan.carpentier@gmail.com>
2025-05-23 14:24:04 -0400

    fmt

--- web/apps/benchmarks-and-reports/src/app/(benchmarks-and-reports)/[version]/datasheet/_utils/constants.tsx
+++ web/apps/benchmarks-and-reports/src/app/(benchmarks-and-reports)/[version]/datasheet/_utils/constants.tsx
@@ -3,126 +3,126 @@ import type { ReactNode } from "react";
 import type { Version } from "~/types/version";
 
 export const DATASHEET_FILENAMES_TO_TITLES: {
-	[K in Version]: {
-		[key: string]: ReactNode;
-	};
+  [K in Version]: {
+    [key: string]: ReactNode;
+  };
 } = {
-	main: {
-		"macOS-apple_m2_pro.json": "Metal on Apple M2 Pro",
-		"Linux-nvidia_rtx_3090_ti.json": "CUDA on NVIDIA RTX 3090 Ti",
-		"Linux-nvidia_rtx_4090.json": "CUDA on NVIDIA RTX 4090",
-		"Linux-g6.xlarge.json": (
-			<>
-				CUDA on{" "}
-				<Link
-					rel="noopener noreferrer"
-					target="_blank"
-					href="https://instances.vantage.sh/aws/ec2/g6.xlarge"
-					className="link"
-				>
-					g6.xlarge
-				</Link>
-			</>
-		),
-		"Linux-cpu.json": (
-			<>
-				CPU only on{" "}
-				<Link
-					rel="noopener noreferrer"
-					target="_blank"
-					href="https://instances.vantage.sh/aws/ec2/c6i.8xlarge"
-					className="link"
-				>
-					c6i.8xlarge
-				</Link>
-			</>
-		),
-	},
-	"release-0.21": {
-		"macOS-apple_m2_pro.json": "Metal on Apple M2 Pro",
-		"Linux-nvidia_rtx_3090_ti.json": "CUDA on NVIDIA RTX 3090 Ti",
-		"macOS-cpu.json": "CPU only on Apple M2 Pro",
-		"Linux-cpu.json": (
-			<>
-				CPU only on{" "}
-				<Link
-					rel="noopener noreferrer"
-					target="_blank"
-					href="https://instances.vantage.sh/aws/ec2/c6i.8xlarge"
-					className="link"
-				>
-					c6i.8xlarge
-				</Link>
-			</>
-		),
-	},
-	"release-1.0": {
-		"macOS-apple_m2_pro.json": "Metal on Apple M2 Pro",
-		"Linux-nvidia_rtx_3090_ti.json": "CUDA on NVIDIA RTX 3090 Ti",
-		"macOS-cpu.json": "CPU only on Apple M2 Pro",
-		"Linux-cpu.json": (
-			<>
-				CPU only on{" "}
-				<Link
-					rel="noopener noreferrer"
-					target="_blank"
-					href="https://instances.vantage.sh/aws/ec2/c6i.8xlarge"
-					className="link"
-				>
-					c6i.8xlarge
-				</Link>
-			</>
-		),
-	},
-	"release-1.1": {
-		"macOS-apple_m2_pro.json": "Metal on Apple M2 Pro",
-		"Linux-nvidia_rtx_3090_ti.json": "CUDA on NVIDIA RTX 3090 Ti",
-		"Linux-cpu.json": (
-			<>
-				CPU only on{" "}
-				<Link
-					rel="noopener noreferrer"
-					target="_blank"
-					href="https://instances.vantage.sh/aws/ec2/c6i.8xlarge"
-					className="link"
-				>
-					c6i.8xlarge
-				</Link>
-			</>
-		),
-	},
-	"release-1.2": {
-		"macOS-apple_m2_pro.json": "Metal on Apple M2 Pro",
-		"Linux-nvidia_rtx_3090_ti.json": "CUDA on NVIDIA RTX 3090 Ti",
-		"Linux-cpu.json": (
-			<>
-				CPU only on{" "}
-				<Link
-					rel="noopener noreferrer"
-					target="_blank"
-					href="https://instances.vantage.sh/aws/ec2/c6i.8xlarge"
-					className="link"
-				>
-					c6i.8xlarge
-				</Link>
-			</>
-		),
-	},
-	"release-2.0": {
-		"macOS-apple_m2_pro.json": "Metal on Apple M2 Pro",
-		"Linux-nvidia_rtx_3090_ti.json": "CUDA on NVIDIA RTX 3090 Ti",
-		"Linux-cpu.json": (
-			<>
-				CPU only on{" "}
-				<Link
-					rel="noopener noreferrer"
-					target="_blank"
-					href="https://instances.vantage.sh/aws/ec2/c6i.8xlarge"
-					className="link"
-				>
-					c6i.8xlarge
-				</Link>
-			</>
-		),
-	},
+  main: {
+    "macOS-apple_m2_pro.json": "Metal on Apple M2 Pro",
+    "Linux-nvidia_rtx_3090_ti.json": "CUDA on NVIDIA RTX 3090 Ti",
+    "Linux-nvidia_rtx_4090.json": "CUDA on NVIDIA RTX 4090",
+    "Linux-g6.xlarge.json": (
+      <>
+        CUDA on{" "}
+        <Link
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://instances.vantage.sh/aws/ec2/g6.xlarge"
+          className="link"
+        >
+          g6.xlarge
+        </Link>
+      </>
+    ),
+    "Linux-cpu.json": (
+      <>
+        CPU only on{" "}
+        <Link
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://instances.vantage.sh/aws/ec2/c6i.8xlarge"
+          className="link"
+        >
+          c6i.8xlarge
+        </Link>
+      </>
+    ),
+  },
+  "release-0.21": {
+    "macOS-apple_m2_pro.json": "Metal on Apple M2 Pro",
+    "Linux-nvidia_rtx_3090_ti.json": "CUDA on NVIDIA RTX 3090 Ti",
+    "macOS-cpu.json": "CPU only on Apple M2 Pro",
+    "Linux-cpu.json": (
+      <>
+        CPU only on{" "}
+        <Link
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://instances.vantage.sh/aws/ec2/c6i.8xlarge"
+          className="link"
+        >
+          c6i.8xlarge
+        </Link>
+      </>
+    ),
+  },
+  "release-1.0": {
+    "macOS-apple_m2_pro.json": "Metal on Apple M2 Pro",
+    "Linux-nvidia_rtx_3090_ti.json": "CUDA on NVIDIA RTX 3090 Ti",
+    "macOS-cpu.json": "CPU only on Apple M2 Pro",
+    "Linux-cpu.json": (
+      <>
+        CPU only on{" "}
+        <Link
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://instances.vantage.sh/aws/ec2/c6i.8xlarge"
+          className="link"
+        >
+          c6i.8xlarge
+        </Link>
+      </>
+    ),
+  },
+  "release-1.1": {
+    "macOS-apple_m2_pro.json": "Metal on Apple M2 Pro",
+    "Linux-nvidia_rtx_3090_ti.json": "CUDA on NVIDIA RTX 3090 Ti",
+    "Linux-cpu.json": (
+      <>
+        CPU only on{" "}
+        <Link
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://instances.vantage.sh/aws/ec2/c6i.8xlarge"
+          className="link"
+        >
+          c6i.8xlarge
+        </Link>
+      </>
+    ),
+  },
+  "release-1.2": {
+    "macOS-apple_m2_pro.json": "Metal on Apple M2 Pro",
+    "Linux-nvidia_rtx_3090_ti.json": "CUDA on NVIDIA RTX 3090 Ti",
+    "Linux-cpu.json": (
+      <>
+        CPU only on{" "}
+        <Link
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://instances.vantage.sh/aws/ec2/c6i.8xlarge"
+          className="link"
+        >
+          c6i.8xlarge
+        </Link>
+      </>
+    ),
+  },
+  "release-2.0": {
+    "macOS-apple_m2_pro.json": "Metal on Apple M2 Pro",
+    "Linux-nvidia_rtx_3090_ti.json": "CUDA on NVIDIA RTX 3090 Ti",
+    "Linux-cpu.json": (
+      <>
+        CPU only on{" "}
+        <Link
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://instances.vantage.sh/aws/ec2/c6i.8xlarge"
+          className="link"
+        >
+          c6i.8xlarge
+        </Link>
+      </>
+    ),
+  },
 } as const satisfies Record<Version, Record<string, ReactNode>>;
