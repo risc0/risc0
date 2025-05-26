e3c2de1c6a48c1d87628749acf53aa6e3da05435
Cohan Carpentier <cohan.carpentier@gmail.com>
2025-05-26 15:03:31 -0400

    reports: add version-3.0

--- web/apps/benchmarks-and-reports/src/types/version.ts
+++ web/apps/benchmarks-and-reports/src/types/version.ts
@@ -1,1 +1,8 @@
-export type Version = "main" | "release-0.21" | "release-1.0" | "release-1.1" | "release-1.2" | "release-2.0";
+export type Version =
+	| "main"
+	| "release-0.21"
+	| "release-1.0"
+	| "release-1.1"
+	| "release-1.2"
+	| "release-2.0"
+	| "release-3.0";
