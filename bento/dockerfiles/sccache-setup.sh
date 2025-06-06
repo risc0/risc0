87ae62a73e125038494248612798dcfb8d9a56c2
CrazyFrog <anna.shuraeva13@gmail.com>
2025-06-06 13:36:22 +0200

    Update sccache-setup.sh

--- bento/dockerfiles/sccache-setup.sh
+++ bento/dockerfiles/sccache-setup.sh
@@ -4,6 +4,6 @@ set -e -u -o pipefail
 SCCACHE_ARCH=$1
 SCCACHE_VERSION=$2
 SCCACHE_FILE=sccache-$SCCACHE_VERSION-$SCCACHE_ARCH
-SCCACHE_URL=https://github.com/mozilla/sccache/releases/download
+SCCACHE_URL=https://github.com/mozilla/sccache/releases/tag/v0.10.0
 curl -L "$SCCACHE_URL/$SCCACHE_VERSION/$SCCACHE_FILE.tar.gz" | tar xz
 mv -f $SCCACHE_FILE/sccache /usr/local/bin
