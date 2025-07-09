db87dfa08eafa5d268aad61fbedb8eb6dcc21b7e
leopardracer <136604165+leopardracer@users.noreply.github.com>
2025-07-09 15:02:20 +0300

    Update api.rs

--- risc0/r0vm/src/api.rs
+++ risc0/r0vm/src/api.rs
@@ -538,7 +538,7 @@ async fn prove_groth16(
     //     snark_stream,
     // ) = helpers::get_or_create_streams(&state.db_pool, &api_key)
     //     .await
-    //     .context("Failed to get / create steams")?;
+    //     .context("Failed to get / create streams")?;
 
     // let task_def = serde_json::to_value(TaskType::Snark(WorkflowSnarkReq {
     //     receipt: start_req.session_id,
