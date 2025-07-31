094fee0c3ecbdea2b7499cd89bc3991f02fa6501
Remi Bernotavicius <bobbobbio@gmail.com>
2025-07-31 12:33:17 -0700

    Add groth16-sys crate

---
+++ risc0/groth16-sys/build.rs
@@ -0,0 +1,39 @@
+// Copyright 2025 RISC Zero, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::env;
+
+use risc0_build_kernel::{KernelBuild, KernelType};
+
+fn main() {
+    if env::var("CARGO_FEATURE_CUDA").is_ok() {
+        build_cuda_kernels();
+    }
+}
+
+fn build_cuda_kernels() {
+    let mut build = KernelBuild::new(KernelType::Cuda);
+    if env::var("CARGO_FEATURE_SETUP").is_ok() {
+        build.flag("-DSRS_READ_COEFFS");
+    }
+    build
+        .files(["kernels/cuda/ffi.cu"])
+        .deps(["kernels/cuda"])
+        .flag("-D__ADX__")
+        .include(env::var("DEP_BLST_C_SRC").unwrap())
+        .include(env::var("DEP_SPPARK_ROOT").unwrap())
+        .compile("risc0_groth16_cuda");
+
+    println!("cargo:rustc-link-lib=static=blst");
+}
