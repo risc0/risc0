182adc78a3ff4d52985c84afd9a50cad0a1a375a
Remi Bernotavicius <bobbobbio@gmail.com>
2025-08-07 15:46:23 -0700

    Remove the lock around groth16

--- risc0/zkvm/src/host/server/prove/mod.rs
+++ risc0/zkvm/src/host/server/prove/mod.rs
@@ -174,9 +174,6 @@ pub trait ProverServer: private::Sealed {
         &self,
         receipt: &SuccinctReceipt<ReceiptClaim>,
     ) -> Result<Groth16Receipt<ReceiptClaim>> {
-        #[cfg(feature = "cuda")]
-        let _lock = risc0_zkp::hal::cuda::singleton().lock();
-
         let ident_receipt = self.identity_p254(receipt).unwrap();
         let seal_bytes = ident_receipt.get_seal_bytes();
         let seal = shrink_wrap(&seal_bytes)?.to_vec();
