0ada05acdc1a7e66b9d210e3e9247066d2bd9058
Cohan Carpentier <cohan.carpentier@gmail.com>
2025-08-19 09:20:43 -0400

    deps

--- web/apps/risczero/next.config.ts
+++ web/apps/risczero/next.config.ts
@@ -5,122 +5,118 @@ import deepmerge from "deepmerge";
 import type { NextConfig } from "next";
 
 const config: NextConfig = deepmerge(nextConfigBase, {
-  experimental: {
-    reactCompiler: true,
-    ppr: false, // dont enable ppr, it sucks
-  },
+	experimental: {
+		reactCompiler: true,
+		ppr: false, // dont enable ppr, it sucks
+	},
 
-  typescript: {
-    ignoreBuildErrors: true, // TODO: remove this
-  },
+	images: {
+		remotePatterns: [
+			{ hostname: "www.notion.so" },
+			{ hostname: "lh3.googleusercontent.com" },
+			{ hostname: "s3-us-west-2.amazonaws.com" },
+			{ hostname: "avatars.githubusercontent.com" },
+			{ hostname: "avatar.vercel.sh" },
+		],
+	},
 
-  images: {
-    remotePatterns: [
-      { hostname: "www.notion.so" },
-      { hostname: "lh3.googleusercontent.com" },
-      { hostname: "s3-us-west-2.amazonaws.com" },
-      { hostname: "avatars.githubusercontent.com" },
-      { hostname: "avatar.vercel.sh" },
-    ],
-  },
+	async redirects() {
+		return [
+			{
+				source: "/tos",
+				destination: "/terms-of-service",
+				permanent: true,
+			},
+			{
+				source: "/zkvm",
+				destination: "/",
+				permanent: true,
+			},
+			{
+				source: "/team",
+				destination: "/our-story",
+				permanent: true,
+			},
+			{
+				source: "/blog/ethdenver",
+				destination: "/blog/ethdenver-2024-recap",
+				permanent: true,
+			},
+			{
+				source: "/get-started",
+				destination: "/",
+				permanent: true,
+			},
+			{
+				source: "/use-cases-demos",
+				destination: "/",
+				permanent: true,
+			},
+			{
+				source: "/policy",
+				destination: "/privacy-policy",
+				permanent: true,
+			},
+			{
+				source: "/news/:path*",
+				destination: "/blog/:path*",
+				permanent: true,
+			},
 
-  // biome-ignore lint/suspicious/useAwait: needs to be async
-  async redirects() {
-    return [
-      {
-        source: "/tos",
-        destination: "/terms-of-service",
-        permanent: true,
-      },
-      {
-        source: "/zkvm",
-        destination: "/",
-        permanent: true,
-      },
-      {
-        source: "/team",
-        destination: "/our-story",
-        permanent: true,
-      },
-      {
-        source: "/blog/ethdenver",
-        destination: "/blog/ethdenver-2024-recap",
-        permanent: true,
-      },
-      {
-        source: "/get-started",
-        destination: "/",
-        permanent: true,
-      },
-      {
-        source: "/use-cases-demos",
-        destination: "/",
-        permanent: true,
-      },
-      {
-        source: "/policy",
-        destination: "/privacy-policy",
-        permanent: true,
-      },
-      {
-        source: "/news/:path*",
-        destination: "/blog/:path*",
-        permanent: true,
-      },
-
-      // CSV-based redirects from old webflow site
-      {
-        source: "/docs/(.*)",
-        destination: "https://dev.risczero.com",
-        permanent: true,
-      },
-      {
-        source: "/docs",
-        destination: "https://dev.risczero.com",
-        permanent: true,
-      },
-      {
-        source: "/mailing",
-        destination: "/",
-        permanent: true,
-      },
-      {
-        source: "/proof-system-in-detail.pdf",
-        destination: "https://dev.risczero.com/proof-system-in-detail.pdf",
-        permanent: true,
-      },
-      {
-        source: "/technology---zkvm",
-        destination: "/",
-        permanent: true,
-      },
-      {
-        source: "/technology---bonsai",
-        destination: "/bonsai",
-        permanent: true,
-      },
-      {
-        source: "/blog/risc-zero-take-the-rekt-test",
-        destination: "/blog/risc-zero-takes-the-rekt-test",
-        permanent: true,
-      },
-      {
-        source: "/technology-zkvm",
-        destination: "/",
-        permanent: true,
-      },
-      {
-        source: "/technology-bonsai",
-        destination: "/bonsai",
-        permanent: true,
-      },
-      {
-        source: "/install",
-        destination: "https://risc0-artifacts.s3.us-west-2.amazonaws.com/rzup/prod/install",
-        permanent: true,
-      },
-    ];
-  },
+			// CSV-based redirects from old webflow site
+			{
+				source: "/docs/(.*)",
+				destination: "https://dev.risczero.com",
+				permanent: true,
+			},
+			{
+				source: "/docs",
+				destination: "https://dev.risczero.com",
+				permanent: true,
+			},
+			{
+				source: "/mailing",
+				destination: "/",
+				permanent: true,
+			},
+			{
+				source: "/proof-system-in-detail.pdf",
+				destination: "https://dev.risczero.com/proof-system-in-detail.pdf",
+				permanent: true,
+			},
+			{
+				source: "/technology---zkvm",
+				destination: "/",
+				permanent: true,
+			},
+			{
+				source: "/technology---bonsai",
+				destination: "/bonsai",
+				permanent: true,
+			},
+			{
+				source: "/blog/risc-zero-take-the-rekt-test",
+				destination: "/blog/risc-zero-takes-the-rekt-test",
+				permanent: true,
+			},
+			{
+				source: "/technology-zkvm",
+				destination: "/",
+				permanent: true,
+			},
+			{
+				source: "/technology-bonsai",
+				destination: "/bonsai",
+				permanent: true,
+			},
+			{
+				source: "/install",
+				destination:
+					"https://risc0-artifacts.s3.us-west-2.amazonaws.com/rzup/prod/install",
+				permanent: true,
+			},
+		];
+	},
 });
 
 export default config;
--- web/apps/risczero/src/app/not-found.tsx
+++ web/apps/risczero/src/app/not-found.tsx
@@ -1,39 +1,42 @@
+import { AsciiArt } from "@/components/ascii-art";
+import { ProgressBarLink } from "@/components/progress-bar-link";
 import { Badge } from "@risc0/ui/badge";
 import { Button } from "@risc0/ui/button";
 import type { Metadata } from "next";
-import { AsciiArt } from "@/components/ascii-art";
-import { ProgressBarLink } from "@/components/progress-bar-link";
 import { Subtitle } from "./_components/subtitle";
 import { Title } from "./_components/title";
 
 export const metadata: Metadata = {
-  title: "Page Not Found",
+	title: "Page Not Found",
 };
 
 export default function NotFoundPage() {
-  return (
-    <div className="-mb-24 container flex min-h-[56dvh] max-w-screen-lg flex-col items-center gap-16 py-12 lg:flex-row lg:items-center lg:justify-between">
-      <div className="space-y-10">
-        <div className="space-y-1">
-          <Badge variant="secondary" className="mb-2 self-start">
-            404 Error
-          </Badge>
-          <Title className="text-2xl md:text-2xl">Page Not Found</Title>
-          <Subtitle className="text-muted-foreground md:text-lg">
-            Sorry, the page you are looking for doesn't exist.
-          </Subtitle>
-        </div>
-        <ProgressBarLink href="/" className="inline-block" tabIndex={-1}>
-          <Button size="lg" autoFocus className="focus-visible:ring-offset-background">
-            Take Me Home
-          </Button>
-        </ProgressBarLink>
-      </div>
+	return (
+		<div className="-mb-24 container flex min-h-[56dvh] max-w-screen-lg flex-col items-center gap-16 py-12 lg:flex-row lg:items-center lg:justify-between">
+			<div className="space-y-10">
+				<div className="space-y-1">
+					<Badge variant="secondary" className="mb-2 self-start">
+						404 Error
+					</Badge>
+					<Title className="text-2xl md:text-2xl">Page Not Found</Title>
+					<Subtitle className="text-muted-foreground md:text-lg">
+						Sorry, the page you are looking for doesn't exist.
+					</Subtitle>
+				</div>
+				<ProgressBarLink href="/" className="inline-block" tabIndex={-1}>
+					<Button
+						size="lg"
+						autoFocus
+						className="focus-visible:ring-offset-background"
+					>
+						Take Me Home
+					</Button>
+				</ProgressBarLink>
+			</div>
 
-      <AsciiArt
-        label="404"
-        className="text-sm opacity-60"
-        code={`// Code
+			<AsciiArt
+				className="text-sm opacity-60"
+				code={`// Code
   ___   ___  ________  ___   ___
  |\\  \\ |\\  \\|\\   __  \\|\\  \\ |\\  \\
  \\ \\  \\\\_\\  \\ \\  \\|\\  \\ \\  \\\\_\\  \\
@@ -41,7 +44,7 @@ export default function NotFoundPage() {
    \\|_____|\\  \\ \\  \\\\\\  \\|_____|\\  \\
           \\ \\__\\ \\_______\\     \\ \\__\\
            \\|__|\\|_______|      \\|__|`}
-      />
-    </div>
-  );
+			/>
+		</div>
+	);
 }
