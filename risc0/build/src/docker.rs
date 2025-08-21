de6780ccdee3cc277dff55d5d471b07d5a456ee1
fbwoolf <6493321+fbwoolf@users.noreply.github.com>
2025-08-21 13:23:42 -0400

    chore: add MIT license

--- risc0/build/src/docker.rs
+++ risc0/build/src/docker.rs
@@ -1,16 +1,11 @@
 // Copyright 2025 RISC Zero, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// This project is dual-licensed under either:
+// - Apache License, Version 2.0 (LICENSE-APACHE or https://www.apache.org/licenses/LICENSE-2.0)
+// - MIT License (LICENSE-MIT or https://opensource.org/licenses/MIT)
+// at your option.
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
 
 use std::{fs, path::Path, process::Command};
 
@@ -262,7 +257,7 @@ mod test {
         compare_image_id(
             &guest_list,
             "hello_commit",
-            "dc6efb770a23359f19f40b961fe200eaa5783448b430b254ce5a93add287bd67",
+            "a38116dbc7da96a605d9cdce2d2012b97f6f8138c5efa425c3571c7a1b7de513",
         );
     }
 }
--- risc0/cargo-risczero/templates/rust-starter/methods/build.rs
+++ risc0/cargo-risczero/templates/rust-starter/methods/build.rs
@@ -1,3 +1,12 @@
+// Copyright 2025 RISC Zero, Inc.
+//
+// This project is dual-licensed under either:
+// - Apache License, Version 2.0 (LICENSE-APACHE or https://www.apache.org/licenses/LICENSE-2.0)
+// - MIT License (LICENSE-MIT or https://opensource.org/licenses/MIT)
+// at your option.
+//
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
 fn main() {
     risc0_build::embed_methods();
 }
