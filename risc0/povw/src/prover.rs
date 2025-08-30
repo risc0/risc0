6bbfb7421343220d94cc8c1b9885c8814652673c
Victor Graf <victor@risczero.com>
2025-08-30 15:59:59 -0700

    add a note

--- risc0/povw/src/prover.rs
+++ risc0/povw/src/prover.rs
@@ -217,6 +217,10 @@ impl<P: Prover> WorkLogUpdateProver<P> {
             .build()
             .context("failed to build ExecutorEnv")?;
 
+        // NOTE: This may block the current thread for a significant amount of time. It is not
+        // trivial to wrap this statement in e.g. tokio's spawn_blocking because self contains
+        // a VerifierContext which does not implement Send. If this causes any issues, the caller
+        // can mitigate the issue by building and calling the prover in a seperate thread.
         let prove_info = self
             .prover
             .prove_with_ctx(
