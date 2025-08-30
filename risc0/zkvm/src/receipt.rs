d065f4fb2915832abf2efb3e7f13ad41e25a0d64
VolodymyrBg <aqdrgg19@gmail.com>
2025-08-30 05:40:20 +0300

    docs: fix typos and grammar in risc0 zkp/zkvm docs and comments (#3373)
    Fixed a lot of typos in code comments
    "are have been → have been"
    "it attempted" → "is attempted"
    "know → known"
    
    and many others

--- risc0/zkvm/src/receipt.rs
+++ risc0/zkvm/src/receipt.rs
@@ -125,7 +125,7 @@ pub struct Receipt {
     /// Metadata providing context on the receipt, about the proving system, SDK versions, and other
     /// information to help with interoperability. It is not cryptographically bound to the receipt,
     /// and should not be used for security-relevant decisions, such as choosing whether or not to
-    /// accept a receipt based on it's stated version.
+    /// accept a receipt based on its stated version.
     pub metadata: ReceiptMetadata,
 }
 
@@ -228,8 +228,8 @@ impl Receipt {
             journal: MaybePruned::Pruned(self.journal.digest()),
             // TODO(#982): It would be reasonable for this method to allow integrity verification
             // for receipts that have a non-empty assumptions list, but it is not supported here
-            // because we don't have a enough information to open the assumptions list unless we
-            // require it be empty.
+            // because we don't have enough information to open the assumptions list unless we
+            // require it to be empty.
             assumptions: Assumptions(vec![]).into(),
         });
 
