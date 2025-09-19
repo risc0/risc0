2c974247118552a83b3ba476ede11b5c2916bc6f
Frank Laub <flaub@risc0.com>
2025-09-18 18:02:39 -0700

    Dual-license Apache 2 OR MIT (#3426)
    Co-authored-by: fbwoolf <6493321+fbwoolf@users.noreply.github.com>

--- license-check.py
+++ license-check.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import sys
-import os
 import re
 from pathlib import Path
 import subprocess
@@ -10,17 +9,18 @@ import argparse
 PUBLIC_HEADER = """
 // Copyright {YEAR} RISC Zero, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0 OR MIT
 """.strip()
 
 PUBLIC_HEADER_RE = re.compile(
