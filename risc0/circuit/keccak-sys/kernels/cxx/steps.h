c1216e8a530c479faaf375dfc0d9dd7e407f35a0
Jeremy Bruestle <jeremy@risczero.com>
2025-10-01 16:48:18 -0700

    Bootstrap new circuit in and update preflight code

--- risc0/circuit/keccak-sys/kernels/cxx/steps.h
+++ risc0/circuit/keccak-sys/kernels/cxx/steps.h
@@ -1,17 +1,16 @@
 // Copyright 2025 RISC Zero, Inc.
 //
-// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
-// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
-// http://opensource.org/licenses/MIT>, at your option. This file may not be
-// copied, modified, or distributed except according to those terms.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
-// SPDX-License-Identifier: Apache-2.0 OR MIT
 
 #pragma once
 
@@ -45,10 +44,10 @@ extern void step_Top_18(ExecContext& ctx,MutableBuf arg0)  ;
 extern void step_Top_19(ExecContext& ctx,MutableBuf arg0)  ;
 extern void step_Top_20(ExecContext& ctx,MutableBuf arg0)  ;
 extern void step_Top_21(ExecContext& ctx,MutableBuf arg0)  ;
-extern void step_Top_22(ExecContext& ctx,MutableBuf arg0)  ;
+extern void step_Top_22(ExecContext& ctx,MutableBuf arg0, GlobalBuf arg1)  ;
 extern void step_Top_23(ExecContext& ctx,MutableBuf arg0)  ;
 extern void step_Top_24(ExecContext& ctx,MutableBuf arg0)  ;
-extern void step_Top_25(ExecContext& ctx,MutableBuf arg0, GlobalBuf arg1)  ;
+extern void step_Top_25(ExecContext& ctx,MutableBuf arg0)  ;
 extern void step_Top_26(ExecContext& ctx,MutableBuf arg0)  ;
 extern void step_Top_27(ExecContext& ctx,MutableBuf arg0)  ;
 extern void step_Top_28(ExecContext& ctx,MutableBuf arg0)  ;
@@ -56,25 +55,5 @@ extern void step_Top_29(ExecContext& ctx,MutableBuf arg0)  ;
 extern void step_Top_30(ExecContext& ctx,MutableBuf arg0)  ;
 extern void step_Top_31(ExecContext& ctx,MutableBuf arg0)  ;
 extern void step_Top_32(ExecContext& ctx,MutableBuf arg0)  ;
-extern void step_Top_33(ExecContext& ctx,MutableBuf arg0)  ;
-extern void step_Top_34(ExecContext& ctx,MutableBuf arg0)  ;
-extern void step_Top_35(ExecContext& ctx,MutableBuf arg0)  ;
-extern void step_Top_36(ExecContext& ctx,MutableBuf arg0)  ;
-extern void step_Top_37(ExecContext& ctx,MutableBuf arg0)  ;
-extern void step_Top_38(ExecContext& ctx,MutableBuf arg0)  ;
-extern void step_Top_39(ExecContext& ctx,MutableBuf arg0)  ;
-extern void step_Top_40(ExecContext& ctx,MutableBuf arg0)  ;
-extern void step_Top_41(ExecContext& ctx,MutableBuf arg0)  ;
-extern void step_Top_42(ExecContext& ctx,MutableBuf arg0)  ;
-extern void step_Top_43(ExecContext& ctx,MutableBuf arg0)  ;
-extern void step_Top_44(ExecContext& ctx,MutableBuf arg0)  ;
-extern void step_Top_45(ExecContext& ctx,MutableBuf arg0)  ;
-extern void step_Top_46(ExecContext& ctx,MutableBuf arg0)  ;
-extern void step_Top_47(ExecContext& ctx,MutableBuf arg0)  ;
-extern void step_Top_48(ExecContext& ctx,MutableBuf arg0)  ;
-extern void step_Top_49(ExecContext& ctx,MutableBuf arg0)  ;
-extern void step_Top_50(ExecContext& ctx,MutableBuf arg0)  ;
-extern void step_Top_51(ExecContext& ctx,MutableBuf arg0)  ;
-extern void step_Top_52(ExecContext& ctx,MutableBuf arg0)  ;
 
 } // namespace risc0::circuit::keccak::cpu
