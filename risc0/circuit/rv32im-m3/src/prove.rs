eb06603c3fd183bc3b211af9f593f75a8276ff19
Frank Laub <frank@risczero.com>
2025-10-02 15:29:42 -0700

    Add all tests

--- risc0/circuit/rv32im-m3/src/prove.rs
+++ risc0/circuit/rv32im-m3/src/prove.rs
@@ -14,6 +14,7 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
 #[cfg(test)]
+#[cfg(feature = "cuda")]
 mod tests {
     use cfg_if::cfg_if;
     use risc0_circuit_rv32im_m3_sys::*;
@@ -74,7 +75,6 @@ mod tests {
         ($func_name:ident) => {
             #[test_log::test]
             #[gpu_guard::gpu_guard]
-            #[ignore]
             fn $func_name() {
                 run_test(stringify!($func_name));
             }
@@ -83,49 +83,49 @@ mod tests {
 
     test_case!(add);
     test_case!(addi);
-    // test_case!(and);
-    // test_case!(andi);
-    // test_case!(auipc);
-    // test_case!(beq);
-    // test_case!(bge);
-    // test_case!(bgeu);
-    // test_case!(blt);
-    // test_case!(bltu);
-    // test_case!(bne);
-    // test_case!(div);
-    // test_case!(divu);
+    test_case!(and);
+    test_case!(andi);
+    test_case!(auipc);
+    test_case!(beq);
+    test_case!(bge);
+    test_case!(bgeu);
+    test_case!(blt);
+    test_case!(bltu);
+    test_case!(bne);
+    test_case!(div);
+    test_case!(divu);
     // test_case!(fence);
-    // test_case!(jal);
-    // test_case!(jalr);
-    // test_case!(lb);
-    // test_case!(lbu);
-    // test_case!(lh);
-    // test_case!(lhu);
-    // test_case!(lui);
-    // test_case!(lw);
-    // test_case!(mul);
-    // test_case!(mulh);
-    // test_case!(mulhsu);
-    // test_case!(mulhu);
-    // test_case!(or);
-    // test_case!(ori);
-    // test_case!(rem);
-    // test_case!(remu);
-    // test_case!(sb);
-    // test_case!(sh);
-    // test_case!(simple);
-    // test_case!(sll);
-    // test_case!(slli);
-    // test_case!(slt);
-    // test_case!(slti);
-    // test_case!(sltiu);
-    // test_case!(sltu);
-    // test_case!(sra);
-    // test_case!(srai);
-    // test_case!(srl);
-    // test_case!(srli);
-    // test_case!(sub);
-    // test_case!(sw);
-    // test_case!(xor);
-    // test_case!(xori);
+    test_case!(jal);
+    test_case!(jalr);
+    test_case!(lb);
+    test_case!(lbu);
+    test_case!(lh);
+    test_case!(lhu);
+    test_case!(lui);
+    test_case!(lw);
+    test_case!(mul);
+    test_case!(mulh);
+    test_case!(mulhsu);
+    test_case!(mulhu);
+    test_case!(or);
+    test_case!(ori);
+    test_case!(rem);
+    test_case!(remu);
+    test_case!(sb);
+    test_case!(sh);
+    test_case!(simple);
+    test_case!(sll);
+    test_case!(slli);
+    test_case!(slt);
+    test_case!(slti);
+    test_case!(sltiu);
+    test_case!(sltu);
+    test_case!(sra);
+    test_case!(srai);
+    test_case!(srl);
+    test_case!(srli);
+    test_case!(sub);
+    test_case!(sw);
+    test_case!(xor);
+    test_case!(xori);
 }
