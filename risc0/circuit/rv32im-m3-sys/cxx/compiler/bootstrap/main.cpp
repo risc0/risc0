ca2c73102045710452b79b7b60ceea7aca7947bf
Jeremy Bruestle <jeremy@risczero.com>
2025-10-06 16:04:32 -0700

    License files

--- risc0/circuit/rv32im-m3-sys/cxx/compiler/bootstrap/main.cpp
+++ risc0/circuit/rv32im-m3-sys/cxx/compiler/bootstrap/main.cpp
@@ -1,3 +1,18 @@
+// Copyright 2025 RISC Zero, Inc.
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
 #include "core/log.h"
 #include "compiler/extractor/RecordingVal.h"
 #include "rv32im/circuit/verify.h"
