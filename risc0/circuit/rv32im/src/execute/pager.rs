1eb2fb269b98297c716f720e5311b05fb11cafa2
Victor Graf <victor@risczero.com>
2025-10-22 16:18:42 -0700

    use array instead of Vec for Page to remove one pointer indirection

--- risc0/circuit/rv32im/src/execute/pager.rs
+++ risc0/circuit/rv32im/src/execute/pager.rs
@@ -358,16 +358,6 @@ impl PagedMemory {
         }
     }
 
-    pub(crate) fn peek_page(&mut self, page_idx: u32) -> Result<Vec<u8>> {
-        if let Some(cache_idx) = self.page_table.get(page_idx) {
-            // Loaded, get from cache
-            Ok(self.page_cache[cache_idx].data().clone())
-        } else {
-            // Unloaded, peek into image
-            Ok(self.image.get_page(page_idx)?.data().clone())
-        }
-    }
-
     #[inline(always)]
     fn load_ram(&mut self, addr: WordAddr) -> Result<u32> {
         let page_idx = addr.page_idx();
