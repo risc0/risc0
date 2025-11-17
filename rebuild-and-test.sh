f8bb608a0206d1eaf225cb9c4ea5ddfc2ab83c20
Carsten Munk <carsten.munk@gmail.com>
2025-11-17 12:48:36 +0000

    stash
    Signed-off-by: Carsten Munk <carsten.munk@gmail.com>

--- rebuild-and-test.sh
+++ rebuild-and-test.sh
@@ -17,12 +17,12 @@ fi
 cd tools/elf-to-bin
 cargo build
 cd ../../risc0/zkos/linux
-cargo risczero bake
+cargo risczero bake --debug
 cd ~/risc0
 mkdir -p test-bins test-logs
 (for x in riscv-tests/isa/*-p-*.dump; do target/debug/elf-to-bin --guest-elf riscv-tests/isa/`basename $x .dump` --kernel-elf risc0/zkos/linux/elfs/vmlinuz.elf --output test-bins/`basename $x .dump`.bin; done)
 for x in test-bins/*; do
     echo -n `basename $x` " "
-    RUST_BACKTRACE=1 RISC0_DEV_MODE=1 RUST_LOG=trace r0vm --elf $x -- /bin/test &> test-logs/`basename $x`.log
+    RUST_BACKTRACE=1 RUST_LOG=trace r0vm --elf $x -- /bin/test &> test-logs/`basename $x`.log
     grep -a sys_exit test-logs/`basename $x`.log || (echo -n "crash: no sys_exit, see "; grep -L sys_exit test-logs/`basename $x`.log)
 done
