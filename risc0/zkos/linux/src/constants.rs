75be813ca41b350f9f995fc2cde4937615c43262
Carsten Munk <carsten.munk@gmail.com>
2025-11-19 08:48:15 +0000

    revert the memory changes
    Signed-off-by: Carsten Munk <carsten.munk@gmail.com>

--- risc0/zkos/linux/src/constants.rs
+++ risc0/zkos/linux/src/constants.rs
@@ -26,28 +26,24 @@ pub const FP_REGS_PTR: *mut u64 = 0xffff_4000 as *mut u64;
 pub const FCSR_PTR: *mut u32 = 0xffff_4100 as *mut u32;
 // Memory layout constants for RISC-V RV32 in zkOS
 // The address space is divided into:
-// - User space: 0x00000000 - 0x3FFFFFFF (1GB)
-// - Kernel space: 0x40000000 - 0xFFFFFFFF (3GB)
+// - User space: 0x00000000 - 0xBFFFFFFF (3GB)
+// - Kernel space: 0xC0000000 - 0xFFFFFFFF (1GB)
 
-/// Start of kernel memory space (1GB boundary)
-pub const KERNEL_SPACE_START: u32 = 0x4000_0000;
+/// Start of kernel memory space (3GB boundary)
+pub const KERNEL_SPACE_START: u32 = 0xC000_0000;
 
 /// End of user memory space (just before kernel space)
-pub const USER_SPACE_END: u32 = 0x4000_0000;
+pub const USER_SPACE_END: u32 = 0xC000_0000;
 
 pub const USER_MEMORY_START_PTR: *const u8 = 0x0001_1100 as *const u8;
-pub const USER_MEMORY_START: usize = 0x0001_1100;
-pub const USER_MEMORY_LENGTH: usize = (USER_SPACE_END as usize) - USER_MEMORY_START;
+pub const USER_MEMORY_LENGTH: usize = 0x4000_0000;
 pub const USER_START_PTR: *const usize = 0x0001_0000 as *const usize;
-pub const USER_STACK_ADDR: usize = 0x3f00_0000;
+pub const USER_STACK_ADDR: usize = 0x9c7ffe00;
 pub const USER_STACK_PTR: *const usize = USER_STACK_ADDR as *const usize;
-pub const USER_FIXUP_ADDR: usize = 0x3fff_0000;
+pub const USER_FIXUP_ADDR: usize = 0xbfff_0000;
 pub const USER_FIXUP_PTR: *mut u32 = USER_FIXUP_ADDR as *mut u32;
 #[allow(dead_code)]
 pub const USER_STACK_SIZE: usize = 2 * 1024 * 1024;
-pub const USER_STACK_MMAP_GAP: usize = 0x0800_0000; // 128MB gap between stack and mmap arena
-pub const USER_MMAP_ALIGN: usize = 0x0080_0000; // 8MB alignment for mmap base
-pub const USER_MMAP_BASE_INIT: u32 = (USER_STACK_ADDR as u32) - (USER_STACK_MMAP_GAP as u32); // 0x3780_0000
 
 #[allow(dead_code)]
 pub const USER_PHENT_SIZE: usize = 32; // ELF32_Phdr size in bytes
@@ -56,7 +52,7 @@ pub const PAGE_SIZE: usize = 4096;
 // pub const AT_VECTOR_SIZE_BASE: usize = 20;
 // pub const AT_VECTOR_SIZE_ARCH: usize = 7;
 // pub const AT_VECTOR_SIZE: usize = 2 * (AT_VECTOR_SIZE_ARCH + AT_VECTOR_SIZE_BASE + 1);
-pub const ASCII_TABLE_PTR: *const u8 = 0x3ff0_0200 as *const u8;
+pub const ASCII_TABLE_PTR: *const u8 = 0xbf00_0200 as *const u8;
 
 /// Program header table address (stored in memory)
 #[allow(dead_code)]
