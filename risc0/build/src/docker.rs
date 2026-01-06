a6115d06b9b2e94b3acfa23171c45d80d7a31cd9
Victor Graf <victor@risczero.com>
2026-01-06 11:02:31 -0800

    update reproducible build digest

--- risc0/build/src/docker.rs
+++ risc0/build/src/docker.rs
@@ -267,7 +267,7 @@ mod test {
         compare_image_id(
             &guest_list,
             "hello_commit",
-            "1adc9844aed376a96b3829cff37eafa0cf6bf9d5dc096b48a30a1ba5f8829e96",
+            "22c8e8d34512b667fd0ca3d6ab4d5c7b2449b22e859332f3e1ab27f912ec4938",
         );
     }
 }
