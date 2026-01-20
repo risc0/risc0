8379f985de7db623f405b0edafb4fc97b6f0dc50
Remi Bernotavicius <bobbobbio@gmail.com>
2026-01-20 11:53:07 -0800

    Add some checks for the size of the ZKRS in the zkr crates.

--- risc0/circuit/recursion-zkrs/build.rs
+++ risc0/circuit/recursion-zkrs/build.rs
@@ -17,6 +17,18 @@ fn main() {
     generate_zkr_table();
 }
 
+/// The crates.io upload limit is 10mb. Give ourselves 4k of overhead for other files cargo adds.
+const UPLOAD_LIMIT: u64 = (10 * 1024 * 1024) - 4 * 1024;
+
+const SOURCE_FILES: [&str; 3] = ["build.rs", "src/lib.rs", "Cargo.toml"];
+
+fn source_files_size() -> u64 {
+    SOURCE_FILES
+        .into_iter()
+        .map(|f| std::fs::File::open(f).unwrap().metadata().unwrap().len())
+        .sum()
+}
+
 fn generate_zkr_table() {
     use std::fmt::Write as _;
     use std::path::Path;
@@ -27,6 +39,8 @@ fn generate_zkr_table() {
 
     writeln!(&mut output, "pub const ZKRS: &[(&[u8], usize, &str)] = &[").unwrap();
 
+    let mut total_bytes = 0;
+
     for po2 in 12..=24 {
         let zkr_name = format!("lift_rv32im_m3_{po2}.zkr");
         let zkr_path = Path::new("src")
@@ -34,15 +48,25 @@ fn generate_zkr_table() {
             .canonicalize()
             .unwrap();
         println!("cargo:rerun-if-changed={}", zkr_path.display());
-        let mut decoder = XzDecoder::new(std::fs::File::open(&zkr_path).unwrap());
+
+        let f = std::fs::File::open(&zkr_path).unwrap();
+        total_bytes += f.metadata().unwrap().len();
+        let mut decoder = XzDecoder::new(f);
         std::io::copy(&mut decoder, &mut std::io::sink()).unwrap();
         let size = decoder.total_out();
+
         writeln!(
             &mut output,
             "(include_bytes!(\"{}\"), {size}, \"{zkr_name}\"),",
             zkr_path.display()
         )
         .unwrap();
+    }
+
+    total_bytes += source_files_size();
+
+    if total_bytes > UPLOAD_LIMIT {
+        panic!("ZKRs are too big for uploading to crates.io ({total_bytes} > {UPLOAD_LIMIT})");
     }
 
     writeln!(&mut output, "];").unwrap();
